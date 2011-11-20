a9dbce74145cfa88974c7385434e2e2e10a439f0
rgrp <rufus.pollock@okfn.org>
2011-11-20 19:11:06 +0000

    [api][s]: (refs #5) use generified API setup to add API support for Task, TaskRun.

--- test/test_api.py
+++ test/test_api.py
@@ -30,3 +30,30 @@ class TestAPI:
         out = model.Session.query(model.App).filter_by(name=name).one()
         assert out
 
+        data = dict(
+            app_id=out.id,
+            state=1,
+            info='my job data'
+            )
+        data = json.dumps(data)
+        res = self.app.post('/api/task',
+            data=data
+        )
+        tasks = model.Session.query(model.Task).filter_by(app_id=out.id).all()
+        assert tasks, tasks
+
+        data = dict(
+            app_id=out.id,
+            job_id=tasks[0].id,
+            info='my job result'
+            )
+        data = json.dumps(data)
+        res = self.app.post('/api/taskrun',
+            data=data
+        )
+        taskrun = model.Session.query(model.TaskRun).filter_by(app_id=out.id).all()
+        assert taskrun, taskrun
+        assert taskrun[0].create_time, taskrun
+        assert taskrun[0].app_id == out.id
+
+
