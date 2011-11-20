1097f44ce8f4599d8b6c0854da71323dba857f7c
rgrp <rufus.pollock@okfn.org>
2011-11-20 11:34:30 +0000

    [settings][xs]: remove sqlalchemy url from default_settings.

--- pybossa/default_settings.py
+++ pybossa/default_settings.py
@@ -7,8 +7,6 @@ PORT = 5000
 SECRET = 'foobar'
 SECRET_KEY = 'my-session-secret'
 
-SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://tester:tester@localhost/pybossa'
-
 ## project configuration
 TITLE = 'PyBossa'
 COPYRIGHT = 'Your Institution'
