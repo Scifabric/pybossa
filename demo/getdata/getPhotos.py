dccab4f41956edcc2355bc306aedf1150fd7de40
Daniel Lombraña González <teleyinex@gmail.com>
2011-12-07 11:37:16 +0100

    The script creates the application in PyBOSSA (refs #10)
    The demo application checks if the application has been already registered,
    otherwise it will create it in the DB

--- demo/getdata/getPhotos.py
+++ demo/getdata/getPhotos.py
@@ -17,39 +17,73 @@
 # along with PyBOSSA.  If not, see <http://www.gnu.org/licenses/>.
 
 
-import urllib
+import urllib2
 import json
 
+def createApp():
+    """
+    Creates the Flickr Person Finder application. First checks if the
+    application already exists in PyBOSSA, otherwise it will create it.
+    """
+
+    # Data application
+    name = u'Flickr Person Finder'
+    short_name = u'FlickrPerson'
+    description = u'Do you see a human in this photo?'
+    data = dict(name = name, short_name = short_name, description = description)
+    data = json.dumps(data)
+
+    url_api = 'http://0.0.0.0:5000/api/'
+
+    # Checking which apps have been already registered in the DB
+    create_app = True
+    apps = json.loads(urllib2.urlopen(url_api + 'app').read())
+    for app in apps:
+        if app['name'] == name: 
+            print '{app_name} app is already registered in the DB'.format(app_name = name)
+            create_app = False
+    
+    if create_app:
+        print "The application is not registered in PyBOSSA. Creating it..."
+        # Setting the POST action
+        request = urllib2.Request(url_api + 'app')
+        request.add_data(data)
+        request.add_header('Content-type', 'application/json')
+
+        # Create the app in PyBOSSA
+        output = json.loads(urllib2.urlopen(request).read())
+        if (output['create_time'] != None):
+            print "Done!"
+        else:
+            print "Error creating the application"
+
 def getFlickrPhotos(size="big"):
-        # Flickr key and tag to search
-        key = "1def541a422a4fe07880ab3204a1cc7e"
-        tags = "rainforest"
-        
-        # Get the ID of the photos and load it in the output var
-        query = "http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=" + key + "&tags=" + tags + "&format=json&nojsoncallback=1"
-        output = json.load(urllib.urlopen(query))
-        
-        # For each photo ID create its direct URL according to its size: big, medium, small
-        # (or thumbnail) + Flickr page hosting the photo
-        photos = []
-        for photo in output['photos']['photo']:
-                # Query for getting the farm, secret, etc. for the photo
-                query = "http://api.flickr.com/services/rest/?format=json&method=" + "flickr.photos.getInfo&api_key=" + key + "&photo_id=" + photo['id'] + "&secret=" + photo['secret'] + "&nojsoncallback=1";
-                info = json.load(urllib.urlopen(query))
-                # Base URL for creating the URLs according to its size
-                baseURL = 'http://farm' + str(info['photo']['farm']) + '.static.flickr.com/' + str(info['photo']['server']) + '/' + str(info['photo']['id']) + '_' + str(info['photo']['secret'])
-                if (size == "big"):
-                        PhotoURL = baseURL + '_b.jpg'
-                elif (size == "medium"):
-                        PhotoURL = baseURL + '_m.jpg'                                                                                                              
-                else:
-                        PhotoURL = baseURL + '_s.jpg'
-                # Flickr Page hosting the photo (with all the Flickr goodies)
-                # PhotoFlickrPage = 'http://www.flickr.com/photos/' + str(info['photo']['owner']['nsid']) + '/' + str(info['photo']['id'])
-                photos.append(PhotoURL)
-        return photos
+    # Flickr key and tag to search
+    key = "1def541a422a4fe07880ab3204a1cc7e"
+    tags = "rainforest"
 
-photos = getFlickrPhotos()
+    # Get the ID of the photos and load it in the output var
+    query = "http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=" + key + "&tags=" + tags + "&format=json&nojsoncallback=1"
+    output = json.load(urllib2.urlopen(query))
 
-for url in photos:
-        print url
+    # For each photo ID create its direct URL according to its size: big, medium, small
+    # (or thumbnail) + Flickr page hosting the photo
+    photos = []
+    for photo in output['photos']['photo']:
+        # Query for getting the farm, secret, etc. for the photo
+        query = "http://api.flickr.com/services/rest/?format=json&method=" + "flickr.photos.getInfo&api_key=" + key + "&photo_id=" + photo['id'] + "&secret=" + photo['secret'] + "&nojsoncallback=1";
+        info = json.load(urllib2.urlopen(query))
+        # Base URL for creating the URLs according to its size
+        baseURL = 'http://farm' + str(info['photo']['farm']) + '.static.flickr.com/' + str(info['photo']['server']) + '/' + str(info['photo']['id']) + '_' + str(info['photo']['secret'])
+        if (size == "big"):
+            PhotoURL = baseURL + '_b.jpg'
+        elif (size == "medium"):
+            PhotoURL = baseURL + '_m.jpg'                                                                                                              
+        else:
+            PhotoURL = baseURL + '_s.jpg'
+            # Flickr Page hosting the photo (with all the Flickr goodies)
+            # PhotoFlickrPage = 'http://www.flickr.com/photos/' + str(info['photo']['owner']['nsid']) + '/' + str(info['photo']['id'])
+            photos.append(PhotoURL)
+            return photos
+
+createApp()
