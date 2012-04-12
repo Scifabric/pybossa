7684d107a81ea9dc803f183b27782f6d8aecd863
Daniel Lombraña González <teleyinex@gmail.com>
2012-04-12 13:47:13 -0300

    FlickrPerson endpoint and template removed. Closes #65
    The FlickrPerson demo application now uses the new interface, so this route and the template are no longer valid.

--- pybossa/web.py
+++ pybossa/web.py
@@ -126,11 +126,6 @@ def home():
             }
     return render_template('/home/index.html', stats=stats)
 
-
-@app.route('/flickrperson')
-def flickrperson():
-    return render_template('/flickrperson/example.html')
-
 if __name__ == "__main__":
     logging.basicConfig(level=logging.NOTSET)
     app.run(host=app.config['HOST'], port=app.config['PORT'], debug=app.config.get('DEBUG', True))
