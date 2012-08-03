Scifabric/pybossa

commit 80b418e8f2f92b558a3ca60355b9b9f0a3980692

using automated query generator

pybossa/api.py
@@ -59,7 +59,7 @@ class APIBase(MethodView):
                     if k == 'limit':
                         limit = True
                     if k != 'limit' and k != 'api_key' and request.args[k] != '' and hasattr(self.__class__, k):
-                        query = query.filter("%s = '%s'" % (k, request.args[k].replace('\'', '\\\'')))
+                        query.filter(getattr(self.__class__, k) == request.args[k])
                 if limit:
                     query = query.limit(int(request.args['limit']))
                 else:

commit 4119ce40d17167af3ca25eb83ee7281dd7939401

[api][s]: fix to bug in api object search/filter introduced in 80b418e8f2f92b558a3ca60355b9b9f0a3980692.

pybossa/api.py
@@ -64,8 +64,13 @@ class APIBase(MethodView):
                 for k in request.args.keys():
                     if k == 'limit':
                         limit = True
-                    if k != 'limit' and k != 'api_key' and request.args[k] != '' and hasattr(self.__class__, k):
-                        query.filter(getattr(self.__class__, k) == request.args[k])
+                    if k != 'limit' and k != 'api_key':
+                        if not hasattr(self.__class__, k):
+                            return Response(json.dumps({
+                                'error': 'no such column: %s' % k}
+                                ), mimetype='application/json')
+                        query = query.filter(getattr(self.__class__, k) == request.args[k])
+
                 if limit:
                     query = query.limit(int(request.args['limit']))
                 else:

test/test_api.py
@@ -94,6 +94,16 @@ class TestAPI:
             res = self.app.get("/api/%s?wrongfield=value" % endpoint)
             data = json.loads(res.data)
             assert "no such column: wrongfield" in data['error'], data
+    
+    def test_query_sql_injection(self):
+        q = '1%3D1;SELECT%20*%20FROM%20task%20WHERE%201=1'
+        res = self.app.get('/api/task?' + q)
+        data = json.loads(res.data)
+        assert "no such column: " in data['error'], data
+        q = 'app_id=1%3D1;SELECT%20*%20FROM%20task%20WHERE%201'
+        res = self.app.get('/api/task?' + q)
+        data = json.loads(res.data)
+        assert len(data) == 0
 
     def test_query_app(self):
         """Test API query for app endpoint works"""