ac2f4a86b4f1a67d5e54c8028d5e546aaed6f7ac
Daniel Lombraña González <teleyinex@gmail.com>
2013-05-07 15:02:15 +0200

    Use the standard folder

--- cookbooks/pybossa/recipes/default.rb
+++ cookbooks/pybossa/recipes/default.rb
@@ -1,50 +1,49 @@
+package 'postgresql-9.1'
 package "postgresql-server-dev-9.1" 
 package "python-dev"
 
-user "pybossa" do
-  home "/home/pybossa"
-  shell "/bin/bash"
-end
+#user "pybossa" do
+#  home "/home/pybossa"
+#  shell "/bin/bash"
+#end
+#
+#directory "/home/pybossa" do
+#  owner "pybossa"
+#  group "pybossa"
+#  mode 0755
+#  action :create
+#end
+#
+#git "Clone" do
+#    repository "git://github.com/PyBossa/pybossa.git"
+#    reference "master"
+#    action :checkout
+#    destination "/home/pybossa/pybossa/"
+#    user "pybossa"
+#    group "pybossa"
+#    enable_submodules true
+#end
 
-directory "/home/pybossa" do
-  owner "pybossa"
-  group "pybossa"
-  mode 0755
-  action :create
-end
-
-git "Clone" do
-    repository "git://github.com/PyBossa/pybossa.git"
-    reference "master"
-    action :checkout
-    destination "/home/pybossa/pybossa/"
-    user "pybossa"
-    group "pybossa"
-    enable_submodules true
-end
-
-python_virtualenv "/home/pybossa/pybossa/env" do
-    owner "pybossa"
-    group "pybossa"
+python_virtualenv "/vagrant/env" do
     action :create
 end
 
 execute "install pybossa requirements" do
-    command ". env/bin/activate; pip install -e /home/pybossa/pybossa"
-    cwd "/home/pybossa/pybossa"
+    command ". env/bin/activate; pip install -e /vagrant"
+    cwd "/vagrant"
     user "pybossa"
 end
 
 execute "setup pybossa DB" do
     command "cp alembic.ini.template alembic.ini"
     user "pybossa"
-    cwd "/home/pybossa/pybossa"
+    cwd "/vagrant"
 end
 
 execute "setup pybossa DB" do
     command "cp settings_local.py.tmpl settings_local.py"
     user "pybossa"
-    cwd "/home/pybossa/pybossa"
+    cwd "/vagrant"
 end
 
 execute "create user db tester" do
@@ -62,5 +61,5 @@ end
 execute "populate pybossa db" do
     command ". env/bin/activate; python cli.py db_create"
     user "pybossa"
-    cwd "/home/pybossa/pybossa"
+    cwd "/vagrant"
 end
