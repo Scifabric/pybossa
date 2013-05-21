89bf067c975b82af8f8d80d3c1a3522b3462f2c8
Daniel Lombraña González <teleyinex@gmail.com>
2013-05-21 10:45:22 +0200

    Add Category table

---
+++ alembic/versions/47dd43c1491_create_category_tabl.py
@@ -0,0 +1,35 @@
+"""create category table
+
+Revision ID: 47dd43c1491
+Revises: 27bf0aefa49d
+Create Date: 2013-05-21 10:41:43.548449
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '47dd43c1491'
+down_revision = '27bf0aefa49d'
+
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy import ForeignKey
+import datetime
+
+
+def make_timestamp():
+    now = datetime.datetime.utcnow()
+    return now.isoformat()
+
+
+def upgrade():
+    op.create_table(
+        'category',
+        sa.Column('id', sa.Integer, primary_key=True),
+        sa.Column('name', sa.Text, nullable=False, unique=True),
+        sa.Column('created', sa.Text, default=make_timestamp),
+        sa.Column('app_id', sa.Integer, ForeignKey('app.id'), unique=True)
+    )
+
+
+def downgrade():
+    op.drop_table('category')
