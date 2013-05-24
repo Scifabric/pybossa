b35c1cf6d6fdd028f74836aa637700f0a23a5bea
Carlos <carlos.val@bifi.es>
2013-05-24 18:35:22 +0200

    Add team auth

--- pybossa/auth/__init__.py
+++ pybossa/auth/__init__.py
@@ -5,6 +5,7 @@ from flaskext.login import current_user
 import app
 import task
 import taskrun
+import team
 
 class Requirement(object):
     """ Checks a function call and raises an exception if the
