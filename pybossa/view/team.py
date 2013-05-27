27205d3048b482c2669eca44c4d7cdc649299637
CarlosVal22 <carlos.val@bifi.es>
2013-05-27 11:36:29 +0200

    Revision require auth

--- pybossa/view/team.py
+++ pybossa/view/team.py
@@ -51,17 +51,15 @@ def team_title(team, page_name):
     return "Team: %s &middot; %s" % (team.name, page_name)
 
 def get_team(name):
-    if current_user.admin == 1:
-       return Team.query.filter_by(name=name).first_or_404()
-    elif current_user.is_anonymous():
+    if current_user.is_anonymous():
        return Team.query.filter_by(name=name, public='t').first_or_404()    
+    elif current_user.admin == 1:
+       return Team.query.filter_by(name=name).first_or_404()
     else:
-	   '''  tiene que encontrar los publicos ''
-	   ''' y a los que pertenezca '''
-       return Team.query.outerjoin(User2Team)\
-		         .filter(Team.name==name)\
-			 .filter(or_ (Team.public=='t', User2Team.user_id==current_user.id))\
-			 .first_or_404()
+       return Team.query.filter(Team.name==name)\
+                      .outerjoin(User2Team)\
+                      .filter(or_ (Team.public =='t', User2Team.user_id == current_user.id))\
+                      .first_or_404()
 
 def user_belong_team(team_id):
    ''' Is a user belong to a team'''
@@ -105,16 +103,15 @@ def get_signed_teams(page=1, per_page=5):
                    created=row.created, description=row.description,
                    owner_id=row.owner_id,
                    owner=row.owner,
-				   public=row.public
+		   public=row.public
                    )       
-       print "Name %s" % row.name
+
        team['rank'], team['score'] = get_rank(row.id)	
        team['members'] =get_number_members(row.id)
        teams.append(team)
 
    return teams, count
 
-# Ok
 class TeamForm(Form):
    ''' Modify Team '''
    id = IntegerField(label=None, widget=HiddenInput())
@@ -131,12 +128,10 @@ class TeamForm(Form):
 
    public = BooleanField(lazy_gettext('Public'))
 
-# Ok
 @blueprint.route('/', defaults={'page': 1})
 @blueprint.route('/page/<int:page>')
 def index(page):
-    """By default show the Public Teams"""
-
+    '''By default show the Public Teams'''
     return team_index(page, cached_teams.get_publics, 'public',
                       True, False, lazy_gettext('Public Teams'))
 
@@ -144,77 +139,68 @@ def index(page):
 @blueprint.route('/signed/page/<int:page>')
 @login_required
 def signed(page):
-    """By show the Signed Teams"""
-
-    #if current_user.is_anonymous():    
-    #   list_teams = cached_teams.get_publics
-    #else:
-
-    list_teams = get_signed_teams
-
-    return team_index(page, list_teams, 'signed',
+    '''By show the Signed Teams'''
+    return team_index(page, get_signed_teams, 'signed',
                       True, False, lazy_gettext('Signed Teams'))
 					  
-# Ok
 def team_index(page, lookup, team_type, fallback, use_count, title):
-    """Show apps of app_type"""
-    if not require.team.read():
-        abort(403)
+   '''Show apps of app_type'''
+   if not require.team.read():
+       abort(403)
   
-    per_page = 5
+   per_page = 5
 
-    teams, count = lookup(page, per_page)
+   teams, count = lookup(page, per_page)
 
-    team_owner = []
-    if not current_user.is_anonymous():
+   team_owner = []
+   if not current_user.is_anonymous():
        team_owner = Team.query.filter(Team.owner_id==current_user.id).first()
    
        for team in teams:
-           team['belong'] = user_belong_team(Team.id)
+           team['belong'] = user_belong_team(team['id'])
     
-    pagination = Pagination(page, per_page, count)
-    template_args = {
+   pagination = Pagination(page, per_page, count)
+   template_args = {
         "teams": teams,
         "team_owner": team_owner,
         "title": title,
         "pagination": pagination,
         "team_type": team_type}
 
-    if use_count:
+   if use_count:
         template_args.update({"count": count})
 
-    return render_template('/team/index.html', **template_args)
+   return render_template('/team/index.html', **template_args)
 
-# Ok
 @blueprint.route('/<name>/')
 def details(name=None):
    ''' Team details '''
    team = get_team(name)
-   
+
+   print "Detail: %s " % require.team.read()
+   if not require.team.read():
+        abort(403)
+  
+   # Get Extra data 
    data = dict( belong = user_belong_team(team.id),
                 members = get_number_members(team.id))
    data['rank'], data['score'] = get_rank(team.id)
    
    try:
        require.team.read(team)
-       #require.team.update(team)
        template = '/team/team.html'
    except HTTPException:
-       #if team.hidden:
-       #    team = None
-       print "exception"
        template = '/team/index.html'
 
    title = team_title(team, None)
    template_args = {
                     "team": team, 
-		            "title": title,
-		            "data": data,
+		    "title": title,
+		    "data": data,
                    }
 
    return render_template(template, **template_args)
 
-# Ok
 @blueprint.route('/<name>/settings')
 @login_required
 def settings(name):
@@ -234,14 +220,17 @@ def settings(name):
 @blueprint.route('/<type>/search', methods=['GET', 'POST'])
 def search_teams(type):
    ''' Search Team '''
+
+   if not require.team.read():
+        abort(403)
+
    form = SearchForm(request.form)
    teams = db.session.query(Team)\
-                .all()
+                     .all()
 
    if request.method == 'POST' and form.user.data:
        query = '%' + form.user.data.lower() + '%'
 
-       print type
        if type == 'public':
            founds = db.session.query(Team)\
                       .filter(func.lower(Team.name).like(query))\
@@ -277,9 +266,10 @@ def search_teams(type):
 def search_users(name):
    team = get_team(name)
 
+   # ???
+
    form = SearchForm(request.form)
-   users = db.session.query(User)\
-                .all()
+   users = db.session.query(User).all()
 
    if request.method == 'POST' and form.user.data:
        query = '%' + form.user.data.lower() + '%'
@@ -320,6 +310,10 @@ class SearchForm(Form):
 @login_required
 def new():
    team = Team.query.filter(Team.owner_id==current_user.id).first()
+
+   if not require.team.create(team):
+       abort(403)
+
    if team:
        flash("<strong>Ooops!</strong> You already ownn your group "
            "<strong>%s</strong>" % team.name)
@@ -351,7 +345,7 @@ def new():
 
    # Insert into the current user in the new group 
    user2team = User2Team( user_id = current_user.id,
-                                 team_id = team.id)
+                          team_id = team.id)
 
    db.session.add(user2team)
    db.session.commit()
@@ -362,6 +356,9 @@ def new():
 @blueprint.route('/<name>/users')
 def users(name):
    team = get_team(name)
+
+   if not require.team.read():
+       abort(403)
 
    # Search users in the team
    belongs = User2Team.query.filter(User2Team.team_id == team.id)\
@@ -376,16 +373,16 @@ def users(name):
 
    return render_template(template, **template_args)
 
-# OK
 @blueprint.route('/<name>/delete', methods=['GET', 'POST'])
 @login_required
 def delete(name):
    ''' Delete the team owner of de current_user '''
    team = get_team(name)
-   title = team_title(team, "Delete")
 
    if not require.team.delete(team):
       abort(403)
+
+   title = team_title(team, "Delete")
 
    if request.method == 'GET':
        return render_template('/team/delete.html',
@@ -399,12 +396,14 @@ def delete(name):
    flash(lazy_gettext('Team deleted!'), 'success')
    return redirect(url_for('team.index'))
 
-# OK
 @blueprint.route('/<name>/update', methods=['GET', 'POST'])
 @login_required
 def update(name):
    ''' Update the team owner of the current user '''
    team = get_team(name)
+
+   if not require.team.update(team):
+       abort(403)
   
    def handle_valid_form(form):
        new_team = Team(id=form.id.data,
@@ -443,6 +442,9 @@ def update(name):
 def user_add(name):
    ''' Add Current User to a team '''
    team = get_team(name)
+
+   if not require.team.read():
+       abort(403)
   
    # Search relationship
    user2team = db.session.query(User2Team)\
@@ -469,6 +471,9 @@ def user_add(name):
 def user_delete(name):
    team = get_team(name)
 
+   if not require.team.read():
+       abort(403)
+
    ''' Check if exits association'''
    user2team = db.session.query(User2Team)\
                    .filter(User2Team.user_id == current_user.id )\
@@ -487,6 +492,9 @@ def user_delete(name):
 def join_add(name,user):
    ''' Add user from a team '''
    team = get_team(name)
+
+   if not require.team.update(team):
+       abort(403)
   
    user = User.query.filter_by(name=user).first()
    if not user:
@@ -518,6 +526,9 @@ def join_add(name,user):
 def join_delete(name, user):
    ''' Delete user from a team '''
    team = get_team(name)
+
+   if not require.team.update(team):
+      abort(403)
 
    user = User.query.filter_by(name=user).first()
    if not user:
