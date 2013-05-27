f257e2bb7394b5d216e1a4596abcc77f681a717f
Carlos <carlos.val@bifi.es>
2013-05-27 07:10:58 +0200

    Auth for teams

--- pybossa/auth/team.py
+++ pybossa/auth/team.py
@@ -7,7 +7,7 @@ def read(team=None):
     return True
 
 def update(team):
-    if not current_user.is_anonymous() and (team.owner == current_user.id
+    if not current_user.is_anonymous() and (team.owner_id == current_user.id
                                             or current_user.admin is True):
         return True
     else:
