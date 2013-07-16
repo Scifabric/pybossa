175b8037bca7aff88010cd60464b21afc9d7f18f
Daniel Lombraña González <teleyinex@gmail.com>
2013-07-16 10:13:46 +0200

    Add Video task template importer

--- pybossa/view/importer.py
+++ pybossa/view/importer.py
@@ -48,6 +48,9 @@ googledocs_urls = {
     'sound': "https://docs.google.com/spreadsheet/ccc"
              "?key=0AsNlt0WgPAHwdEczcWduOXRUb1JUc1VGMmJtc2xXaXc"
              "&usp=sharing",
+    'video': "https://docs.google.com/spreadsheet/ccc"
+             "?key=0AsNlt0WgPAHwdGZ2UGhxSTJjQl9YNVhfUVhGRUdoRWc"
+             "&usp=sharing",
     'map': "https://docs.google.com/spreadsheet/ccc"
            "?key=0AsNlt0WgPAHwdGZnbjdwcnhKRVNlN1dGXy0tTnNWWXc"
            "&usp=sharing",
