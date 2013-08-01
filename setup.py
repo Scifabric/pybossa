c84654d0953cacb730e09042475914408a1a94bf
Daniel Lombraña González <teleyinex@gmail.com>
2013-08-01 13:00:31 +0200

    Fix setup.py. Close #571
    Update the setup.py to use SEMVER. The commit also includes some minor fixes
    in the setup file, to reflect the new status.

--- setup.py
+++ setup.py
@@ -6,26 +6,25 @@ except:
 
 setup(
     name = 'pybossa',
-    version = '0.1a',
+    version = '0.1.0',
     packages = find_packages(),
     install_requires = requirements,
     dependency_links = ['git+https://github.com/Hypernode/M2Crypto#egg=M2Crypto-0.22.dev'],
     # metadata for upload to PyPI
     author = 'Citizen Cyberscience Centre and Open Knowledge Foundation',
     # TODO: change
-    author_email = 'info@okfn.org',
-    description = 'pybossa is a RESTful data store for tabular and table-like data.',
-    long_description = '''PyBossa is a rewrite of Bossa in Python.
-    ''',
-    license = 'MIT',
-    url = 'https://github.com/citizen-cyberscience-centre/pybossa',
+    author_email = 'info@pybossa.com',
+    description = 'Open Source CrowdSourcing framework',
+    long_description = '''PyBossa is an open source crowdsourcing solution for volunteer computing, thinking and sensing ''',
+    license = 'AGPLv3',
+    url = 'https://github.com/PyBossa/pybossa',
     download_url = '',
     include_package_data = True,
     classifiers = [
         'Development Status :: 3 - Alpha',
         'Environment :: Console',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: GNU Affero v3',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Topic :: Software Development :: Libraries :: Python Modules'
