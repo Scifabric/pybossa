1a47af9a2bf54202f17d4b7a9f7aa7f9c16aa79d
Daniel Lombraña González <teleyinex@gmail.com>
2013-11-25 15:23:04 +0100

    Cache built using redis-py
    This is a decorator that caches objects into Redis, using a master-slave
    approach thanks to the sentinel feature of Redis.
    
    Sentinel handles automatically client discovery, and creates master/slave nodes
    for a high-available and load balance cache.

--- pybossa/cache/__init__.py
+++ pybossa/cache/__init__.py
@@ -16,7 +16,43 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with PyBossa.  If not, see <http://www.gnu.org/licenses/>.
 # Cache global variables for timeouts
+from random import choice
+import settings_local as settings
+from redis.sentinel import Sentinel
+from werkzeug.contrib.cache import RedisCache
+from functools import wraps
+try:
+    import cPickle as pickle
+except ImportError: # pragma: no cover
+    import pickle
+
 ONE_DAY = 24 * 60 * 60
 ONE_HOUR = 60 * 60
 HALF_HOUR = 30 * 60
 FIVE_MINUTES = 5 * 60
+
+
+def memoize(timeout=300):
+    def decorator(f):
+        @wraps(f)
+        def wrapper(*args, **kwargs):
+            key = "pybossa_cache:%s" % f.__name__
+            key += "_args"
+            for i in args:
+                key += ":%s" % i
+            #key += "_kwargs"
+            #for i in frozenset(kwargs.items()):
+            #    key += ":%s" % i
+            print key
+            sentinel = Sentinel(settings.REDIS_SENTINEL, socket_timeout=0.1)
+            master = sentinel.master_for('mymaster')
+            slave = sentinel.slave_for('mymaster')
+            output = slave.get(key)
+            if output:
+                return pickle.loads(output)
+            else:
+                output = f(*args, **kwargs)
+                master.setex(key, timeout, pickle.dumps(output))
+                return output
+        return wrapper
+    return decorator
