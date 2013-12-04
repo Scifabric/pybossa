6dc9bb8d6032397821b664030e41c50a8cfd1269
Daniel Lombraña González <teleyinex@gmail.com>
2013-12-04 19:44:28 +0100

    Update cookbooks to run PyBossa with Redis

--- pybossa/core.py
+++ pybossa/core.py
@@ -49,7 +49,7 @@ def create_app():
 
 def configure_app(app):
     app.config.from_object(settings)
-    app.config.from_envvar('PYBOSSA_SETTINGS', silent=False)
+    app.config.from_envvar('PYBOSSA_SETTINGS', silent=True)
     # parent directory
     here = os.path.dirname(os.path.abspath( __file__ ))
     config_path = os.path.join(os.path.dirname(here), 'settings_local.py')
