c521906c5c0f6ac61dd5a76c3a51645b02e69657
Daniel Lombraña González <teleyinex@gmail.com>
2014-01-21 12:26:55 +0100

    Refactor some code

--- test/test_facebook.py
+++ test/test_facebook.py
@@ -15,10 +15,8 @@
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with PyBossa.  If not, see <http://www.gnu.org/licenses/>.
-from base import web, db, model, Fixtures
+from base import web, model, Fixtures
 import pybossa.view.facebook as facebook
-import json
-from mock import patch, Mock
 
 
 class TestFacebook:
