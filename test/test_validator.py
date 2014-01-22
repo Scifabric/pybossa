817f5db50567bb8175695c4d866a1ad87af93b0e
Daniel Lombraña González <teleyinex@gmail.com>
2014-01-22 09:00:08 +0100

    Increase coverage

---
+++ test/test_validator.py
@@ -0,0 +1,51 @@
+# -*- coding: utf8 -*-
+# This file is part of PyBossa.
+#
+# Copyright (C) 2013 SF Isle of Man Limited
+#
+# PyBossa is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# PyBossa is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with PyBossa.  If not, see <http://www.gnu.org/licenses/>.
+from base import web, model, Fixtures, db
+import pybossa.validator
+from pybossa.view.account import LoginForm
+from flaskext.wtf import ValidationError
+from nose.tools import raises
+
+
+class TestValidator:
+    def setUp(self):
+        self.app = web.app
+        model.rebuild_db()
+        Fixtures.create()
+
+    def tearDown(self):
+        db.session.remove()
+
+    @raises(ValidationError)
+    def test_unique(self):
+        """Test VALIDATOR Unique works."""
+        with self.app.test_request_context('/'):
+            f = LoginForm()
+            f.email.data = Fixtures.email_addr
+            u = pybossa.validator.Unique(db.session, model.User,
+                                         model.User.email_addr)
+            u.__call__(f, f.email)
+
+    @raises(ValidationError)
+    def test_not_allowed_chars(self):
+        """Test VALIDATOR NotAllowedChars works."""
+        with self.app.test_request_context('/'):
+            f = LoginForm()
+            f.email.data = Fixtures.email_addr + "$"
+            u = pybossa.validator.NotAllowedChars()
+            u.__call__(f, f.email)
