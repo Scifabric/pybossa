8106916e340bf9c7202f4e406bb32885238e4d84
alejandrodob <alejandrodob@gmail.com>
2014-04-01 09:43:12 +0200

    Added alembic migration script

---
+++ alembic/versions/43c3a523af05_html_to_markdown_in_app_long_description.py
@@ -0,0 +1,43 @@
+"""html to markdown in app long_description
+
+Revision ID: 43c3a523af05
+Revises: 46c3f68e950a
+Create Date: 2014-03-31 15:47:57.151875
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '43c3a523af05'
+down_revision = '46c3f68e950a'
+
+from alembic import op
+import sqlalchemy as sa
+from html2text import html2text
+from flask.ext.misaka import markdown
+
+
+def upgrade():
+    conn = op.get_bind()
+    query = 'SELECT id, long_description FROM "app";'
+    query_result = conn.execute(query)
+    old_descriptions = query_result.fetchall()
+    for old_desc in old_descriptions:
+        print old_desc.long_description
+        new_descritpion = html2text(old_desc.long_description)
+        print new_descritpion.__class__
+        query = ("UPDATE \"app\" SET long_description=\'%s\' WHERE id=%s;"
+                % (new_descritpion, old_desc.id))
+        conn.execute(query)
+
+
+
+def downgrade():
+    conn = op.get_bind()
+    query = 'SELECT id, long_description FROM "app";'
+    query_result = conn.execute(query)
+    old_descriptions = query_result.fetchall()
+    for old_desc in old_descriptions:
+        new_descritpion = markdown(old_desc.long_description)
+        query = ("UPDATE \"app\" SET long_description=\'%s\' WHERE id=%s;"
+                % (new_descritpion, old_desc.id))
+        conn.execute(query)
