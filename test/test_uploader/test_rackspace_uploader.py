1e088c5e7ccfb6ed1564f8a295fd93b72c8fc54c
Daniel Lombraña González <teleyinex@gmail.com>
2014-04-08 14:17:12 +0200

    Unit Tests for Rackspace

--- test/test_uploader/test_rackspace_uploader.py
+++ test/test_uploader/test_rackspace_uploader.py
@@ -19,13 +19,15 @@
 
 from base import web, model, Fixtures, db, redis_flushall
 from pybossa.uploader.rackspace import RackspaceUploader
-from mock import patch
+from mock import patch, PropertyMock
 from werkzeug.datastructures import FileStorage
+from pyrax.fakes import FakeContainer
 
 
 class TestRackspaceUploader:
 
     """Test PyBossa Rackspace Uploader module."""
+
 
     def setUp(self):
         """SetUp method."""
@@ -45,7 +47,8 @@ class TestRackspaceUploader:
         model.rebuild_db()
         redis_flushall()
 
-    @patch('pybossa.uploader.rackspace.pyrax.set_credentials', return_value=True)
+    @patch('pybossa.uploader.rackspace.pyrax.set_credentials',
+           return_value=True)
     def test_rackspace_uploader_init(self, Mock):
         """Test RACKSPACE UPLOADER init works."""
         new_extensions = set(['pdf', 'doe'])
@@ -58,3 +61,58 @@ class TestRackspaceUploader:
             for ext in new_extensions:
                 err_msg = "The .%s extension should be allowed" % ext
                 assert ext in u.allowed_extensions, err_msg
+
+    @patch('pybossa.uploader.rackspace.pyrax.set_credentials',
+           return_value=True)
+    @patch('pybossa.uploader.rackspace.pyrax.utils.get_checksum',
+           return_value="1234abcd")
+    def test_rackspace_uploader_upload_correct_file(self, mock, mock2):
+        """Test RACKSPACE UPLOADER upload file works."""
+        with patch('pybossa.uploader.rackspace.pyrax.cloudfiles.upload_file',
+                   return_value=True):
+            u = RackspaceUploader("username",
+                                  "apikey",
+                                  "ORD")
+            file = FileStorage(filename='test.jpg')
+            err_msg = "Upload file should return True"
+            assert u.upload_file(file) is True, err_msg
+
+    @patch('pybossa.uploader.rackspace.pyrax.set_credentials',
+           return_value=True)
+    @patch('pybossa.uploader.rackspace.pyrax.utils.get_checksum',
+           return_value="1234abcd")
+    def test_rackspace_uploader_upload_wrong_file(self, mock, mock2):
+        """Test RACKSPACE UPLOADER upload wrong file extension works."""
+        with patch('pybossa.uploader.rackspace.pyrax.cloudfiles.upload_file',
+                   return_value=True):
+            u = RackspaceUploader("username",
+                                  "apikey",
+                                  "ORD")
+            file = FileStorage(filename='test.docs')
+            err_msg = "Upload file should return False"
+            res = u.upload_file(file)
+            print res
+            assert res is False, err_msg
+
+    @patch('pybossa.uploader.rackspace.pyrax.set_credentials',
+           return_value=True)
+    def test_rackspace_uploader_lookup_url(self, mock1):
+        """Test RACKSPACE UPLOADER upload wrong file extension works."""
+        uri = 'http://rackspace.com'
+        with patch('pyrax.fakes.FakeContainer.cdn_enabled', new_callable=PropertyMock) as mock_cdn_enabled:
+            mock_cdn_enabled.return_value = True
+            with patch('pyrax.fakes.FakeContainer.cdn_uri', new_callable=PropertyMock) as mock_cdn_uri:
+                mock_cdn_uri.return_value = uri
+                fake_container = FakeContainer('a', 'b', 0, 0)
+                #fake_container.cdn_enabled = True
+                filename = 'test.jpg'
+                with patch('pybossa.uploader.rackspace.pyrax.cloudfiles.get_container',
+                           return_value=fake_container):
+
+                    u = RackspaceUploader("username",
+                                          "apikey",
+                                          "ORD")
+                    res = u._lookup_url('rackspace', {'filename': filename})
+                    expected_url = "%s/%s" % (uri, filename)
+                    err_msg = "We should get the following URL: %s" % expected_url
+                    assert res == expected_url, err_msg
