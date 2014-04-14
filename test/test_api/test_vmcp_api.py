f9446b261e80806adb8e8d99ad5c568fc0e2ecad
Daniel Lombraña González <teleyinex@gmail.com>
2014-04-14 12:11:17 +0200

    Remove db as it is not used.

--- test/test_api/test_vmcp_api.py
+++ test/test_api/test_vmcp_api.py
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with PyBossa.  If not, see <http://www.gnu.org/licenses/>.
 import json
-from default import flask_app, db, with_context
+from default import flask_app, with_context
 from mock import patch
 from test_api import HelperAPI
 
