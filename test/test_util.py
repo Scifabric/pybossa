67e7dae8088b70c9778176729c85f363a9a0c820
Daniel Lombraña González <teleyinex@gmail.com>
2014-04-16 12:10:07 +0200

    Make it more generic

--- test/test_util.py
+++ test/test_util.py
@@ -46,8 +46,9 @@ class TestWebModule(Test):
         err_msg = "CORS should be enabled"
         print res.headers
         assert res.headers['Access-Control-Allow-Origin'] == '*', err_msg
-        methods = 'PUT, HEAD, DELETE, OPTIONS, GET'
-        assert res.headers['Access-Control-Allow-Methods'] == methods, err_msg
+        methods = ['PUT', 'HEAD', 'DELETE', 'OPTIONS', 'GET']
+        for m in methods:
+            assert m in res.headers['Access-Control-Allow-Methods'], err_msg
         assert res.headers['Access-Control-Max-Age'] == '21600', err_msg
         headers = 'CONTENT-TYPE, AUTHORIZATION'
         assert res.headers['Access-Control-Allow-Headers'] == headers, err_msg
