dc03e6a2291e968c7bf952d9af0cf19872d8b74b
alejandrodob <alejandrodob@gmail.com>
2014-04-21 11:52:30 +0200

    Refactor: added setup_db function

--- pybossa/core.py
+++ pybossa/core.py
@@ -53,8 +53,7 @@ def create_app(theme='default'):
     setup_gravatar(app)
     #gravatar = Gravatar(app, size=100, rating='g', default='mm',
                         #force_default=False, force_lower=False)
-    db.app = app
-    db.init_app(app)
+    setup_db(app)
     mail.init_app(app)
     sentinel.init_app(app)
     signer.init_app(app)
@@ -86,6 +85,11 @@ def configure_app(app):
 
 def setup_markdown(app):
     misaka.init_app(app)
+
+
+def setup_db(app):
+    db.app = app
+    db.init_app(app)
 
 
 def setup_gravatar(app):
