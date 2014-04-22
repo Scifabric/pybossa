38f9d09b621137ca918d35e68b6bcbdd6c40f196
Daniel Lombraña González <teleyinex@gmail.com>
2014-04-22 11:08:43 +0200

    Serve uploaded content

---
+++ pybossa/view/uploads.py
@@ -0,0 +1,32 @@
+# -*- coding: utf8 -*-
+# This file is part of PyBossa.
+#
+# Copyright (C) 2014 SF Isle of Man Limited
+#
+# PyBossa is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# PyBossa is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with PyBossa.  If not, see <http://www.gnu.org/licenses/>.
+"""
+PyBossa Uploads view for LocalUploader application.
+
+This module serves uploaded content like avatars.
+
+"""
+from flask import Blueprint, send_from_directory
+from pybossa.core import uploader
+
+
+blueprint = Blueprint('uploads', __name__)
+
+@blueprint.route('/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(uploader.upload_folder, filename)
