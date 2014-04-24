509ef8a1e8ec7c4618e9eae4899260725ab4e5bc
alejandrodob <alejandrodob@gmail.com>
2014-04-24 12:11:05 +0200

    Tests for n_registered_volunteers

--- test/test_cache/test_cache_apps.py
+++ test/test_cache/test_cache_apps.py
@@ -20,6 +20,8 @@ import hashlib
 from default import Test, db, with_context
 from pybossa.model.app import App
 from pybossa.model.task import Task
+from pybossa.model.task_run import TaskRun
+from pybossa.model.user import User
 from pybossa.cache import apps as cached_apps
 
 
@@ -28,15 +30,15 @@ class TestAppsCache(Test):
     @with_context
     def setUp(self):
         super(TestAppsCache, self).setUp()
-        self.root, self.user1, self.user2 = self.create_users()
-        db.session.add_all([self.root, self.user1, self.user2])
+        self.user = self.create_users()[0]
+        db.session.add(self.user)
         db.session.commit()
 
-    def create_app_with_tasks(self, owner, completed_tasks, ongoing_tasks):
+    def create_app_with_tasks(self, completed_tasks, ongoing_tasks):
         app = App(name='my_app',
                   short_name='my_app_shortname',
                   description=u'description')
-        app.owner = owner
+        app.owner = self.user
         db.session.add(app)
         for i in range(completed_tasks):
             task = Task(app_id = 1, state = 'completed', n_answers=3)
@@ -47,12 +49,51 @@ class TestAppsCache(Test):
         db.session.commit()
         return app
 
+    def create_app_with_contributors(self, anonymous, registered, two_tasks=False):
+        app = App(name='my_app',
+                  short_name='my_app_shortname',
+                  description=u'description')
+        app.owner = self.user
+        db.session.add(app)
+        task = Task(app=app)
+        db.session.add(task)
+        if two_tasks:
+            task2 = Task(app=app)
+            db.session.add(task2)
+        for i in range(anonymous):
+            task_run = TaskRun(app_id = 1,
+                               task_id = 1,
+                               user_ip = '127.0.0.%s' % i)
+            db.session.add(task_run)
+            if two_tasks:
+                task_run2 = TaskRun(app_id = 1,
+                               task_id = 2,
+                               user_ip = '127.0.0.%s' % i)
+                db.session.add(task_run2)
+        for i in range(registered):
+            user = User(email_addr = "%s@a.com" % i,
+                        name = "user%s" % i,
+                        passwd_hash = "1234%s" % i,
+                        fullname = "user_fullname%s" % i)
+            db.session.add(user)
+            task_run = TaskRun(app_id = 1,
+                               task_id = 1,
+                               user = user)
+            db.session.add(task_run)
+            if two_tasks:
+                task_run2 = TaskRun(app_id = 1,
+                               task_id = 2,
+                               user = user)
+                db.session.add(task_run2)
+        db.session.commit()
+        return app
+
 
     @with_context
     def test_n_completed_tasks_no_completed_tasks(self):
         """Test CACHE APPS n_completed_tasks returns 0 if no completed tasks"""
 
-        app = self.create_app_with_tasks(self.root, completed_tasks=0, ongoing_tasks=5)
+        app = self.create_app_with_tasks(completed_tasks=0, ongoing_tasks=5)
         completed_tasks = cached_apps.n_completed_tasks(app.id)
 
         err_msg = "Completed tasks is %s, it should be 0" % completed_tasks
@@ -64,7 +105,7 @@ class TestAppsCache(Test):
         """Test CACHE APPS n_completed_tasks returns number of completed tasks
         if there are any"""
 
-        app = self.create_app_with_tasks(self.root, completed_tasks=5, ongoing_tasks=5)
+        app = self.create_app_with_tasks(completed_tasks=5, ongoing_tasks=5)
         completed_tasks = cached_apps.n_completed_tasks(app.id)
 
         err_msg = "Completed tasks is %s, it should be 5" % completed_tasks
@@ -76,21 +117,47 @@ class TestAppsCache(Test):
         """Test CACHE APPS n_completed_tasks returns number of tasks if all
         tasks are completed"""
 
-        app = self.create_app_with_tasks(self.root, completed_tasks=5, ongoing_tasks=0)
+        app = self.create_app_with_tasks(completed_tasks=4, ongoing_tasks=0)
         completed_tasks = cached_apps.n_completed_tasks(app.id)
 
-        err_msg = "Completed tasks is %s, it should be 5" % completed_tasks
-        assert completed_tasks == 5, err_msg
+        err_msg = "Completed tasks is %s, it should be 4" % completed_tasks
+        assert completed_tasks == 4, err_msg
 
 
     @with_context
     def test_n_registered_volunteers(self):
-        """Test CACHE APPS number of registered users contributed to a given app"""
+        """Test CACHE APPS n_registered_volunteers returns number of volunteers
+        that contributed to an app when each only submited one task run"""
+
+        app = self.create_app_with_contributors(anonymous=0, registered=3)
+        registered_volunteers = cached_apps.n_registered_volunteers(app.id)
+
+        err_msg = "Volunteers is %s, it should be 3" % registered_volunteers
+        assert registered_volunteers == 3, err_msg
+
+
+    @with_context
+    def test_n_registered_volunteers_with_more_than_one_taskrun(self):
+        """Test CACHE APPS n_registered_volunteers returns number of volunteers
+        that contributed to an app when any submited more than one task run"""
+
+        app = self.create_app_with_contributors(anonymous=0, registered=2, two_tasks=True)
+        registered_volunteers = cached_apps.n_registered_volunteers(app.id)
+
+        err_msg = "Volunteers is %s, it should be 2" % registered_volunteers
+        assert registered_volunteers == 2, err_msg
 
 
     @with_context
     def test_n_anonymous_volunteers(self):
-        """Test CACHE APPS number of anonymous users contributed to a given app"""
+        """Test CACHE APPS n_anonymous_volunteers returns number of volunteers
+        that contributed to an app when each only submited one task run"""
+
+
+    @with_context
+    def test_n_anonymous_volunteers_with_more_than_one_taskrun(self):
+        """Test CACHE APPS n_anonymous_volunteers returns number of volunteers
+        that contributed to an app when any submited more than one task run"""
 
 
     @with_context
