615ebc7a9bf941fb53bec6f2aa19248a3814c556
Daniel Lombraña González <teleyinex@gmail.com>
2014-04-30 18:30:26 +0200

    Timeouts for anon and registered users

--- pybossa/core.py
+++ pybossa/core.py
@@ -354,3 +354,5 @@ def setup_csrf_protection(app):
 def setup_cache_timeouts(app):
     global timeouts
     timeouts['APP_TIMEOUT'] = app.config['APP_TIMEOUT']
+    timeouts['REGISTERED_USERS_TIMEOUT'] = app.config['REGISTERED_USERS_TIMEOUT']
+    timeouts['ANON_USERS_TIMEOUT'] = app.config['ANON_USERS_TIMEOUT']
