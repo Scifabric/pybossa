fca4f7842896c48e864cc9f35056a24f47cf628f
alejandrodob <alejandrodob@gmail.com>
2014-05-06 18:28:29 +0200

    Use base.py instead of default.py for factory-boy changed tests

--- test/test_api/test_app_api.py
+++ test/test_api/test_app_api.py
@@ -17,8 +17,7 @@
 # along with PyBossa.  If not, see <http://www.gnu.org/licenses/>.
 import json
 from mock import patch
-#from base import web, model, Fixtures, db, redis_flushall
-from default import db, with_context
+from base import db, with_context
 from nose.tools import assert_equal, assert_raises
 from test_api import TestAPI
 from pybossa.model.app import App
@@ -476,7 +475,7 @@ class TestAppAPI(TestAPI):
         user = UserFactory.create()
         tasks = TaskFactory.create_batch(2, app=app, info={'question': 'answer'})
 
-        # All users are allowed to participate by default
+        # All users are allowed to participate by base
         # As Anonymous user
         url = '/api/app/%s/newtask' % app.id
         res = self.app.get(url, follow_redirects=True)
--- test/test_api/test_vmcp_api.py
+++ test/test_api/test_vmcp_api.py
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with PyBossa.  If not, see <http://www.gnu.org/licenses/>.
 import json
-from default import flask_app, with_context
+from base import flask_app, with_context
 from mock import patch
 from test_api import TestAPI
 
