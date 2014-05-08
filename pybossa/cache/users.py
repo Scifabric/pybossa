f2102fcb22f26cf6b4b5fed25295a297256bb8f4
alejandrodob <alejandrodob@gmail.com>
2014-05-08 14:54:49 +0200

    Get user.info in get_users_page too, Use new avatars instead of gravatars for users in /account -- community

--- pybossa/cache/users.py
+++ pybossa/cache/users.py
@@ -196,15 +196,16 @@ def get_total_users():
 def get_users_page(page, per_page=24):
     offset = (page - 1) * per_page
     sql = text('''SELECT "user".id, "user".name, "user".fullname, "user".email_addr,
-               "user".created, COUNT(task_run.id) AS task_runs from task_run, "user"
-               WHERE "user".id=task_run.user_id group by "user".id
+               "user".created, "user".info, COUNT(task_run.id) AS task_runs
+               FROM task_run, "user"
+               WHERE "user".id=task_run.user_id GROUP BY "user".id
                ORDER BY "user".created DESC LIMIT :limit OFFSET :offset''')
     results = db.engine.execute(sql, limit=per_page, offset=offset)
     accounts = []
     for row in results:
         user = dict(id=row.id, name=row.name, fullname=row.fullname,
                     email_addr=row.email_addr, created=row.created,
-                    task_runs=row.task_runs,
+                    task_runs=row.task_runs, info=row.info,
                     registered_ago=pretty_date(row.created))
         accounts.append(user)
     return accounts
