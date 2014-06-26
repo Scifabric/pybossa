2313e1b3ab41754b1787b22202521b2ed384fec1
alejandrodob <alejandrodob@gmail.com>
2014-06-26 12:02:11 +0200

    Added tests for cookies

--- test/test_cookies.py
+++ test/test_cookies.py
@@ -39,7 +39,8 @@ class TestCookieHandler(object):
         mock_response.set_cookie.assert_called_with('my_projectpswd',
                                                     [user], max_age=1200)
 
-    def test_cookie_habdler_updates_cookie(self):
+
+    def test_cookie_handler_updates_cookie(self):
         """Test that a CookieHandler updates the cookie with another user's info"""
         mock_signer = MagicMock()
         mock_signer.loads = lambda x: x
@@ -57,7 +58,31 @@ class TestCookieHandler(object):
                                                     max_age=1200)
 
 
+    def test_cookie_handler_retrieves_cookie_if_exists(self):
+        """Test that a CookieHandler retrieves a cookie form the request if that
+        exists"""
+        mock_signer = MagicMock()
+        mock_signer.loads = lambda x: x
+        mock_request = MagicMock(cookies={'my_projectpswd': ['a_user']})
+        project = MagicMock(short_name='my_project')
+        cookie_handler = CookieHandler(request=mock_request, signer=mock_signer)
+
+        cookie = cookie_handler.get_cookie_from(project)
+
+        assert cookie == ['a_user'], cookie
 
 
+    def test_cookie_handler_empty_list_if_no_cookie(self):
+        """Test that a CookieHandler retrieves an empty list if the cookie for 
+        a project does not exist"""
+        mock_signer = MagicMock()
+        mock_signer.loads = lambda x: x
+        mock_request = MagicMock(cookies={})
+        project = MagicMock(short_name='my_project')
+        cookie_handler = CookieHandler(request=mock_request, signer=mock_signer)
+
+        cookie = cookie_handler.get_cookie_from(project)
+
+        assert cookie == [], cookie
 
 
