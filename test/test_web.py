6c5ae5bad66b71c1663d50bc4dc14446897fd5b5
alejandrodob <alejandrodob@gmail.com>
2014-07-03 13:09:23 +0200

    Revert change in test

--- test/test_web.py
+++ test/test_web.py
@@ -2735,8 +2735,6 @@ class TestWeb(web.Helper):
         assert "Please sign in to access this page" in res.data, err_msg
         res = self.app.post(url, follow_redirects=True)
         assert "Please sign in to access this page" in res.data, err_msg
-        self.register(fullname="newd", name="newd")
-        self.signout()
         # Authenticated user
         self.register()
         user = db.session.query(User).get(1)
