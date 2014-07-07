59ac8005ee6b2964a21f1a658149441be8429a6e
alejandrodob <alejandrodob@gmail.com>
2014-07-07 11:59:45 +0200

    Use repository

--- pybossa/view/facebook.py
+++ pybossa/view/facebook.py
@@ -32,6 +32,9 @@ from pybossa.util import get_user_signup_method
 # are available
 blueprint = Blueprint('facebook', __name__)
 
+from pybossa.repository.user_repository import UserRepository
+user_repo = UserRepository(db)
+
 
 @blueprint.route('/', methods=['GET', 'POST'])
 def login():  # pragma: no cover
@@ -66,9 +69,7 @@ def oauth_authorized(resp):  # pragma: no cover
     user = manage_user(access_token, user_data, next_url)
     if user is None:
         # Give a hint for the user
-        user = db.session.query(User)\
-                 .filter_by(email_addr=user_data['email'])\
-                 .first()
+        user = user_repo.get_by(email_addr=user_data['email'])
         if user is not None:
             msg, method = get_user_signup_method(user)
             flash(msg, 'info')
@@ -102,14 +103,12 @@ def manage_user(access_token, user_data, next_url):
     if user is None:
         facebook_token = dict(oauth_token=access_token)
         info = dict(facebook_token=facebook_token)
-        user = db.session.query(User)\
-                 .filter_by(name=user_data['username']).first()
+        user = user_repo.get_by_name(user_data['username'])
         # NOTE: Sometimes users at Facebook validate their accounts without
         # registering an e-mail (see this http://stackoverflow.com/a/17809808)
         email = None
         if user_data.get('email'):
-            email = db.session.query(User)\
-                      .filter_by(email_addr=user_data['email']).first()
+            email = user_repo.get_by(email_addr=user_data['email'])
 
         if user is None and email is None:
             if not user_data.get('email'):
@@ -119,8 +118,7 @@ def manage_user(access_token, user_data, next_url):
                    email_addr=user_data['email'],
                    facebook_user_id=user_data['id'],
                    info=info)
-            db.session.add(user)
-            db.session.commit()
+            user_repo.save(user)
             return user
         else:
             return None
