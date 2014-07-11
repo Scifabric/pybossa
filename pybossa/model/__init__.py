3f8c3168555c3396789ea8649ecdc426562891b0
Daniel Lombraña González <teleyinex@gmail.com>
2014-07-11 15:01:27 +0200

    Add new mutable system.

--- pybossa/model/__init__.py
+++ pybossa/model/__init__.py
@@ -22,6 +22,7 @@ import uuid
 
 from sqlalchemy import Text
 from sqlalchemy.orm import relationship, backref, class_mapper
+from sqlalchemy.ext.mutable import Mutable
 from sqlalchemy.types import TypeDecorator
 from sqlalchemy import event
 from sqlalchemy.engine import reflection
@@ -95,7 +96,55 @@ class JSONType(TypeDecorator):
         return json.loads(json.dumps(value))
 
 
+class JSONEncodedDict(TypeDecorator):
+    "Represents an immutable structure as a json-encoded string."
 
+    impl = Text
+
+    def process_bind_param(self, value, dialect):
+        if value is not None:
+            value = json.dumps(value)
+        return value
+
+    def process_result_value(self, value, dialect):
+        if value is not None:
+            value = json.loads(value)
+        return value
+
+class MutableDict(Mutable, dict):
+    @classmethod
+    def coerce(cls, key, value):
+        "Convert plain dictionaries to MutableDict."
+
+        if not isinstance(value, MutableDict):
+            if isinstance(value, dict):
+                return MutableDict(value)
+
+            # this call will raise ValueError
+            return Mutable.coerce(key, value)
+        else:
+            return value
+
+    def __setitem__(self, key, value):
+        "Detect dictionary set events and emit change events."
+
+        dict.__setitem__(self, key, value)
+        self.changed()
+
+    def __delitem__(self, key):
+        "Detect dictionary del events and emit change events."
+
+        dict.__delitem__(self, key)
+        self.changed()
+
+    def __getstate__(self):
+        return dict(self)
+
+    def __setstate__(self, state):
+        self.update(state)
+
+MutableDict.associate_with(JSONType)
+MutableDict.associate_with(JSONEncodedDict)
 
 def make_timestamp():
     now = datetime.datetime.utcnow()
