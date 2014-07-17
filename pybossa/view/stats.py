66a5824bee9716abc372c18659baf02f63822334
Daniel Lombraña González <teleyinex@gmail.com>
2014-07-17 09:50:16 +0200

    Use slave session.

--- pybossa/view/stats.py
+++ pybossa/view/stats.py
@@ -139,23 +139,30 @@ def get_top5_apps_24_hours():
 
 @cache(timeout=ONE_DAY, key_prefix="site_top5_users_24_hours")
 def get_top5_users_24_hours():
-    # Top 5 Most active users in last 24 hours
-    sql = text('''SELECT "user".id, "user".fullname, "user".name,
-               COUNT(task_run.app_id) AS n_answers FROM "user", task_run
-               WHERE "user".id=task_run.user_id
-               AND DATE(task_run.finish_time) > NOW() - INTERVAL '24 hour'
-               AND DATE(task_run.finish_time) <= NOW()
-               GROUP BY "user".id
-               ORDER BY n_answers DESC LIMIT 5;''')
+    try:
+        session = get_session(db, bind='slave')
+        # Top 5 Most active users in last 24 hours
+        sql = text('''SELECT "user".id, "user".fullname, "user".name,
+                   COUNT(task_run.app_id) AS n_answers FROM "user", task_run
+                   WHERE "user".id=task_run.user_id
+                   AND DATE(task_run.finish_time) > NOW() - INTERVAL '24 hour'
+                   AND DATE(task_run.finish_time) <= NOW()
+                   GROUP BY "user".id
+                   ORDER BY n_answers DESC LIMIT 5;''')
 
-    results = db.engine.execute(sql, limit=5)
-    top5_users_24_hours = []
-    for row in results:
-        user = dict(id=row.id, fullname=row.fullname,
-                    name=row.name,
-                    n_answers=row.n_answers)
-        top5_users_24_hours.append(user)
-    return top5_users_24_hours
+        results = session.execute(sql, dict(limit=5))
+        top5_users_24_hours = []
+        for row in results:
+            user = dict(id=row.id, fullname=row.fullname,
+                        name=row.name,
+                        n_answers=row.n_answers)
+            top5_users_24_hours.append(user)
+        return top5_users_24_hours
+    except:
+        session.rollback()
+        raise
+    finally:
+        session.close()
 
 
 @cache(timeout=ONE_DAY, key_prefix="site_locs")
