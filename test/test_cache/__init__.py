75304fe83266f01430ea2254a4259907da239526
alejandrodob <alejandrodob@gmail.com>
2014-08-13 11:13:57 +0200

    Added tests for cache decorator

--- test/test_cache/__init__.py
+++ test/test_cache/__init__.py
@@ -17,16 +17,31 @@
 # along with PyBossa.  If not, see <http://www.gnu.org/licenses/>.
 
 import hashlib
-from pybossa.cache import get_key_to_hash, get_hash_key
+from mock import patch
+from pybossa.cache import get_key_to_hash, get_hash_key, cache, memoize
+from pybossa.sentinel import Sentinel
+from settings_test import REDIS_SENTINEL, REDIS_KEYPREFIX
+
+class FakeApp(object):
+    def __init__(self):
+        self.config = { 'REDIS_SENTINEL': REDIS_SENTINEL }
+
+test_sentinel = Sentinel(app=FakeApp())
 
 
 class TestCache(object):
+
+    def setUp(self):
+        test_sentinel.master.flushall()
+
+
     def test_00_get_key_to_hash_with_args(self):
         """Test CACHE get_key_to_hash with args works."""
         expected = ':1:a'
         key_to_hash = get_key_to_hash(1, 'a')
         err_msg = "Different key_to_hash %s != %s" % (key_to_hash, expected)
         assert key_to_hash == expected, err_msg
+
 
     def test_01_get_key_to_hash_with_kwargs(self):
         """Test CACHE get_key_to_hash with kwargs works."""
@@ -35,12 +50,14 @@ class TestCache(object):
         err_msg = "Different key_to_hash %s != %s" % (key_to_hash, expected)
         assert key_to_hash == expected, err_msg
 
+
     def test_02_get_key_to_hash_with_args_and_kwargs(self):
         """Test CACHE get_key_to_hash with args and kwargs works."""
         expected = ':1:a'
         key_to_hash = get_key_to_hash(1, vowel='a')
         err_msg = "Different key_to_hash %s != %s" % (key_to_hash, expected)
         assert key_to_hash == expected, err_msg
+
 
     def test_03_get_hash_key(self):
         """Test CACHE get_hash_key works."""
@@ -51,3 +68,50 @@ class TestCache(object):
         key = get_hash_key(prefix, key_to_hash)
         err_msg = "The expected key is different %s != %s" % (expected, key)
         assert expected == key, err_msg
+
+
+    @patch('pybossa.cache.sentinel', new=test_sentinel)
+    def test_cache_stores_function_call_first_time_its_called(self):
+        """Test CACHE cache decorator stores the result of calling a function
+        in the cache the first time it's called"""
+
+        @cache(key_prefix='my_cached_func')
+        def my_func():
+            return 'my_func was called'
+        my_func()
+        key = "%s::%s" % (REDIS_KEYPREFIX, 'my_cached_func')
+        assert test_sentinel.master.keys() == [key], test_sentinel.master.keys()
+
+
+    @patch('pybossa.cache.sentinel', new=test_sentinel)
+    def test_cache_gets_function_from_cache_second_after_first_call(self):
+        """Test CACHE cache retrieves the function value from cache after it has
+        been called the first time, and does not call the function but once"""
+
+        @cache(key_prefix='my_cached_func')
+        def my_func(call_count = 0):
+            call_count = call_count + 1
+            return call_count
+
+        first_call = my_func()
+        second_call = my_func()
+
+        assert second_call == 1, second_call
+        assert second_call is first_call, second_call
+
+
+    @patch('pybossa.cache.sentinel', new=test_sentinel)
+    def test_cache_gets_function_from_cache_second_after_first_call(self):
+        """Test CACHE cache retrieves the function value from cache after it has
+        been called the first time, and does not call the function but once"""
+
+        @cache(key_prefix='my_cached_func')
+        def my_func(call_count = 0):
+            call_count = call_count + 1
+            return call_count
+
+        first_call = my_func()
+        second_call = my_func()
+
+        assert second_call == 1, second_call
+        assert second_call is first_call, second_call
