5222033b7e85e1b1bac48297e76711268bd2d41e
alejandrodob <alejandrodob@gmail.com>
2014-08-13 11:47:48 +0200

    Refactor: extracted test class with patch decorator

--- test/test_cache/__init__.py
+++ test/test_cache/__init__.py
@@ -22,18 +22,9 @@ from pybossa.cache import get_key_to_hash, get_hash_key, cache, memoize
 from pybossa.sentinel import Sentinel
 from settings_test import REDIS_SENTINEL, REDIS_KEYPREFIX
 
-class FakeApp(object):
-    def __init__(self):
-        self.config = { 'REDIS_SENTINEL': REDIS_SENTINEL }
-
-test_sentinel = Sentinel(app=FakeApp())
 
 
-class TestCache(object):
-
-    def setUp(self):
-        test_sentinel.master.flushall()
-
+class TestCacheHashFunctions(object):
 
     def test_00_get_key_to_hash_with_args(self):
         """Test CACHE get_key_to_hash with args works."""
@@ -70,7 +61,19 @@ class TestCache(object):
         assert expected == key, err_msg
 
 
-    @patch('pybossa.cache.sentinel', new=test_sentinel)
+
+class FakeApp(object):
+    def __init__(self):
+        self.config = { 'REDIS_SENTINEL': REDIS_SENTINEL }
+
+test_sentinel = Sentinel(app=FakeApp())
+
+@patch('pybossa.cache.sentinel', new=test_sentinel)
+class TestCacheMemoizeFunctions(object):
+
+    def setUp(self):
+        test_sentinel.master.flushall()
+
     def test_cache_stores_function_call_first_time_called(self):
         """Test CACHE cache decorator stores the result of calling a function
         in the cache the first time it's called"""
@@ -80,24 +83,10 @@ class TestCache(object):
             return 'my_func was called'
         my_func()
         key = "%s::%s" % (REDIS_KEYPREFIX, 'my_cached_func')
+
         assert test_sentinel.master.keys() == [key], test_sentinel.master.keys()
 
 
-    @patch('pybossa.cache.sentinel', new=test_sentinel)
-    def test_cache_returns_expected_value(self):
-        """Test CACHE cache decorator returns the expected function return value"""
-
-        @cache(key_prefix='my_cached_func')
-        def my_func():
-            return 'my_func was called'
-        first_call = my_func()
-        second_call = my_func()
-
-        assert first_call == 'my_func was called', first_call
-        assert second_call == 'my_func was called', second_call
-
-
-    @patch('pybossa.cache.sentinel', new=test_sentinel)
     def test_cache_gets_function_from_cache_second_after_first_call(self):
         """Test CACHE cache retrieves the function value from cache after it has
         been called the first time, and does not call the function but once"""
@@ -111,3 +100,30 @@ class TestCache(object):
 
         assert second_call == 1, second_call
         assert second_call is first_call, second_call
+
+
+    def test_cache_returns_expected_value(self):
+        """Test CACHE cache decorator returns the expected function return value"""
+
+        @cache(key_prefix='my_cached_func')
+        def my_func():
+            return 'my_func was called'
+        first_call = my_func()
+        second_call = my_func()
+
+        assert first_call == 'my_func was called', first_call
+        assert second_call == 'my_func was called', second_call
+
+
+    def test_memoize_stores_function_call_first_time_called(self):
+        """Test CACHE memoize decorator stores the result of calling a function
+        in the cache the first time it's called"""
+
+        @memoize()
+        def my_func(*args, **kwargs):
+            return {'args': args, 'kwargs': kwargs}
+        my_func()
+        key_pattern = "%s:%s_args:*" % (REDIS_KEYPREFIX, my_func.__name__)
+
+        assert len(test_sentinel.master.keys(key_pattern)) == 1
+
