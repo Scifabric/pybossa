c75f78344aade1da7e08bb1f2b612649e2faf781
alejandrodob <alejandrodob@gmail.com>
2014-08-28 17:19:46 +0200

    Fixed tests and use factories in test stats

--- test/test_stats.py
+++ test/test_stats.py
@@ -18,7 +18,8 @@
 
 import datetime
 import time
-from default import Test, db, with_context
+from factories import AppFactory, TaskFactory, TaskRunFactory, AnonymousTaskRunFactory
+from default import Test, with_context
 from pybossa.model.task_run import TaskRun
 import pybossa.stats as stats
 
@@ -26,8 +27,10 @@ import pybossa.stats as stats
 class TestStats(Test):
     def setUp(self):
         super(TestStats, self).setUp()
-        with self.flask_app.app_context():
-            self.create()
+        self.project = AppFactory.create()
+        for task in TaskFactory.create_batch(4, app=self.project, n_answers=3):
+            TaskRunFactory.create(task=task)
+            AnonymousTaskRunFactory.create(task=task)
 
     # Tests
     # Fixtures will create 10 tasks and will need 10 answers per task, so
@@ -35,53 +38,43 @@ class TestStats(Test):
     # Only 10 task_runs are saved in the DB
 
     def test_01_stats_dates(self):
-        """Test STATS dates method works"""
+        """Test STATS dates method works for dates_anon and dates_auth"""
         today = unicode(datetime.date.today())
-        with self.flask_app.test_request_context('/'):
-            dates, dates_n_tasks, dates_anon, dates_auth = stats.stats_dates(1)
-            err_msg = "There should be 10 answers today"
-            print dates
-            assert dates[today] == 10, err_msg
-            err_msg = "There should be 100 answers per day"
-            print dates_n_tasks
-            assert dates_n_tasks[today] == 100, err_msg
-            err_msg = "The SUM of answers from anon and auth users should be 10"
-            print (dates_anon, dates_auth)
-            assert (dates_anon[today] + dates_auth[today]) == 10, err_msg
+        _, _, dates_anon, dates_auth = stats.stats_dates(self.project.id)
+        err_msg = "The SUM of answers from anon and auth users should be 8"
+        print (dates_anon, dates_auth)
+        assert (dates_anon[today] + dates_auth[today]) == 8, err_msg
 
     def test_02_stats_hours(self):
         """Test STATS hours method works"""
         hour = unicode(datetime.datetime.utcnow().strftime('%H'))
-        with self.flask_app.test_request_context('/'):
-            hours, hours_anon, hours_auth, max_hours,\
-                max_hours_anon, max_hours_auth = stats.stats_hours(1)
-            print hours
-            for i in range(0, 24):
-                # There should be only 10 answers at current hour
-                if str(i).zfill(2) == hour:
-                    err_msg = "At time %s there should be 10 answers" \
-                              "but there are %s" % (str(i).zfill(2),
-                                                    hours[str(i).zfill(2)])
-                    assert hours[str(i).zfill(2)] == 10, "There should be 10 answers"
-                else:
-                    err_msg = "At time %s there should be 0 answers" \
-                              "but there are %s" % (str(i).zfill(2),
-                                                    hours[str(i).zfill(2)])
-                    assert hours[str(i).zfill(2)] == 0, err_msg
+        hours, hours_anon, hours_auth, max_hours,\
+            max_hours_anon, max_hours_auth = stats.stats_hours(self.project.id)
+        print hours
+        for i in range(0, 24):
+            # There should be only 8 answers at current hour
+            if str(i).zfill(2) == hour:
+                err_msg = "At time %s there should be 8 answers" \
+                          "but there are %s" % (str(i).zfill(2),
+                                                hours[str(i).zfill(2)])
+                assert hours[str(i).zfill(2)] == 8, "There should be 8 answers"
+            else:
+                err_msg = "At time %s there should be 0 answers" \
+                          "but there are %s" % (str(i).zfill(2),
+                                                hours[str(i).zfill(2)])
+                assert hours[str(i).zfill(2)] == 0, err_msg
 
-                if str(i).zfill(2) == hour:
-                    tmp = (hours_anon[hour] + hours_auth[hour])
-                    assert tmp == 10, "There should be 10 answers"
-                else:
-                    tmp = (hours_anon[str(i).zfill(2)] + hours_auth[str(i).zfill(2)])
-                    assert tmp == 0, "There should be 0 answers"
-            err_msg = "It should be 10, as all answers are submitted in the same hour"
-            tr = db.session.query(TaskRun).all()
-            for t in tr:
-                print t.finish_time
-            assert max_hours == 10, err_msg
-            assert (max_hours_anon + max_hours_auth) == 10, err_msg
+            if str(i).zfill(2) == hour:
+                tmp = (hours_anon[hour] + hours_auth[hour])
+                assert tmp == 8, "There should be 8 answers"
+            else:
+                tmp = (hours_anon[str(i).zfill(2)] + hours_auth[str(i).zfill(2)])
+                assert tmp == 0, "There should be 0 answers"
+        err_msg = "It should be 8, as all answers are submitted in the same hour"
+        assert max_hours == 8, err_msg
+        assert (max_hours_anon + max_hours_auth) == 8, err_msg
 
+    @with_context
     def test_03_stats(self):
         """Test STATS stats method works"""
         today = unicode(datetime.date.today())
@@ -89,67 +82,68 @@ class TestStats(Test):
         date_ms = time.mktime(time.strptime(today, "%Y-%m-%d")) * 1000
         anon = 0
         auth = 0
-        with self.flask_app.test_request_context('/'):
-            dates_stats, hours_stats, user_stats = stats.get_stats(1)
-            for item in dates_stats:
-                if item['label'] == 'Anon + Auth':
-                    assert item['values'][0][0] == date_ms, item['values'][0][0]
-                    assert item['values'][0][1] == 10, "There should be 10 answers"
-                if item['label'] == 'Anonymous':
-                    assert item['values'][0][0] == date_ms, item['values'][0][0]
-                    anon = item['values'][0][1]
-                if item['label'] == 'Authenticated':
-                    assert item['values'][0][0] == date_ms, item['values'][0][0]
-                    auth = item['values'][0][1]
-                if item['label'] == 'Total':
-                    assert item['values'][0][0] == date_ms, item['values'][0][0]
-                    assert item['values'][0][1] == 10, "There should be 10 answers"
-                if item['label'] == 'Expected Answers':
-                    assert item['values'][0][0] == date_ms, item['values'][0][0]
-                    for i in item['values']:
-                        assert i[1] == 100, "Each date should have 100 answers"
-                    assert item['values'][0][1] == 100, "There should be 10 answers"
-                if item['label'] == 'Estimation':
-                    assert item['values'][0][0] == date_ms, item['values'][0][0]
-                    v = 10
-                    for i in item['values']:
-                        assert i[1] == v, "Each date should have 10 extra answers"
-                        v = v + 10
-            assert auth + anon == 10, "date stats sum of auth and anon should be 10"
+        TaskRunFactory.create(task=self.project.tasks[0])
+        TaskRunFactory.create(task=self.project.tasks[1])
+        dates_stats, hours_stats, user_stats = stats.get_stats(self.project.id)
+        print dates_stats
+        for item in dates_stats:
+            if item['label'] == 'Anon + Auth':
+                assert item['values'][0][0] == date_ms, item['values'][0][0]
+                assert item['values'][0][1] == 10, "There should be 10 answers"
+            if item['label'] == 'Anonymous':
+                assert item['values'][0][0] == date_ms, item['values'][0][0]
+                anon = item['values'][0][1]
+            if item['label'] == 'Authenticated':
+                assert item['values'][0][0] == date_ms, item['values'][0][0]
+                auth = item['values'][0][1]
+            if item['label'] == 'Total Tasks':
+                assert item['values'][0][0] == date_ms, item['values'][0][0]
+                assert item['values'][0][1] == 4, "There should be 4 tasks"
+            if item['label'] == 'Expected Answers':
+                assert item['values'][0][0] == date_ms, item['values'][0][0]
+                for i in item['values']:
+                    assert i[1] == 100, "Each date should have 100 answers"
+                assert item['values'][0][1] == 100, "There should be 10 answers"
+            if item['label'] == 'Estimation':
+                assert item['values'][0][0] == date_ms, item['values'][0][0]
+                v = 2
+                for i in item['values']:
+                    assert i[1] == v, "Each date should have 2 extra answers"
+                    v = v + 2
+        assert auth + anon == 10, "date stats sum of auth and anon should be 10"
 
-            max_hours = 0
-            for item in hours_stats:
-                if item['label'] == 'Anon + Auth':
-                    max_hours = item['max']
-                    print item
-                    assert item['max'] == 10, item['max']
-                    assert item['max'] == 10, "Max hours value should be 10"
-                    for i in item['values']:
-                        if i[0] == hour:
-                            assert i[1] == 10, "There should be 10 answers"
-                            assert i[2] == 5, "The size of the bubble should be 5"
-                        else:
-                            assert i[1] == 0, "There should be 0 answers"
-                            assert i[2] == 0, "The size of the buggle should be 0"
-                if item['label'] == 'Anonymous':
-                    anon = item['max']
-                    for i in item['values']:
-                        if i[0] == hour:
-                            assert i[1] == anon, "There should be anon answers"
-                            assert i[2] == (anon * 5) / max_hours, "The size of the bubble should be 5"
-                        else:
-                            assert i[1] == 0, "There should be 0 answers"
-                            assert i[2] == 0, "The size of the buggle should be 0"
-                if item['label'] == 'Authenticated':
-                    auth = item['max']
-                    for i in item['values']:
-                        if i[0] == hour:
-                            assert i[1] == auth, "There should be anon answers"
-                            assert i[2] == (auth * 5) / max_hours, "The size of the bubble should be 5"
-                        else:
-                            assert i[1] == 0, "There should be 0 answers"
-                            assert i[2] == 0, "The size of the buggle should be 0"
-            assert auth + anon == 10, "date stats sum of auth and anon should be 10"
+        max_hours = 0
+        for item in hours_stats:
+            if item['label'] == 'Anon + Auth':
+                max_hours = item['max']
+                assert item['max'] == 10, item['max']
+                assert item['max'] == 10, "Max hours value should be 10"
+                for i in item['values']:
+                    if i[0] == hour:
+                        assert i[1] == 10, "There should be 10 answers"
+                        assert i[2] == 5, "The size of the bubble should be 5"
+                    else:
+                        assert i[1] == 0, "There should be 0 answers"
+                        assert i[2] == 0, "The size of the buggle should be 0"
+            if item['label'] == 'Anonymous':
+                anon = item['max']
+                for i in item['values']:
+                    if i[0] == hour:
+                        assert i[1] == anon, "There should be anon answers"
+                        assert i[2] == (anon * 5) / max_hours, "The size of the bubble should be 5"
+                    else:
+                        assert i[1] == 0, "There should be 0 answers"
+                        assert i[2] == 0, "The size of the buggle should be 0"
+            if item['label'] == 'Authenticated':
+                auth = item['max']
+                for i in item['values']:
+                    if i[0] == hour:
+                        assert i[1] == auth, "There should be anon answers"
+                        assert i[2] == (auth * 5) / max_hours, "The size of the bubble should be 5"
+                    else:
+                        assert i[1] == 0, "There should be 0 answers"
+                        assert i[2] == 0, "The size of the buggle should be 0"
+        assert auth + anon == 10, "date stats sum of auth and anon should be 8"
 
-            err_msg = "date stats sum of auth and anon should be 10"
-            assert user_stats['n_anon'] + user_stats['n_auth'], err_msg
+        err_msg = "user stats sum of auth and anon should be 7"
+        assert user_stats['n_anon'] + user_stats['n_auth'] == 7, err_msg
