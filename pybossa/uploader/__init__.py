aaab2fd85cbd586d438398c4245ab9cb26ff9243
Daniel Lombraña González <teleyinex@gmail.com>
2014-09-11 16:36:38 +0200

    Resize forces the with,height of the image.

--- pybossa/uploader/__init__.py
+++ pybossa/uploader/__init__.py
@@ -73,7 +73,7 @@ class Uploader(object):
             m = BytesIO()
             im = Image.open(file)
             target = im.crop(coordinates)
-            target.thumbnail(self.size, Image.ANTIALIAS)
+            target.resize(self.size, Image.ANTIALIAS)
             # Scale down the image to Indexed mode
             scale_down_img = target.convert('P', colors=255, palette=Image.ADAPTIVE)
             scale_down_img.save(m, format=extension)
