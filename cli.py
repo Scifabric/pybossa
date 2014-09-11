50862ca53624820512deefec016e0d67f1f8ceab
Daniel Lombraña González <teleyinex@gmail.com>
2014-09-11 16:44:41 +0200

    Script to resize user avatars.

--- cli.py
+++ cli.py
@@ -244,6 +244,113 @@ def bootstrap_avatars():
                             print "Something failed, this project will use the placehoder."
 
 
+def resize_avatars():
+    """Resize avatars to 512px."""
+    if app.config['UPLOAD_METHOD'] == 'rackspace':
+        import pyrax
+        import tempfile
+        import requests
+        from PIL import Image
+        import time
+        pyrax.set_setting("identity_type", "rackspace")
+        pyrax.set_credentials(username=app.config['RACKSPACE_USERNAME'],
+                              api_key=app.config['RACKSPACE_API_KEY'],
+                              region=app.config['RACKSPACE_REGION'])
+
+        cf = pyrax.cloudfiles
+        #users = User.query.all()
+        #users = [User.query.get(5305)]
+        #users = [User.query.get(5662)]
+        users = [User.query.get(3)]
+        print "Downloading avatars for %s users" % len(users)
+        dirpath = tempfile.mkdtemp()
+        for u in users:
+            try:
+                cont = cf.get_container(u.info['container'])
+                avatar_url = "%s/%s" % (cont.cdn_uri, u.info['avatar'])
+                r = requests.get(avatar_url, stream=True)
+                if r.status_code == 200:
+                    print "Downloading avatar for %s ..." % u.name
+                    #container = "user_%s" % u.id
+                    #try:
+                    #    cf.get_container(container)
+                    #except pyrax.exceptions.NoSuchContainer:
+                    #    cf.create_container(container)
+                    #    cf.make_container_public(container)
+                    prefix = time.time()
+                    filename = "%s_avatar.png" % prefix
+                    with open(os.path.join(dirpath, filename), 'wb') as f:
+                        for chunk in r.iter_content(1024):
+                            f.write(chunk)
+                    # Resize image
+                    im = Image.open(os.path.join(dirpath, filename))
+                    size = 512, 512
+                    tmp = im.resize(size, Image.ANTIALIAS)
+                    scale_down_img = tmp.convert('P', colors=255, palette=Image.ADAPTIVE)
+                    scale_down_img.save(os.path.join(dirpath, filename), format='png')
+
+                    print "New scaled down image created!"
+                    print "%s" % (os.path.join(dirpath, filename))
+                    print "---"
+
+                    chksum = pyrax.utils.get_checksum(os.path.join(dirpath,
+                                                                   filename))
+                    cf.upload_file(container,
+                                   os.path.join(dirpath, filename),
+                                   obj_name=filename,
+                                   etag=chksum)
+                    # delete old avatar
+                    obj = cont.get_object(u.info['avatar'])
+                    obj.delete()
+                    # Update new values
+                    u.info['avatar'] = filename
+                    u.info['container'] = container
+                    db.session.commit()
+                    print "Done!"
+                else:
+                    print "No Avatar found."
+            except:
+                raise
+                print "No Avatar, this user will use the placehoder."
+
+
+        #apps = App.query.all()
+        #print "Downloading avatars for %s projects" % len(apps)
+        #for a in apps:
+        #    if a.info.get('thumbnail') and not a.info.get('container'):
+        #        print "Working on project: %s ..." % a.short_name
+        #        print "Saving avatar: %s ..." % a.info.get('thumbnail')
+        #        url = urlparse(a.info.get('thumbnail'))
+        #        if url.scheme and url.netloc:
+        #            container = "user_%s" % a.owner_id
+        #            try:
+        #                cf.get_container(container)
+        #            except pyrax.exceptions.NoSuchContainer:
+        #                cf.create_container(container)
+        #                cf.make_container_public(container)
+
+        #            try:
+        #                r = requests.get(a.info.get('thumbnail'), stream=True)
+        #                if r.status_code == 200:
+        #                    prefix = time.time()
+        #                    filename = "app_%s_thumbnail_%i.png" % (a.id, prefix)
+        #                    with open(os.path.join(dirpath, filename), 'wb') as f:
+        #                        for chunk in r.iter_content(1024):
+        #                            f.write(chunk)
+        #                    chksum = pyrax.utils.get_checksum(os.path.join(dirpath,
+        #                                                                   filename))
+        #                    cf.upload_file(container,
+        #                                   os.path.join(dirpath, filename),
+        #                                   obj_name=filename,
+        #                                   etag=chksum)
+        #                    a.info['thumbnail'] = filename
+        #                    a.info['container'] = container
+        #                    db.session.commit()
+        #                    print "Done!"
+        #            except:
+        #                print "Something failed, this project will use the placehoder."
+
+
 
 
 ## ==================================================
