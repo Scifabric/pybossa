d74e49ef7fe1bded33c8e949cad8cd24561089d9
alejandrodob <alejandrodob@gmail.com>
2014-09-26 13:13:29 +0200

    Fixed migration

--- alembic/versions/29353a1877ba_remove_featured_table.py
+++ alembic/versions/29353a1877ba_remove_featured_table.py
@@ -12,10 +12,21 @@ down_revision = '537db2979434'
 
 from alembic import op
 import sqlalchemy as sa
+import datetime
+
+
+def make_timestamp():
+    now = datetime.datetime.utcnow()
+    return now.isoformat()
 
 
 def upgrade():
-    op.add_column('app', sa.Column('featured', sa.Boolean, nullable=False, default=False))
+    # First, create column and fill all records with default value.
+    # To avoid integrity error, the constraint non-nullable will be set after that
+    op.add_column('app', sa.Column('featured', sa.Boolean, default=False))
+    query = 'UPDATE "app" SET featured=false;'
+    op.execute(query)
+    op.alter_column('app', 'featured', nullable=False)
     query = 'UPDATE "app" SET featured=true WHERE app.id IN (SELECT app_id FROM FEATURED);'
     op.execute(query)
     op.drop_table('featured')
@@ -26,8 +37,8 @@ def downgrade():
         'featured',
         sa.Column('id', sa.Integer, primary_key=True),
         sa.Column('created', sa.Text, default=make_timestamp),
-        sa.Column('app_id', sa.Integer, ForeignKey('app.id'), unique=True)
+        sa.Column('app_id', sa.Integer, sa.ForeignKey('app.id'), unique=True)
     )
-    query = 'INSERT INTO "featured" (app_id) VALUES (SELECT id FROM "app" WHERE featured=true);'
+    query = 'INSERT INTO "featured" (app_id) SELECT id FROM "app" WHERE featured=true;'
     op.execute(query)
     op.drop_column('app', 'featured')
