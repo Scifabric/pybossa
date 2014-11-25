e618764e86d530b961193a615c47b25a49fac927
alejandrodob <alejandrodob@gmail.com>
2014-11-25 11:44:14 +0100

    Fix bug

--- pybossa/sentinel/__init__.py
+++ pybossa/sentinel/__init__.py
@@ -11,6 +11,6 @@ class Sentinel(object):
     def init_app(self, app):
         self.connection = sentinel.Sentinel(app.config['REDIS_SENTINEL'],
                                                   socket_timeout=0.1)
-        redis_db = app.config['REDIS_DB'] or 0
+        redis_db = app.config.get('REDIS_DB') or 0
         self.master = self.connection.master_for('mymaster', db=redis_db)
         self.slave = self.connection.slave_for('mymaster', db=redis_db)
