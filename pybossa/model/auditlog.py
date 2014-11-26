28c9c50b44d1b9c140d0b2e8b6b67da85253e226
Daniel Lombraña González <teleyinex@gmail.com>
2014-11-26 13:39:21 +0100

    Auditlog model.

---
+++ pybossa/model/auditlog.py
@@ -0,0 +1,41 @@
+# -*- coding: utf8 -*-
+# This file is part of PyBossa.
+#
+# Copyright (C) 2013 SF Isle of Man Limited
+#
+# PyBossa is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# PyBossa is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with PyBossa.  If not, see <http://www.gnu.org/licenses/>.
+
+from sqlalchemy import Integer, Text
+from sqlalchemy.schema import Column, ForeignKey
+
+from pybossa.core import db
+from pybossa.model import DomainObject, make_timestamp
+
+
+
+class Auditlog(db.Model, DomainObject):
+    '''A Table with Audit logs for Projects.'''
+
+    __tablename__ = 'auditlog'
+
+    #: Audit log ID
+    id = Column(Integer, primary_key=True)
+    #: App.id
+    app_id = Column(Integer, ForeignKey('app.id'))
+    #: Short name of the project
+    app_short_name = Column(Text, nullable=False)
+    #: UTC timestamp when the Category was created
+    created = Column(Text, default=make_timestamp)
+    #: Log message
+    log = Column(Text, nullable=False)
