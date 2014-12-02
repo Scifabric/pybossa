6d72fed97661b9d658c90499ae5b5821e587f62a
Daniel Lombraña González <teleyinex@gmail.com>
2014-12-02 10:53:42 +0100

    Tests

--- test/test_auditlog.py
+++ test/test_auditlog.py
@@ -227,3 +227,33 @@ class TestAuditlogWEB(web.Helper):
             assert log.action == 'update', log.action
             assert log.user_name == 'johndoe', log.user_name
             assert log.user_id == 1, log.user_id
+
+    @with_context
+    def test_app_description(self):
+        self.register()
+        self.new_application()
+        short_name = 'sampleapp'
+
+        url = "/app/%s/update" % short_name
+
+        attribute = 'description'
+
+        new_string = 'New Something'
+
+        old_value = self.data[attribute]
+
+        self.data['description'] = new_string
+
+        self.app.post(url, data=self.data, follow_redirects=True)
+
+        logs = auditlog_repo.filter_by(app_short_name=short_name)
+        assert len(logs) == 1, logs
+        for log in logs:
+            assert log.attribute == attribute, log.attribute
+            assert log.old_value == old_value, log.old_value
+            assert log.new_value == self.data[attribute], log.new_value
+            assert log.caller == 'web', log.caller
+            assert log.action == 'update', log.action
+            assert log.user_name == 'johndoe', log.user_name
+            assert log.user_id == 1, log.user_id
+
