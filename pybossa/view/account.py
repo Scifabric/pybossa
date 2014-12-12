488f3fcdfd9d5ba22fcc44c604ad5db8e6b12ff9
Daniel Lombraña González <teleyinex@gmail.com>
2014-12-12 10:13:18 +0100

    Fixes.

--- pybossa/view/account.py
+++ pybossa/view/account.py
@@ -211,14 +211,14 @@ def newsletter_subscribe():
 
     """
     # Save that we've prompted the user to sign up in the newsletter
-    if newsletter.app:
+    if newsletter.app and current_user.is_authenticated():
         next_url = request.args.get('next') or url_for('home.home')
+        user = user_repo.get(current_user.id)
         if current_user.newsletter_prompted is False:
-            user = user_repo.get(current_user.id)
             user.newsletter_prompted = True
             user_repo.update(user)
         if request.args.get('subscribe') == 'True':
-            newsletter.subscribe_user(current_user)
+            newsletter.subscribe_user(user)
             flash("You are subscribed to our newsletter!")
             return redirect(next_url)
         elif request.args.get('subscribe') == 'False':
