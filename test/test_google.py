b9260a25591fadd26f91422c900575aee56f269a
Daniel Lombraña González <teleyinex@gmail.com>
2014-12-12 10:51:05 +0100

    Test.

--- test/test_google.py
+++ test/test_google.py
@@ -17,6 +17,8 @@
 # along with PyBossa.  If not, see <http://www.gnu.org/licenses/>.
 from default import Test
 from pybossa.view.google import manage_user
+from mock import patch
+from factories import UserFactory
 
 
 class TestGoogle(Test):
@@ -47,3 +49,33 @@ class TestGoogle(Test):
         user = manage_user(token, user_data, None)
         err_msg = "User should be the same"
         assert user.google_user_id == '10', err_msg
+
+    @patch('pybossa.view.google.newsletter', autospec=True)
+    def test_manage_user_with_newsletter(self, newsletter):
+        """Test GOOGLE manage_user with newsletter works."""
+        # First with a new user
+        newsletter.app = True
+        user_data = dict(id='1', name='google',
+                         email='g@g.com')
+        token = 't'
+        user = manage_user(token, user_data, None)
+        assert user.email_addr == user_data['email'], user
+        assert user.name == user_data['name'], user
+        assert user.fullname == user_data['name'], user
+        assert user.google_user_id == user_data['id'], user
+        newsletter.subscribe_user.assert_called_once_with(user)
+
+    @patch('pybossa.view.google.newsletter', autospec=True)
+    def test_manage_user_with_newsletter_only_once(self, newsletter):
+        """Test GOOGLE manage_user with newsletter only once works."""
+        # Second with the same user
+        token = 't'
+        user = UserFactory.create(fullname='john', name='john',
+                                  google_user_id='1')
+        user_data = dict(id=str(user.id), name=user.name, email=user.email_addr)
+        r_user = manage_user(token, user_data, None)
+        assert r_user.email_addr == user_data['email'], user
+        assert r_user.name == user_data['name'], user
+        assert r_user.fullname == user_data['name'], user
+        assert r_user.google_user_id == user_data['id'], user
+        assert newsletter.subscribe_user.called is False, newsletter.subscribe_user.called
