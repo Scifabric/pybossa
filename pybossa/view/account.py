bb10067aa23a40958287ebc51159935c82af0dae
Daniel Lombraña González <teleyinex@gmail.com>
2014-12-12 08:56:43 +0100

    Show a 404 if newsletter is not configured.

--- pybossa/view/account.py
+++ pybossa/view/account.py
@@ -211,21 +211,24 @@ def newsletter_subscribe():
 
     """
     # Save that we've prompted the user to sign up in the newsletter
-    next_url = request.args.get('next') or url_for('home.home')
-    if current_user.newsletter_prompted is False:
-        user = user_repo.get(current_user.id)
-        user.newsletter_prompted = True
-        user_repo.update(user)
-    if request.args.get('subscribe') == 'True':
-        newsletter.subscribe_user(current_user)
-        flash("You are subscribed to our newsletter!")
-        return redirect(next_url)
-    elif request.args.get('subscribe') == 'False':
-        return redirect(next_url)
+    if newsletter.app:
+        next_url = request.args.get('next') or url_for('home.home')
+        if current_user.newsletter_prompted is False:
+            user = user_repo.get(current_user.id)
+            user.newsletter_prompted = True
+            user_repo.update(user)
+        if request.args.get('subscribe') == 'True':
+            newsletter.subscribe_user(current_user)
+            flash("You are subscribed to our newsletter!")
+            return redirect(next_url)
+        elif request.args.get('subscribe') == 'False':
+            return redirect(next_url)
+        else:
+            return render_template('account/newsletter.html',
+                                   title=gettext("Subscribe to our Newsletter"),
+                                   next=next_url)
     else:
-        return render_template('account/newsletter.html',
-                               title=gettext("Subscribe to our Newsletter"),
-                               next=next_url)
+        return abort(404)
 
 
 
