7e099bada533c163a5073ca53a34768e10a003db
alejandrodob <alejandrodob@gmail.com>
2014-12-18 15:46:08 +0100

    Remove test file commited by mistake

--- test/test_mofa.py
+++
@@ -1,9 +0,0 @@
-from factories import AppFactory
-from pybossa.core import db
-from default import Test
-
-class TestMofa(Test):
-    def test_one(self):
-        app = AppFactory.build()
-        print app in db.session
-        assert False
