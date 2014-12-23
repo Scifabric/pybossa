8d1c5c3ca0c0262f88ddd0fdbd24299c24fc5bf9
alejandrodob <alejandrodob@gmail.com>
2014-12-23 17:26:43 +0100

    Remove unused imports

--- pybossa/auditlogger.py
+++ pybossa/auditlogger.py
@@ -16,8 +16,6 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with PyBossa.  If not, see <http://www.gnu.org/licenses/>.
 
-import json
-from flask import request
 from sqlalchemy.orm.attributes import get_history
 from sqlalchemy.exc import IntegrityError
 
