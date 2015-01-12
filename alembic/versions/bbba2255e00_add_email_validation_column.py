d663e6e71f298b5045fb01b405aa4c93feec10fb
Daniel Lombraña González <teleyinex@gmail.com>
2015-01-12 14:12:14 +0100

    Include a columnt to know if an email has been sent.

--- alembic/versions/bbba2255e00_add_email_validation_column.py
+++ alembic/versions/bbba2255e00_add_email_validation_column.py
@@ -16,9 +16,13 @@ import sqlalchemy as sa
 
 def upgrade():
     op.add_column('user', sa.Column('valid_email', sa.Boolean, default=False))
+    op.add_column('user', sa.Column('confirmation_email_sent', sa.Boolean, default=False))
     query = 'UPDATE "user" SET valid_email=false;'
+    op.execute(query)
+    query = 'UPDATE "user" SET confirmation_email_sent=false;'
     op.execute(query)
 
 
 def downgrade():
     op.drop_column('user', 'valid_email')
+    op.drop_column('user', 'confirmation_email_sent')
