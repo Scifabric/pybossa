f5cf145cdf9dbb1d90f5db62f6c39f36f98582fe
alejandrodob <alejandrodob@gmail.com>
2015-01-15 18:17:16 +0100

    Tests for flickr oauth view functions

--- test/test_flickr_oauth.py
+++ test/test_flickr_oauth.py
@@ -16,10 +16,35 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with PyBossa.  If not, see <http://www.gnu.org/licenses/>.
 
-from default import flask_app
-
+from default import flask_app#, with_context
+from mock import patch
+from flask import Response, session
 
 class TestFlickrOauth(object):
 
     def setUp(self):
         self.app = flask_app.test_client()
+
+
+    @patch('pybossa.view.flickr.flickr')
+    def test_flickr_login_specifies_callback(self, flickr_oauth):
+        flickr_oauth.oauth.authorize.return_value = Response(302)
+        self.app.get('/flickr/')
+        flickr_oauth.oauth.authorize.assert_called_with(callback='/flickr/oauth-authorized')
+
+
+    def test_flickr_get_flickr_token_returns_None_if_no_token(self):
+        from pybossa.view.flickr import get_flickr_token
+        with flask_app.test_request_context():
+            token = get_flickr_token()
+
+        assert token is None, token
+
+
+    def test_flickr_get_flickr_token_returns_existing_token(self):
+        from pybossa.view.flickr import get_flickr_token
+        with flask_app.test_request_context():
+            session['flickr_token'] = 'fake_token'
+            token = get_flickr_token()
+
+        assert token is 'fake_token', token
