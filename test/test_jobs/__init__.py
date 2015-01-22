eba0f9db32b8d852773d1ce8dd67a1d42e494324
Daniel Lombraña González <teleyinex@gmail.com>
2015-01-22 14:33:52 +0100

    Test schedule priority with same queue name.

--- test/test_jobs/__init__.py
+++ test/test_jobs/__init__.py
@@ -15,10 +15,14 @@
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with PyBossa.  If not, see <http://www.gnu.org/licenses/>.
-from pybossa.jobs import create_dict_jobs
+from pybossa.jobs import create_dict_jobs, schedule_priority_jobs
 from default import Test, with_context
+from mock import patch
 
 class TestJobs(Test):
+
+    jobs =[dict(name='name', args=[], kwargs={},
+                timeout=10, queue='low')]
 
     @with_context
     def test_create_dict_jobs(self):
@@ -27,3 +31,13 @@ class TestJobs(Test):
         jobs = create_dict_jobs(data, 'function')
         assert len(jobs) == 1
         assert jobs[0]['name'] == 'function'
+
+    @with_context
+    @patch('pybossa.jobs.get_scheduled_jobs')
+    def test_schedule_priority_jobs_same_queue_name(self, get_scheduled_jobs):
+        """Test JOB schedule_priority_jobs same queue works."""
+        get_scheduled_jobs.return_value = self.jobs
+        queue_name = 'low'
+        res = schedule_priority_jobs(queue_name, 10)
+        msg = "%s jobs in %s have been enqueued" % (len(self.jobs), queue_name)
+        assert res == msg, res
