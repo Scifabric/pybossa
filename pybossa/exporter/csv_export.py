3165c950918e6d6d0471d1a82086a72f2c86b222
Marvin Reimer <therealmarv@gmail.com>
2015-02-12 16:31:13 +0100

    remove unused lib

--- pybossa/exporter/csv_export.py
+++ pybossa/exporter/csv_export.py
@@ -22,7 +22,6 @@ CSV Exporter module for exporting tasks and tasks results out of PyBossa
 
 from pybossa.exporter import Exporter
 import tempfile
-from StringIO import StringIO
 from pybossa.core import uploader, task_repo
 from pybossa.model.task import Task
 from pybossa.model.task_run import TaskRun
