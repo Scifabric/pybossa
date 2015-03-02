0dba69f2730d1042d6e50db428576e71e11bb2ae
Daniel Lombraña González <teleyinex@gmail.com>
2015-03-02 14:46:07 +0100

    PEP8

--- pybossa/cache/helpers.py
+++ pybossa/cache/helpers.py
@@ -46,7 +46,8 @@ def n_available_tasks(project_id, user_id=None, user_ip=None):
             user_ip = '127.0.0.1'
         query = text('''SELECT COUNT(id) AS n_tasks FROM task WHERE NOT EXISTS
                        (SELECT task_id FROM task_run WHERE
-                       app_id=:project_id AND user_ip=:user_ip AND task_id=task.id)
+                       app_id=:project_id AND user_ip=:user_ip
+                       AND task_id=task.id)
                        AND app_id=:project_id AND state !='completed';''')
 
         result = session.execute(query, dict(project_id=project_id,
