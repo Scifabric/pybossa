7ae97bfaa638c93b1b5dceaf95f73c480c1eaedb
alejandrodob <alejandrodob@gmail.com>
2015-04-14 14:31:55 +0200

    Update redis-py to v2.10.0

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ requirements = [
     "six>=1.9.0, <2.0.0",
     "nose",
     "rednose",
-    "redis>=2.9, <2.10",
+    "redis>=2.10, <2.10.1",
     "sphinx>=1.2.2, <2.0",
     "coverage",
     "mock",
