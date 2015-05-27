e1fbf443cd6b8a47551a085b5b2bc97aa6df939f
Daniel Lombraña González <teleyinex@gmail.com>
2015-05-27 16:48:00 +0200

    New test.

--- test/test_jobs/test_dashboard_users.py
+++ test/test_jobs/test_dashboard_users.py
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with PyBossa.  If not, see <http://www.gnu.org/licenses/>.
 
-from pybossa.dashboard import dashboard_new_users_week
+from pybossa.dashboard import dashboard_new_users_week, format_returning_users
 from pybossa.dashboard import dashboard_returning_users_week, format_new_users
 from pybossa.core import db
 from datetime import datetime, timedelta
@@ -124,3 +124,20 @@ class TestDashBoardReturningUsers(Test):
         for row in results:
             assert row.n_days == 2
             assert row.user_id == task_run.user_id
+
+    @with_context
+    def test_format_returning_users(self):
+        """Test format returning users works."""
+        TaskRunFactory.create()
+        day = datetime.utcnow() - timedelta(days=1)
+        TaskRunFactory.create(finish_time=day.isoformat())
+        dashboard_returning_users_week()
+        res = format_returning_users()
+        for i in range(1,8):
+            if i == 1:
+                day = '%s day' % i
+            else:
+                day = "%s days" % i
+            err = "%s != %s" % (res['labels'][i - 1], day)
+            assert res['labels'][i - 1] == day, err
+            assert res['series'][0][i - 1] == 0, res['series'][i][0]
