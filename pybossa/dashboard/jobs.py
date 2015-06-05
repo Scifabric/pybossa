75c227afa654385ba7d270ea6de9ca99d28bba56
alejandrodob <alejandrodob@gmail.com>
2015-06-05 12:42:22 +0200

    Extract exists_materialized_view function

--- pybossa/dashboard/jobs.py
+++ pybossa/dashboard/jobs.py
@@ -20,233 +20,202 @@ from sqlalchemy import text
 from pybossa.core import db
 
 
+def _exists_materialized_view(view):
+    sql = text('''SELECT EXISTS (SELECT relname FROM pg_class WHERE
+               relname=:view);''')
+    results = db.slave_session.execute(sql, dict(view=view))
+    for result in results:
+        return result.exists
+    return False
+
+
 def active_users_week():
     """Create or update active users last week materialized view."""
-    # Check first if the materialized view exists
-    sql = text('''SELECT EXISTS (SELECT relname FROM pg_class WHERE
-               relname='dashboard_week_users');''')
-    results = db.slave_session.execute(sql)
-    for row in results:
-        if row.exists:
-            sql = text('''REFRESH MATERIALIZED VIEW
-                       dashboard_week_users''')
-            db.session.execute(sql)
-            db.session.commit()
-            return "Materialized view refreshed"
-        else:
-            sql = text('''CREATE MATERIALIZED VIEW dashboard_week_users AS
-                       WITH crafters_per_day AS
-                            (select to_date(task_run.finish_time,
-                                            'YYYY-MM-DD\THH24:MI:SS.US') AS day,
-                                    user_id, COUNT(task_run.user_id) AS day_crafters
-                            FROM task_run
-                            WHERE to_date(task_run.finish_time,
-                                          'YYYY-MM-DD\THH24:MI:SS.US')
-                                >= NOW() - ('1 week'):: INTERVAL
-                            GROUP BY day, task_run.user_id)
-                       SELECT day, COUNT(crafters_per_day.user_id) AS n_users
-                       FROM crafters_per_day GROUP BY day ORDER BY day;''')
-            results = db.session.execute(sql)
-            db.session.commit()
-            return "Materialized view created"
+    if _exists_materialized_view('dashboard_week_users'):
+        sql = text('''REFRESH MATERIALIZED VIEW
+                   dashboard_week_users''')
+        db.session.execute(sql)
+        db.session.commit()
+        return "Materialized view refreshed"
+    else:
+        sql = text('''CREATE MATERIALIZED VIEW dashboard_week_users AS
+                   WITH crafters_per_day AS
+                        (select to_date(task_run.finish_time,
+                                        'YYYY-MM-DD\THH24:MI:SS.US') AS day,
+                                user_id, COUNT(task_run.user_id) AS day_crafters
+                        FROM task_run
+                        WHERE to_date(task_run.finish_time,
+                                      'YYYY-MM-DD\THH24:MI:SS.US')
+                            >= NOW() - ('1 week'):: INTERVAL
+                        GROUP BY day, task_run.user_id)
+                   SELECT day, COUNT(crafters_per_day.user_id) AS n_users
+                   FROM crafters_per_day GROUP BY day ORDER BY day;''')
+        results = db.session.execute(sql)
+        db.session.commit()
+        return "Materialized view created"
 
 
 def active_anon_week():
     """Create or update active anon last week materialized view."""
-    # Check first if the materialized view exists
-    sql = text('''SELECT EXISTS (SELECT relname FROM pg_class WHERE
-               relname='dashboard_week_anon');''')
-    results = db.slave_session.execute(sql)
-    for row in results:
-        if row.exists:
-            sql = text('''REFRESH MATERIALIZED VIEW
-                       dashboard_week_anon''')
-            db.session.execute(sql)
-            db.session.commit()
-            return "Materialized view refreshed"
-        else:
-            sql = text('''CREATE MATERIALIZED VIEW dashboard_week_anon AS
-                       WITH crafters_per_day AS
-                            (select to_date(task_run.finish_time,
-                                            'YYYY-MM-DD\THH24:MI:SS.US') AS day,
-                                    user_ip, COUNT(task_run.user_ip) AS day_crafters
-                            FROM task_run
-                            WHERE to_date(task_run.finish_time,
-                                          'YYYY-MM-DD\THH24:MI:SS.US')
-                                >= NOW() - ('1 week'):: INTERVAL
-                            GROUP BY day, task_run.user_ip)
-                       SELECT day, COUNT(crafters_per_day.user_ip) AS n_users
-                       FROM crafters_per_day GROUP BY day ORDER BY day;''')
-            results = db.session.execute(sql)
-            db.session.commit()
-            return "Materialized view created"
+    if _exists_materialized_view('dashboard_week_anon'):
+        sql = text('''REFRESH MATERIALIZED VIEW
+                   dashboard_week_anon''')
+        db.session.execute(sql)
+        db.session.commit()
+        return "Materialized view refreshed"
+    else:
+        sql = text('''CREATE MATERIALIZED VIEW dashboard_week_anon AS
+                   WITH crafters_per_day AS
+                        (select to_date(task_run.finish_time,
+                                        'YYYY-MM-DD\THH24:MI:SS.US') AS day,
+                                user_ip, COUNT(task_run.user_ip) AS day_crafters
+                        FROM task_run
+                        WHERE to_date(task_run.finish_time,
+                                      'YYYY-MM-DD\THH24:MI:SS.US')
+                            >= NOW() - ('1 week'):: INTERVAL
+                        GROUP BY day, task_run.user_ip)
+                   SELECT day, COUNT(crafters_per_day.user_ip) AS n_users
+                   FROM crafters_per_day GROUP BY day ORDER BY day;''')
+        results = db.session.execute(sql)
+        db.session.commit()
+        return "Materialized view created"
 
 
 def new_projects_week():
     """Create or update new created projects last week materialized view."""
-    # Check first if the materialized view exists
-    sql = text('''SELECT EXISTS (SELECT relname FROM pg_class WHERE
-               relname='dashboard_week_project_new');''')
-    results = db.slave_session.execute(sql)
-    for row in results:
-        if row.exists:
-            sql = text('''REFRESH MATERIALIZED VIEW
-                       dashboard_week_project_new''')
-            db.session.execute(sql)
-            db.session.commit()
-            return "Materialized view refreshed"
-        else:
-            sql = text('''CREATE MATERIALIZED VIEW dashboard_week_project_new AS
-                       SELECT TO_DATE(project.created, 'YYYY-MM-DD\THH24:MI:SS.US') as day,
-                       project.id, short_name, project.name,
-                       owner_id, "user".name as u_name, "user".email_addr
-                       FROM project, "user"
-                       WHERE TO_DATE(project.created,
-                                    'YYYY-MM-DD\THH24:MI:SS.US') >= now() -
-                                    ('1 week')::INTERVAL
-                       AND "user".id=project.owner_id
-                       GROUP BY project.id, "user".name, "user".email_addr;''')
-            results = db.session.execute(sql)
-            db.session.commit()
-            return "Materialized view created"
+    if _exists_materialized_view('dashboard_week_project_new'):
+        sql = text('''REFRESH MATERIALIZED VIEW
+                   dashboard_week_project_new''')
+        db.session.execute(sql)
+        db.session.commit()
+        return "Materialized view refreshed"
+    else:
+        sql = text('''CREATE MATERIALIZED VIEW dashboard_week_project_new AS
+                   SELECT TO_DATE(project.created, 'YYYY-MM-DD\THH24:MI:SS.US') as day,
+                   project.id, short_name, project.name,
+                   owner_id, "user".name as u_name, "user".email_addr
+                   FROM project, "user"
+                   WHERE TO_DATE(project.created,
+                                'YYYY-MM-DD\THH24:MI:SS.US') >= now() -
+                                ('1 week')::INTERVAL
+                   AND "user".id=project.owner_id
+                   GROUP BY project.id, "user".name, "user".email_addr;''')
+        results = db.session.execute(sql)
+        db.session.commit()
+        return "Materialized view created"
 
 
 def update_projects_week():
     """Create or update updated projects last week materialized view."""
-    # Check first if the materialized view exists
-    sql = text('''SELECT EXISTS (SELECT relname FROM pg_class WHERE
-               relname='dashboard_week_project_update');''')
-    results = db.slave_session.execute(sql)
-    for row in results:
-        if row.exists:
-            sql = text('''REFRESH MATERIALIZED VIEW
-                       dashboard_week_project_update''')
-            db.session.execute(sql)
-            db.session.commit()
-            return "Materialized view refreshed"
-        else:
-            sql = text('''CREATE MATERIALIZED VIEW dashboard_week_project_update AS
-                       SELECT TO_DATE(project.updated, 'YYYY-MM-DD\THH24:MI:SS.US') as day,
-                       project.id, short_name, project.name,
-                       owner_id, "user".name as u_name, "user".email_addr
-                       FROM project, "user"
-                       WHERE TO_DATE(project.updated,
-                                    'YYYY-MM-DD\THH24:MI:SS.US') >= now() -
-                                    ('1 week')::INTERVAL
-                       AND "user".id=project.owner_id
-                       GROUP BY project.id, "user".name, "user".email_addr;''')
-            results = db.session.execute(sql)
-            db.session.commit()
-            return "Materialized view created"
+    if _exists_materialized_view('dashboard_week_project_update'):
+        sql = text('''REFRESH MATERIALIZED VIEW
+                   dashboard_week_project_update''')
+        db.session.execute(sql)
+        db.session.commit()
+        return "Materialized view refreshed"
+    else:
+        sql = text('''CREATE MATERIALIZED VIEW dashboard_week_project_update AS
+                   SELECT TO_DATE(project.updated, 'YYYY-MM-DD\THH24:MI:SS.US') as day,
+                   project.id, short_name, project.name,
+                   owner_id, "user".name as u_name, "user".email_addr
+                   FROM project, "user"
+                   WHERE TO_DATE(project.updated,
+                                'YYYY-MM-DD\THH24:MI:SS.US') >= now() -
+                                ('1 week')::INTERVAL
+                   AND "user".id=project.owner_id
+                   GROUP BY project.id, "user".name, "user".email_addr;''')
+        results = db.session.execute(sql)
+        db.session.commit()
+        return "Materialized view created"
 
 
 def new_tasks_week():
     """Create or update new tasks last week materialized view."""
-    # Check first if the materialized view exists
-    sql = text('''SELECT EXISTS (SELECT relname FROM pg_class WHERE
-               relname='dashboard_week_new_task');''')
-    results = db.slave_session.execute(sql)
-    for row in results:
-        if row.exists:
-            sql = text('''REFRESH MATERIALIZED VIEW
-                       dashboard_week_new_task''')
-            db.session.execute(sql)
-            db.session.commit()
-            return "Materialized view refreshed"
-        else:
-            sql = text('''CREATE MATERIALIZED VIEW dashboard_week_new_task AS
-                          SELECT TO_DATE(task.created,
-                                         'YYYY-MM-DD\THH24:MI:SS.US') AS day,
-                          COUNT(task.id) AS day_tasks
-                          FROM task WHERE TO_DATE(task.created,
-                                                  'YYYY-MM-DD\THH24:MI:SS.US')
-                                              >= now() - ('1 week'):: INTERVAL
-                          GROUP BY day;''')
-            results = db.session.execute(sql)
-            db.session.commit()
-            return "Materialized view created"
+    if _exists_materialized_view('dashboard_week_new_task'):
+        sql = text('''REFRESH MATERIALIZED VIEW
+                   dashboard_week_new_task''')
+        db.session.execute(sql)
+        db.session.commit()
+        return "Materialized view refreshed"
+    else:
+        sql = text('''CREATE MATERIALIZED VIEW dashboard_week_new_task AS
+                      SELECT TO_DATE(task.created,
+                                     'YYYY-MM-DD\THH24:MI:SS.US') AS day,
+                      COUNT(task.id) AS day_tasks
+                      FROM task WHERE TO_DATE(task.created,
+                                              'YYYY-MM-DD\THH24:MI:SS.US')
+                                          >= now() - ('1 week'):: INTERVAL
+                      GROUP BY day;''')
+        results = db.session.execute(sql)
+        db.session.commit()
+        return "Materialized view created"
 
 
 def new_task_runs_week():
     """Create or update new task_runs last week materialized view."""
-    # Check first if the materialized view exists
-    sql = text('''SELECT EXISTS (SELECT relname FROM pg_class WHERE
-               relname='dashboard_week_new_task_run');''')
-    results = db.slave_session.execute(sql)
-    for row in results:
-        if row.exists:
-            sql = text('''REFRESH MATERIALIZED VIEW
-                       dashboard_week_new_task_run''')
-            db.session.execute(sql)
-            db.session.commit()
-            return "Materialized view refreshed"
-        else:
-            sql = text('''CREATE MATERIALIZED VIEW dashboard_week_new_task_run AS
-                          SELECT TO_DATE(task_run.finish_time,
-                                         'YYYY-MM-DD\THH24:MI:SS.US') AS day,
-                          COUNT(task_run.id) AS day_task_runs
-                          FROM task_run WHERE TO_DATE(task_run.finish_time,
-                                                  'YYYY-MM-DD\THH24:MI:SS.US')
-                                              >= now() - ('1 week'):: INTERVAL
-                          GROUP BY day;''')
-            results = db.session.execute(sql)
-            db.session.commit()
-            return "Materialized view created"
+    if _exists_materialized_view('dashboard_week_new_task_run'):
+        sql = text('''REFRESH MATERIALIZED VIEW
+                   dashboard_week_new_task_run''')
+        db.session.execute(sql)
+        db.session.commit()
+        return "Materialized view refreshed"
+    else:
+        sql = text('''CREATE MATERIALIZED VIEW dashboard_week_new_task_run AS
+                      SELECT TO_DATE(task_run.finish_time,
+                                     'YYYY-MM-DD\THH24:MI:SS.US') AS day,
+                      COUNT(task_run.id) AS day_task_runs
+                      FROM task_run WHERE TO_DATE(task_run.finish_time,
+                                              'YYYY-MM-DD\THH24:MI:SS.US')
+                                          >= now() - ('1 week'):: INTERVAL
+                      GROUP BY day;''')
+        results = db.session.execute(sql)
+        db.session.commit()
+        return "Materialized view created"
 
 
 def new_users_week():
     """Create or update new users last week materialized view."""
-    # Check first if the materialized view exists
-    sql = text('''SELECT EXISTS (SELECT relname FROM pg_class WHERE
-               relname='dashboard_week_new_users');''')
-    results = db.slave_session.execute(sql)
-    for row in results:
-        if row.exists:
-            sql = text('''REFRESH MATERIALIZED VIEW
-                       dashboard_week_new_users''')
-            db.session.execute(sql)
-            db.session.commit()
-            return "Materialized view refreshed"
-        else:
-            sql = text('''CREATE MATERIALIZED VIEW dashboard_week_new_users AS
-                          SELECT TO_DATE("user".created,
-                                         'YYYY-MM-DD\THH24:MI:SS.US') AS day,
-                          COUNT("user".id) AS day_users
-                          FROM "user" WHERE TO_DATE("user".created,
-                                                  'YYYY-MM-DD\THH24:MI:SS.US')
-                                              >= now() - ('1 week'):: INTERVAL
-                          GROUP BY day;''')
-            results = db.session.execute(sql)
-            db.session.commit()
-            return "Materialized view created"
+    if _exists_materialized_view('dashboard_week_new_users'):
+        sql = text('''REFRESH MATERIALIZED VIEW
+                   dashboard_week_new_users''')
+        db.session.execute(sql)
+        db.session.commit()
+        return "Materialized view refreshed"
+    else:
+        sql = text('''CREATE MATERIALIZED VIEW dashboard_week_new_users AS
+                      SELECT TO_DATE("user".created,
+                                     'YYYY-MM-DD\THH24:MI:SS.US') AS day,
+                      COUNT("user".id) AS day_users
+                      FROM "user" WHERE TO_DATE("user".created,
+                                              'YYYY-MM-DD\THH24:MI:SS.US')
+                                          >= now() - ('1 week'):: INTERVAL
+                      GROUP BY day;''')
+        results = db.session.execute(sql)
+        db.session.commit()
+        return "Materialized view created"
 
 
 def returning_users_week():
     """Create or update returning users last week materialized view."""
-    # Check first if the materialized view exists
-    sql = text('''SELECT EXISTS (SELECT relname FROM pg_class WHERE
-               relname='dashboard_week_returning_users');''')
-    results = db.slave_session.execute(sql)
-    for row in results:
-        if row.exists:
-            sql = text('''REFRESH MATERIALIZED VIEW
-                       dashboard_week_returning_users''')
-            db.session.execute(sql)
-            db.session.commit()
-            return "Materialized view refreshed"
-        else:
-            sql = text('''CREATE MATERIALIZED VIEW dashboard_week_returning_users AS
-                       WITH data AS (
-                        SELECT user_id, TO_DATE(task_run.finish_time,
-                        'YYYY-MM-DD\THH24:MI:SS.US') AS day
-                       FROM task_run
-                       WHERE TO_DATE(task_run.finish_time,
-                       'YYYY-MM-DD\THH24:MI:SS.US') >= NOW()
-                       - ('1 week')::INTERVAL GROUP BY day, task_run.user_id)
-                       SELECT user_id, COUNT(user_id) AS n_days
-                       FROM data GROUP BY user_id HAVING(count(user_id) > 1)
-                       ORDER by n_days;
-                          ''')
-            results = db.session.execute(sql)
-            db.session.commit()
-            return "Materialized view created"
+    if _exists_materialized_view('dashboard_week_returning_users'):
+        sql = text('''REFRESH MATERIALIZED VIEW
+                   dashboard_week_returning_users''')
+        db.session.execute(sql)
+        db.session.commit()
+        return "Materialized view refreshed"
+    else:
+        sql = text('''CREATE MATERIALIZED VIEW dashboard_week_returning_users AS
+                   WITH data AS (
+                    SELECT user_id, TO_DATE(task_run.finish_time,
+                    'YYYY-MM-DD\THH24:MI:SS.US') AS day
+                   FROM task_run
+                   WHERE TO_DATE(task_run.finish_time,
+                   'YYYY-MM-DD\THH24:MI:SS.US') >= NOW()
+                   - ('1 week')::INTERVAL GROUP BY day, task_run.user_id)
+                   SELECT user_id, COUNT(user_id) AS n_days
+                   FROM data GROUP BY user_id HAVING(count(user_id) > 1)
+                   ORDER by n_days;
+                      ''')
+        results = db.session.execute(sql)
+        db.session.commit()
+        return "Materialized view created"
