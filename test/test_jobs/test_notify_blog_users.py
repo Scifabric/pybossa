c951efa7ea1804f7126734b0e831f9426939772b
Daniel Lombraña González <teleyinex@gmail.com>
2015-07-22 13:10:40 +0200

    New test for notify users.

---
+++ test/test_jobs/test_notify_blog_users.py
@@ -0,0 +1,42 @@
+# -*- coding: utf8 -*-
+# This file is part of PyBossa.
+#
+# Copyright (C) 2014 SF Isle of Man Limited
+#
+# PyBossa is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# PyBossa is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with PyBossa.  If not, see <http://www.gnu.org/licenses/>.
+
+from pybossa.jobs import notify_blog_users
+from default import Test, with_context
+from factories import BlogpostFactory
+from factories import TaskRunFactory
+from factories import ProjectFactory
+from mock import patch, MagicMock
+
+queue = MagicMock()
+queue.enqueue.return_value = True
+
+
+class TestNotifyBlogUsers(Test):
+
+
+    @with_context
+    @patch('pybossa.jobs.requests')
+    def test_notify_blog_users(self, mock):
+        """Test Notify Blog users works."""
+        project = ProjectFactory.create()
+        TaskRunFactory.create(project=project)
+        blog = BlogpostFactory.create(project=project)
+        res = notify_blog_users(blog.id, blog.project.id)
+        msg = "1 users notified by email"
+        assert res == msg, res
