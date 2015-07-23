fb71e68521342190477042a3baadb3321c56ddb1
Daniel Lombraña González <teleyinex@gmail.com>
2015-07-23 11:45:14 +0200

    Test for stats_hours.

--- test/test_cache/test_cache_project_stats.py
+++ test/test_cache/test_cache_project_stats.py
@@ -20,8 +20,8 @@ from default import Test, with_context
 from pybossa.cache.project_stats import *
 from factories import UserFactory, ProjectFactory, TaskFactory, \
     TaskRunFactory, AnonymousTaskRunFactory
-from mock import patch
-from datetime import date, timedelta
+import pytz
+from datetime import date, datetime, timedelta
 
 
 class TestProjectsStatsCache(Test):
@@ -93,3 +93,41 @@ class TestProjectsStatsCache(Test):
         assert len(dates) == 7, len(dates)
         assert len(dates_anon) == 0, len(dates_anon)
         assert len(dates_auth) == 1, len(dates_auth)
+
+    def test_stats_hours(self):
+        """Test CACHE PROJECT STATS hours works."""
+        pr = ProjectFactory.create()
+        task = TaskFactory.create(n_answers=1)
+        today = datetime.now(pytz.utc)
+        TaskFactory.create()
+        TaskRunFactory.create(project=pr, task=task)
+        AnonymousTaskRunFactory.create(project=pr)
+        hours, hours_anon, hours_auth, max_hours, \
+            max_hours_anon, max_hours_auth = stats_hours(pr.id)
+        print hours
+        print hours_anon
+        print hours_auth
+        print max_hours
+        print max_hours_anon
+        print max_hours_auth
+        assert len(hours) == 24, len(hours)
+        assert hours[today.strftime('%H')] == 2, hours[today.strftime('%H')]
+        assert hours_anon[today.strftime('%H')] == 1, hours_anon[today.strftime('%H')]
+        assert hours_auth[today.strftime('%H')] == 1, hours_auth[today.strftime('%H')]
+        assert max_hours == 2
+        assert max_hours_anon == 1
+        assert max_hours_auth == 1
+
+
+    # def test_stats_hours_with_period(self):
+    #     """Test CACHE PROJECT STATS hours with period works."""
+    #     pr = ProjectFactory.create()
+    #     d = date.today() - timedelta(days=6)
+    #     task = TaskFactory.create(n_answers=1, created=d)
+    #     TaskRunFactory.create(project=pr, task=task, created=d, finish_time=d)
+    #     d = date.today() - timedelta(days=16)
+    #     AnonymousTaskRunFactory.create(project=pr, created=d, finish_time=d)
+    #     dates, dates_anon, dates_auth = stats_dates(pr.id, '1 week')
+    #     assert len(dates) == 7, len(dates)
+    #     assert len(dates_anon) == 0, len(dates_anon)
+    #     assert len(dates_auth) == 1, len(dates_auth)
