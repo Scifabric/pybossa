1e1c63aec3d0fb59ab77461bcfe2b2ade4a63cce
Daniel Lombraña González <teleyinex@gmail.com>
2015-09-21 13:39:00 +0200

    Version v0.2.4

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ requirements = [
 
 setup(
     name = 'pybossa',
-    version = '0.2.2',
+    version = '0.2.4',
     packages = find_packages(),
     install_requires = requirements,
     # only needed when installing directly from setup.py (PyPi, eggs?) and pointing to e.g. a git repo.
