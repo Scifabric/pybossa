ad150b2a7c6636bd215080a8e23ab4859782fd01
Daniel Lombraña González <teleyinex@gmail.com>
2015-09-29 10:09:47 +0200

    New test.

--- test/test_view/test_webhook.py
+++ test/test_view/test_webhook.py
@@ -16,13 +16,22 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with PyBossa.  If not, see <http://www.gnu.org/licenses/>.
 
-
+import json
 from helper import web
 from default import with_context
-from factories import ProjectFactory
-from pybossa.core import user_repo
+from factories import ProjectFactory, TaskFactory, AnonymousTaskRunFactory
+from pybossa.core import user_repo, webhook_repo
+from pybossa.model import make_timestamp
+from pybossa.model.webhook import Webhook
 
 class TestWebhookView(web.Helper):
+
+    def payload(self, project, task):
+        return dict(fired_at=make_timestamp(),
+                    project_short_name=project.short_name,
+                    project_id= project.id,
+                    event='task_completed',
+                    task_id=task.id)
 
     @with_context
     def test_webhook_handler_anon(self):
@@ -55,3 +64,23 @@ class TestWebhookView(web.Helper):
         assert res.status_code == 200, res.status_code
         assert "Created" in res.data
         assert "Payload" in res.data
+
+    @with_context
+    def test_webhook_handler_post_oid(self):
+        """Test WEBHOOK post oid works."""
+        self.register()
+        user = user_repo.get(1)
+        project = ProjectFactory.create(owner=user)
+        task = TaskFactory.create(project=project, n_answers=1)
+        AnonymousTaskRunFactory.create(project=project, task=task)
+        payload = self.payload(project, task)
+        webhook = Webhook(project_id=project.id, payload=payload,
+                          response='OK', response_status_code=200)
+        webhook_repo.save(webhook)
+        webhook = webhook_repo.get(1)
+        url = "/project/%s/webhook" % project.short_name
+        res = self.app.get(url)
+        assert res.status_code == 200, res.status_code
+        assert "Created" in res.data
+        assert "Payload" in res.data
+        assert 'fired_at' in res.data
