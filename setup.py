08333522b427f3ceb1c23f0e532370491c831f80
Daniel Lombraña González <teleyinex@gmail.com>
2015-09-30 13:29:37 +0200

    Add versions.

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ requirements = [
 
 setup(
     name = 'pybossa',
-    version = '1.0.0',
+    version = '1.1.0',
     packages = find_packages(),
     install_requires = requirements,
     # only needed when installing directly from setup.py (PyPi, eggs?) and pointing to e.g. a git repo.
