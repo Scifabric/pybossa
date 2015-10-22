03516184eb8b47cef7b454c16c85125e5f6ca162
Marvin Reimer <therealmarv@gmail.com>
2015-10-22 16:54:12 +0200

    fix typo

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ requirements = [
     "Flask-Babel>=0.9, <1.0",
     "Flask-Login",                      # was pinned to Flask-Login==0.2.3 in the past. GitHub version 3.0+ is used now.
     "Flask-Mail>=0.9.0, <1.0",
-    "misaka>=1.0.0, <2.0.0"
+    "misaka>=1.0.0, <2.0.0",
     "Flask-Misaka>=0.2.0, <1.0",
     "Flask-OAuthlib>=0.9.1, <0.9.2",
     "Flask-SQLAlchemy>=2.0, <2.1",
