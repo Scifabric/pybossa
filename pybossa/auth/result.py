c652ee301270f1fca7c14250109d47ae258c3c10
Daniel Lombraña González <teleyinex@gmail.com>
2015-11-18 10:28:21 +0100

    Add result authentication.
    Everything is forbidden except reading it.

---
+++ pybossa/auth/result.py
@@ -0,0 +1,46 @@
+# -*- coding: utf8 -*-
+# This file is part of PyBossa.
+#
+# Copyright (C) 2015 SciFabric LTD.
+#
+# PyBossa is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# PyBossa is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with PyBossa.  If not, see <http://www.gnu.org/licenses/>.
+
+
+class ResultAuth(object):
+    _specific_actions = []
+
+    def __init__(self, project_repo):
+        self.project_repo = project_repo
+
+    @property
+    def specific_actions(self):
+        return self._specific_actions
+
+    def can(self, user, action, result=None, project_id=None):
+        action = ''.join(['_', action])
+        return getattr(self, action)(user, result, project_id)
+
+    def _create(self, user, result, project_id=None):
+        return False
+
+    def _read(self, user, result=None, project_id=None):
+        if (result is None and project_id is None):
+            return False
+        return True
+
+    def _update(self, user, result, project_id=None):
+        return False
+
+    def _delete(self, user, result, project_id=None):
+        return False
