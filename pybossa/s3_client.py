950b5091de0c312dbd45342bfd5fe8f018b0406e
alejandrodob <alejandrodob@gmail.com>
2016-02-02 12:12:36 +0100

    Implement s3 client methods

--- pybossa/s3_client.py
+++ pybossa/s3_client.py
@@ -20,18 +20,24 @@ import boto3
 
 class S3Client(object):
 
-    def __init__(self, role_ARN, identity_token, username=''):
+    def __init__(self, role_ARN, identity_token, username='s3-client'):
         sts = boto3.client('sts')
         response = sts.assume_role_with_web_identity(
             RoleArn=role_ARN,
             RoleSessionName=username,
             WebIdentityToken=identity_token,
-            ProviderId='www.amazon.com'
-        )
+            ProviderId='www.amazon.com')
         print response
-        self.credentials = response['Credentials']
+        credentials = response['Credentials']
         session = boto3.session.Session(
-                    aws_access_key_id=self.credentials['AccessKeyId'],
-                    aws_secret_access_key=self.credentials['SecretAccessKey'],
-                    aws_session_token=self.credentials['SessionToken'])
-        s3 = session.resource('s3')
+                    aws_access_key_id=credentials['AccessKeyId'],
+                    aws_secret_access_key=credentials['SecretAccessKey'],
+                    aws_session_token=credentials['SessionToken'])
+        self.s3 = session.resource('s3')
+
+    def buckets(self):
+        return [bucket.name for bucket in self.s3.buckets.all()]
+
+    def objects(self, bucket_name):
+        bucket = self.s3.Bucket(bucket_name)
+        return [o.key for o in bucket.objects.all()]
