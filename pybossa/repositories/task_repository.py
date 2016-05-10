d091fe9ac0a15035fc25aa7391ca6c5c891b3a87
Daniel Lombraña González <teleyinex@gmail.com>
2016-05-10 15:58:59 +0200

    Remove print.

--- pybossa/repositories/task_repository.py
+++ pybossa/repositories/task_repository.py
@@ -41,7 +41,6 @@ class TaskRepository(Repository):
                         last_id=None, **filters):
 
         query = self.create_context(filters, Task)
-        print query
         if last_id:
             query = query.filter(Task.id > last_id)
             query = query.order_by(Task.id).limit(limit)
