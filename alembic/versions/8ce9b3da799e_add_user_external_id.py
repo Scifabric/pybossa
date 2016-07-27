89493679cbb41232edbd42160d2193af7e74658c
Daniel Lombraña González <teleyinex@gmail.com>
2016-07-27 17:21:08 +0200

    Populate the project secret key with an MD5 secret.

--- alembic/versions/8ce9b3da799e_add_user_external_id.py
+++ alembic/versions/8ce9b3da799e_add_user_external_id.py
@@ -19,8 +19,10 @@ field = 'external_uid'
 def upgrade():
     op.add_column('task_run', sa.Column(field, sa.String))
     op.add_column('project', sa.Column('secret_key', sa.String))
+    query = 'update project set secret_key=md5(random()::text);'
+    op.execute(query)
 
 
 def downgrade():
     op.drop_column('task_run', field)
-    op.drop_column('project', sa.Column('secret_key', sa.String))
+    op.drop_column('project', 'secret_key')
