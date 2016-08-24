944cbd918dff4bdea8901fe51d1f1d594a9bba9b
Georges Labrèche <georges.labreche@gmail.com>
2016-08-24 20:47:50 +0200

    Update jobs.sh

--- jobs.sh
+++ jobs.sh
@@ -1,2 +1,2 @@
 source ./venv/bin/activate
-python app_context_rqworker.py scheduled_jobs super high medium low
+python app_context_rqworker.py scheduled_jobs super high medium low -s > /dev/null 2>&1 # dump all output (stdout and stderr) to /dev/null
