cc20c0301cda92956c43d8513698a9eca3dfe38d
georgeslabreche <georges@opendatakosovo.org>
2016-09-17 19:12:16 +0200

    initial implemention of task run consolidation mongo function

--- pybossa/mongo/task_run_mongo_util.py
+++ pybossa/mongo/task_run_mongo_util.py
@@ -4,3 +4,8 @@ class TaskRunMongoUtil(BaseMongoUtil):
     def __init__(self):
         super(TaskRunMongoUtil, self).__init__('taskruns')
 
+
+    def consolidate_redundancy(self):
+        ''' Go through redundant task run submissions and merge them into single documents
+        '''
+        pass
