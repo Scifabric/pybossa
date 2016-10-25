f126953418c5d0103e15172e83d77a37cf3f8b5a
dtwsdev <dtws-dev@gigwork.net>
2016-10-25 18:02:53 -0400

    Update task.py
    add "exported" column

--- pybossa/model/task.py
+++ pybossa/model/task.py
@@ -52,6 +52,8 @@ class Task(db.Model, DomainObject):
     n_answers = Column(Integer, default=30)
     #: Array of User IDs that favorited this task
     fav_user_ids = Column(MutableList.as_mutable(ARRAY(Integer)))
+    #: completed task can be marked as exported=True after its exported
+    exported = Column(Boolean, default=False)
 
     task_runs = relationship(TaskRun, cascade='all, delete, delete-orphan', backref='task')
 
