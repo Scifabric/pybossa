d1cf824a23fd9cec6914895e7a90feaa43152122
Daniel Lombraña González <teleyinex@gmail.com>
2017-01-04 16:21:10 +0100

    Remove method as we use now Flask-Cors.

--- pybossa/util.py
+++ pybossa/util.py
@@ -81,51 +81,6 @@ def admin_required(f):  # pragma: no cover
     return decorated_function
 
 
-# from http://flask.pocoo.org/snippets/56/
-def crossdomain(origin=None, methods=None, headers=None,
-                max_age=21600, attach_to_all=True,
-                automatic_options=True):
-    """Crossdomain decorator."""
-    if methods is not None:  # pragma: no cover
-        methods = ', '.join(sorted(x.upper() for x in methods))
-    if headers is not None and not isinstance(headers, basestring):
-        headers = ', '.join(x.upper() for x in headers)
-    if not isinstance(origin, basestring):  # pragma: no cover
-        origin = ', '.join(origin)
-    if isinstance(max_age, timedelta):  # pragma: no cover
-        max_age = max_age.total_seconds()
-
-    def get_methods():  # pragma: no cover
-        if methods is not None:
-            return methods
-
-        options_resp = current_app.make_default_options_response()
-        return options_resp.headers['allow']
-
-    def decorator(f):
-
-        def wrapped_function(*args, **kwargs):
-            if automatic_options and request.method == 'OPTIONS':  # pragma: no cover
-                resp = current_app.make_default_options_response()
-            else:
-                resp = make_response(f(*args, **kwargs))
-            if not attach_to_all and request.method != 'OPTIONS':  # pragma: no cover
-                return resp
-
-            h = resp.headers
-
-            h['Access-Control-Allow-Origin'] = origin
-            h['Access-Control-Allow-Methods'] = get_methods()
-            h['Access-Control-Max-Age'] = str(max_age)
-            if headers is not None:
-                h['Access-Control-Allow-Headers'] = headers
-            return resp
-
-        f.provide_automatic_options = False
-        return update_wrapper(wrapped_function, f)
-    return decorator
-
-
 # Fromhttp://stackoverflow.com/q/1551382
 def pretty_date(time=False):
     """Return a pretty date.
