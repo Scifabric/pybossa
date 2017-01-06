4c3bb9371c8551d442b9e7804571493f1d42ca8e
jdesilvio <john.desilvio@yahoo.com>
2017-01-06 20:57:46 +0000

    Added tests for presented time guards

--- test/test_contributions_guard.py
+++ test/test_contributions_guard.py
@@ -31,6 +31,8 @@ class TestContributionsGuard(object):
         self.auth_user = {'user_id': 33, 'user_ip': None}
         self.task = Task(id=22)
 
+    # Task requested guard tests
+
     def test_stamp_registers_specific_user_id_and_task(self):
         key = 'pybossa:task_requested:user:33:task:22'
 
@@ -84,3 +86,60 @@ class TestContributionsGuard(object):
         self.guard.stamp(self.task, self.auth_user)
 
         assert self.guard.retrieve_timestamp(self.task, self.auth_user) == 'now'
+
+
+    # Task presented guard tests
+
+    def test_stamp_presented_time_registers_specific_user_id_and_task(self):
+        key = 'pybossa:task_presented:user:33:task:22'
+
+        self.guard.stamp_presented_time(self.task.id, self.auth_user)
+
+        assert key in self.connection.keys(), self.connection.keys()
+
+    def test_stamp_presented_time_registers_user_as_None_and_task_if_no_id_provided(self):
+        key = 'pybossa:task_presented:user:None:task:22'
+
+        self.guard.stamp_presented_time(self.task.id, self.anon_user)
+
+        assert key in self.connection.keys(), self.connection.keys()
+
+    def test_stamp_presented_time_expires_in_one_hour(self):
+        key = 'pybossa:task_presented:user:33:task:22'
+        ONE_HOUR = 60 * 60
+
+        self.guard.stamp_presented_time(self.task.id, self.auth_user)
+
+        assert self.connection.ttl(key) == ONE_HOUR, self.connection.ttl(key)
+
+    @patch('pybossa.contributions_guard.make_timestamp')
+    def test_stamp_presented_time_adds_a_timestamp_when_the_task_is_stamped(self, make_timestamp):
+        make_timestamp.return_value = "now"
+        key = 'pybossa:task_presented:user:33:task:22'
+
+        self.guard.stamp_presented_time(self.task.id, self.auth_user)
+
+        assert self.connection.get(key) == 'now'
+
+    def test_check_task_presented_stamped_returns_False_for_non_stamped_task(self):
+        assert self.guard.check_task_presented_stamped(self.task.id, self.auth_user) is False
+
+    def test_check_task_presented_stamped_returns_True_for_auth_user_who_was_presented_task(self):
+        self.guard.stamp_presented_time(self.task.id, self.auth_user)
+
+        assert self.guard.check_task_presented_stamped(self.task.id, self.auth_user) is True
+
+    def test_check_task_presented_stamped_returns_True_for_anon_user_who_was_presented_task(self):
+        self.guard.stamp_presented_time(self.task.id, self.anon_user)
+
+        assert self.guard.check_task_presented_stamped(self.task.id, self.anon_user) is True
+
+    def test_retrieve_presented_timestamp_returns_None_for_non_stamped_task(self):
+        assert self.guard.retrieve_presented_timestamp(self.task.id, self.anon_user) is None
+
+    @patch('pybossa.contributions_guard.make_timestamp')
+    def test_retrieve_presented_timestamp_returs_the_timestamp_for_stamped_task(self, make_timestamp):
+        make_timestamp.return_value = "now"
+        self.guard.stamp_presented_time(self.task.id, self.auth_user)
+
+        assert self.guard.retrieve_presented_timestamp(self.task.id, self.auth_user) == 'now'
