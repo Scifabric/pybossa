8a505f66b2cc1a63de49807d4734020a8288fee2
Daniel Lombraña González <teleyinex@gmail.com>
2017-01-24 11:43:10 +0100

    Refactor.

--- pybossa/model/project.py
+++ pybossa/model/project.py
@@ -117,7 +117,10 @@ class Project(db.Model, DomainObject):
     @classmethod
     def public_attributes(self):
         """Return a list of public attributes."""
-        return ['description', 'info', 'n_tasks', 'n_volunteers', 'name', 'overall_progress', 'short_name']
+        return ['description', 'info', 'n_tasks', 'n_volunteers', 'name',
+                'overall_progress', 'short_name', 'created', 'description',
+                'last_activity', 'last_activity_raw', 'overall_progress',
+                'n_tasks', 'n_volunteers', 'owner', 'updated']
 
     @classmethod
     def public_info_keys(self):
