7fe1bbcd631072184060449bece913037ddc0605
Marvin Reimer <therealmarv@gmail.com>
2017-02-02 13:22:57 +0800

    remove debugging code

--- pybossa/view/help.py
+++ pybossa/view/help.py
@@ -32,10 +32,10 @@ def api():
     """Render help/api page."""
     categories = cached_cat.get_used()
     projects = cached_projects.get(categories[0]['short_name'])
-    # if len(projects) > 0:
-    #     project_id = choice(projects)['id']
-    # else:  # pragma: no cover
-    project_id = None
+    if len(projects) > 0:
+        project_id = choice(projects)['id']
+    else:  # pragma: no cover
+        project_id = None
     response = dict(template='help/api.html',
                     title="Help: API",
                     project_id=project_id)
