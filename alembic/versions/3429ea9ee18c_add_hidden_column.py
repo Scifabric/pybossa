92ce76967d5d2231c5183f48c1bb7857de61c753
dtws <dtws@gigwork.net>
2017-02-27 16:31:02 -0500

    fix width

---
+++ alembic/versions/3429ea9ee18c_add_hidden_column.py
@@ -0,0 +1,23 @@
+"""add hidden column
+
+Revision ID: 3429ea9ee18c
+Revises: 41eb366742cf
+Create Date: 2017-02-27 16:49:08.458012
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '3429ea9ee18c'
+down_revision = '41eb366742cf'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    op.add_column('project', sa.Column('hidden', sa.Integer, default=0))
+
+
+def downgrade():
+    op.drop_column('project', 'hidden')
+
