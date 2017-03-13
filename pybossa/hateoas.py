912014e805ce484c349ef3f9a3c55e5d0d6a8f4e
Daniel Lombraña González <teleyinex@gmail.com>
2017-03-13 13:32:00 +0100

    Include blogpost.

--- pybossa/hateoas.py
+++ pybossa/hateoas.py
@@ -75,6 +75,12 @@ class Hateoas(object):
             # TODO: add the projects created by the user as the
             # links with rel=? (maybe 'project'??)
             return None, link
+        elif cls == 'blogpost':
+            link = self.create_link(item.id, title='blogpost')
+            if item.project_id is not None:
+                links = [self.create_link(item.project_id,
+                                          title='project', rel='parent')]
+            return links, link
         else:  # pragma: no cover
             return False
 
