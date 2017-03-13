87ab6d4e641d941ea801105fea5c0d53c94a246d
Daniel Lombraña González <teleyinex@gmail.com>
2017-03-13 12:04:24 +0100

    Fixes.

--- test/test_model/test_model_blogpost.py
+++ test/test_model/test_model_blogpost.py
@@ -173,3 +173,4 @@ class TestBlogpostModel(Test):
 
         tmp = blogpost.to_public_json()
         assert tmp.keys().sort() == Blogpost().public_attributes().sort()
+        assert Blogpost().public_info_keys == []
