f5ee06657f1ce468913ff4f0cdc40bc7abae7afe
dtws <dtws@gigwork.net>
2017-03-31 09:55:23 -0400

    add index on tasks taskruns

---
+++ alembic/versions/263677aea8ab_create_indexes_on_task_and_taskrun.py
@@ -0,0 +1,24 @@
+"""create indexes on task and taskrun
+
+Revision ID: 263677aea8ab
+Revises: f94b5711c6a7
+Create Date: 2017-03-27 17:14:22.948537
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '263677aea8ab'
+down_revision = 'f94b5711c6a7'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    op.create_index('task_run_task_id_index', 'task_run', ['task_id'])
+    op.create_index('task_project_id_fkey', 'task', ['project_id'])
+
+
+def downgrade():
+    op.drop_index('task_project_id_fkey')
+    op.drop_index('task_run_task_id_index')
