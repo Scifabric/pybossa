33532b415a95a067be006a7a4b583a73a07ce935
dtws <dtws@gigwork.net>
2017-04-06 09:25:22 -0400

    changed count function

--- pybossa/cache/projects.py
+++ pybossa/cache/projects.py
@@ -417,7 +417,7 @@ def n_count(category):
                WHERE
                category.short_name=:category
                AND project.published=true
-               AND (project.info->>'passwd_hash') IS NULL
+               AND coalesce(project.hidden, false)=false
                GROUP BY project.id)
                SELECT COUNT(*) FROM uniq
                ''')
