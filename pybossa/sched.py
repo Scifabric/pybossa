f26162e600b12ef7e4f123e474f7c47fded8f793
dtws <dtws@gigwork.net>
2017-04-06 15:44:47 -0400

    smaller blocks

--- pybossa/sched.py
+++ pybossa/sched.py
@@ -184,20 +184,31 @@ def get_locked_task(project_id, user_id=None, user_ip=None,
            (SELECT 1 FROM task_run WHERE project_id=:project_id AND
            user_id=:user_id AND task_id=task.id)
            AND task.project_id=:project_id AND task.state !='completed'
-           group by task.id ORDER BY priority_0 DESC, id ASC LIMIT 100;
+           group by task.id ORDER BY priority_0 DESC, id ASC
+           offset :offset limit :limit;
            ''')
-    rows = session.execute(sql, dict(project_id=project_id, user_id=user_id))
 
     skipped = 0
-    for task_id, taskcount, n_answers, timeout in rows:
-        timeout = timeout or TIMEOUT
-        remaining = n_answers - taskcount
-        if acquire_lock(project_id, task_id, user_id, remaining, timeout):
-            if skipped == offset:
-                return [session.query(Task).get(task_id)]
-            else:
-                skipped += 1
-    return None
+    offset = 0
+    limit = 10
+    while True:
+        rows = session.execute(sql, dict(project_id=project_id,
+                                         user_id=user_id,
+                                         offset=offset,
+                                         limit=limit))
+        if not rows.returns_rows:
+            return None
+
+        for task_id, taskcount, n_answers, timeout in rows:
+            timeout = timeout or TIMEOUT
+            remaining = n_answers - taskcount
+            if acquire_lock(project_id, task_id, user_id, remaining, timeout):
+                if skipped == offset:
+                    rows.close()
+                    return session.query(Task).get(task_id)
+                else:
+                    skipped += 1
+        offset += limit
 
 
 KEY_PREFIX = 'pybossa:project:task_requested:timestamps:{0}:{1}'
