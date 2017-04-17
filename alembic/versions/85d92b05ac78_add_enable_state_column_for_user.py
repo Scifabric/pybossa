f3761e60ae7f4f3866c7dea82e695d0bda38fabc
dtws <dtws@gigwork.net>
2017-04-17 13:37:46 -0400

    add enabled column

---
+++ alembic/versions/85d92b05ac78_add_enable_state_column_for_user.py
@@ -0,0 +1,22 @@
+"""add enable state column for user
+
+Revision ID: 85d92b05ac78
+Revises: 263677aea8ab
+Create Date: 2017-04-17 13:23:49.879496
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '85d92b05ac78'
+down_revision = '263677aea8ab'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    op.add_column('user', sa.Column('enabled', sa.Boolean, default=True))
+
+
+def downgrade():
+    op.drop_column('user', 'enabled')
