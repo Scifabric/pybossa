5fa26636bf68ae915d36fe5f4d2b666f072e10b0
Daniel Lombraña González <teleyinex@gmail.com>
2017-05-03 17:35:31 +0200

    Update public attributes.

--- pybossa/model/project.py
+++ pybossa/model/project.py
@@ -125,4 +125,5 @@ class Project(db.Model, DomainObject):
     @classmethod
     def public_info_keys(self):
         """Return a list of public info keys."""
-        return ['container', 'thumbnail', 'thumbnail_url']
+        return ['container', 'thumbnail', 'thumbnail_url',
+                'task_presenter', 'tutorial', 'sched']
