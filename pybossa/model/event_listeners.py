4d6277a55f212b38194609a3c900f4518635c07a
Daniel Lombraña González <teleyinex@gmail.com>
2017-05-08 13:49:26 +0200

    Remove unused var.

--- pybossa/model/event_listeners.py
+++ pybossa/model/event_listeners.py
@@ -78,10 +78,6 @@ def add_project_event(mapper, conn, target):
 @event.listens_for(Project, 'after_insert')
 def add_onesignal_app(mapper, conn, target):
     """Update PYBOSSA project with onesignal app."""
-    tmp = dict(id=target.id,
-               name=target.name,
-               short_name=target.short_name,
-               info=target.info)
     webpush_queue.enqueue(create_onesignal_app, target.id)
 
 
