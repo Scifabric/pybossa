b1545064785660e68f07e31175c387e05eed6b29
Daniel Lombraña González <teleyinex@gmail.com>
2017-06-08 12:22:49 +0200

    Remove tests.

--- test/test_jobs/test_onesignal.py
+++ test/test_jobs/test_onesignal.py
@@ -17,7 +17,7 @@
 # along with PYBOSSA.  If not, see <http://www.gnu.org/licenses/>.
 
 import json
-from pybossa.jobs import create_onesignal_app, push_notification
+from pybossa.jobs import push_notification
 from default import Test, with_context 
 from mock import patch, MagicMock, call
 from factories import ProjectFactory, UserFactory, CategoryFactory
@@ -30,22 +30,6 @@ class TestOnesignal(Test):
     def setUp(self):
         super(TestOnesignal, self).setUp()
         CategoryFactory.create()
-
-    @with_context
-    @patch('pybossa.jobs.PybossaOneSignal')
-    def test_create_onesignal_no_config(self, mock_onesignal):
-        """Test create_onesignal with no config works."""
-
-        project = ProjectFactory.create()
-
-        client = MagicMock()
-        client.create_app.return_value = (200, 'OK',
-                                          {'basic_auth': 'auth', 'id': 1})
-        mock_onesignal.return_value = client
-
-        create_onesignal_app(project.id)
-
-        assert client.called is False
 
     @with_context
     @patch('pybossa.jobs.PybossaOneSignal')
@@ -64,42 +48,6 @@ class TestOnesignal(Test):
        
         assert res.status_code == 200, res.data
         assert mock_onesignal.called is False
-
-    @with_context
-    @patch('pybossa.jobs.PybossaOneSignal')
-    @patch('pybossa.model.event_listeners.webpush_queue.enqueue')
-    def test_create_onesignal(self, mock_queue, mock_onesignal):
-        """Test create_onesignal with config works."""
-        client = MagicMock()
-        osdata = {'basic_auth': 'auth', 'id': 1}
-        client.create_app.return_value = (200, 'OK', osdata)
-        mock_onesignal.return_value = client
-
-        with patch.dict(self.flask_app.config, {'ONESIGNAL_AUTH_KEY': 'key'}):
-            user = UserFactory.create()
-            project_data = dict(name='name', short_name='short_name',
-                                description='desc',
-                                long_description='long')
-            url = '/api/project?api_key=%s' % user.api_key
-
-            res = self.app.post(url, data=json.dumps(project_data))
-
-            assert res.status_code == 200, res.data
-
-            project = json.loads(res.data)
-
-            mock_queue.assert_called_with(create_onesignal_app, project['id'])
-
-            res = create_onesignal_app(project['id'])
-
-            assert res[0] == 200, res
-            assert res[1] == 'OK', res
-            assert res[2]['id'] == 1, res
-
-            new = project_repo.get(project['id'])
-
-            assert new.info['onesignal'] == osdata, new.info
-            assert new.info['onesignal_app_id'] == 1, new.info
 
     @with_context
     @patch('pybossa.jobs.PybossaOneSignal')
