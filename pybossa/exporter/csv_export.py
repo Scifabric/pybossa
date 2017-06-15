af7530291f745f715fc445e177faf2afb8dc0633
Daniel Lombraña González <teleyinex@gmail.com>
2017-06-15 13:15:50 +0200

    Use Pandas to save the data as CSV.
    This is huge improvement in comparison with our CSVWriter and
    flatten solution. This saves around 4 minutes for a big project.

--- pybossa/exporter/csv_export.py
+++ pybossa/exporter/csv_export.py
@@ -28,96 +28,31 @@ from pybossa.model.task_run import TaskRun
 from pybossa.util import UnicodeWriter
 from werkzeug.datastructures import FileStorage
 from werkzeug.utils import secure_filename
+from flatten_json import flatten
+import pandas as pd
 
 
 class CsvExporter(Exporter):
 
-    def _format_csv_row(self, row, ty):
-        tmp = row.keys()
-        task_keys = []
-        for k in tmp:
-            k = "%s__%s" % (ty, k)
-            task_keys.append(k)
-        if (type(row['info']) == dict):
-            task_info_keys = []
-            tmp = row['info'].keys()
-            for k in tmp:
-                k = "%sinfo__%s" % (ty, k)
-                task_info_keys.append(k)
-        else:
-            task_info_keys = []
-
-        keys = sorted(task_keys + task_info_keys)
-        values = []
-        _prefix = "%sinfo" % ty
-        for k in keys:
-            prefix, k = k.split("__")
-            if prefix == _prefix:
-                if row['info'].get(k) is not None:
-                    values.append(row['info'][k])
-                else:
-                    values.append(None)
-            else:
-                if row.get(k) is not None:
-                    values.append(row[k])
-                else:
-                    values.append(None)
-
-        return values
-
-    def _handle_row(self, writer, t, ty):
-        normal_ty = filter(lambda char: char.isalpha(), ty)
-        writer.writerow(self._format_csv_row(t.dictize(), ty=normal_ty))
-
-    def _get_csv(self, out, writer, table, id):
-        for tr in getattr(task_repo, 'filter_%ss_by' % table)(project_id=id,
-                                                              yielded=True):
-            self._handle_row(writer, tr, table)
-        out.seek(0)
-        yield out.read()
-
-    def _format_headers(self, t, ty):
-        tmp = t.dictize().keys()
-        task_keys = []
-        for k in tmp:
-            k = "%s__%s" % (ty, k)
-            task_keys.append(k)
-        if (type(t.info) == dict):
-            task_info_keys = []
-            tmp = t.info.keys()
-            for k in tmp:
-                k = "%sinfo__%s" % (ty, k)
-                task_info_keys.append(k)
-        else:
-            task_info_keys = []
-        keys = task_keys + task_info_keys
-        return sorted(keys)
-
     def _respond_csv(self, ty, id):
         out = tempfile.TemporaryFile()
         writer = UnicodeWriter(out)
-        t = getattr(task_repo, 'get_%s_by' % ty)(project_id=id)
-        if t is not None:
-            headers = self._format_headers(t, ty)
-            writer.writerow(headers)
-
-            return self._get_csv(out, writer, ty, id)
+        data = getattr(task_repo, 'filter_%ss_by' % ty)(project_id=id)
+        if data is not None:
+            flat_data = [flatten(datum.dictize()) for datum in data]
+            return pd.DataFrame(flat_data)
         else:
-            def empty_csv(out):
-                yield out.read()
-            return empty_csv(out)
-
+            return pd.DataFrame([])
+        
     def _make_zip(self, project, ty):
         name = self._project_name_latin_encoded(project)
-        csv_task_generator = self._respond_csv(ty, project.id)
-        if csv_task_generator is not None:
-            # TODO: use temp file from csv generation directly
+        dataframe = self._respond_csv(ty, project.id)
+        if csv_data is not None:
             datafile = tempfile.NamedTemporaryFile()
             try:
-                for line in csv_task_generator:
-                    datafile.write(str(line))
+                dataframe.to_csv(datafile)
                 datafile.flush()
-                csv_task_generator.close()  # delete temp csv file
+                #csv_task_generator.close()  # delete temp csv file
                 zipped_datafile = tempfile.NamedTemporaryFile()
                 try:
                     _zip = self._zip_factory(zipped_datafile.name)
