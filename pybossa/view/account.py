599fb1c80c74705508eb540519599be853ea3620
Alexander Mendes <alexanderhmendes@gmail.com>
2017-06-20 11:45:45 +0100

    Fix typo

--- pybossa/view/account.py
+++ pybossa/view/account.py
@@ -653,7 +653,7 @@ def forgot_password():
                     '/account/email/forgot_password.html',
                     user=user, recovery_url=recovery_url)
             mail_queue.enqueue(send_mail, msg)
-            flash(gettext("We've send you an email with account "
+            flash(gettext("We've sent you an email with account "
                           "recovery instructions!"),
                   'success')
         else:
