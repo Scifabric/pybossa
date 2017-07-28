db637ab9173b96fd332061e24f357f5ad0ee448a
Marvin Reimer <therealmarv@gmail.com>
2017-07-28 11:45:26 +0200

    point to announcements

---
+++ alembic/versions/451c04151670_.py
@@ -0,0 +1,22 @@
+"""empty message
+
+Revision ID: 451c04151670
+Revises: ('b56c34fc4beb', '52209719b79e')
+Create Date: 2017-10-03 16:05:35.333146
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '451c04151670'
+down_revision = ('b56c34fc4beb', '52209719b79e')
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
