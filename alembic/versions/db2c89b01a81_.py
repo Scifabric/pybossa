d1136925a606a86cb426e53dbd0554200b0c498f
dtws <dtws@gigwork.net>
2017-08-11 09:44:49 -0400

    alembic merge

---
+++ alembic/versions/db2c89b01a81_.py
@@ -0,0 +1,22 @@
+"""empty message
+
+Revision ID: db2c89b01a81
+Revises: ('0a6628a97161', '5fd18f94723a')
+Create Date: 2017-08-10 20:57:42.667767
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = 'db2c89b01a81'
+down_revision = ('0a6628a97161', '5fd18f94723a')
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
