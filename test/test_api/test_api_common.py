55f5c328418e2ae33a1aa872e0b60b85ef0ffb8a
Daniel Lombraña González <teleyinex@gmail.com>
2017-08-30 17:45:02 +0200

    Tests.

--- test/test_api/test_api_common.py
+++ test/test_api/test_api_common.py
@@ -16,6 +16,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with PYBOSSA.  If not, see <http://www.gnu.org/licenses/>.
 import json
+import datetime
 from default import with_context
 from nose.tools import assert_equal, assert_raises
 from test_api import TestAPI
@@ -95,6 +96,14 @@ class TestApiCommon(TestAPI):
         data = json.loads(res.data)
         assert len(data) == 1, len(data)
         assert data[0].get('id') == project_created.id
+        year = datetime.datetime.now().year
+        res = self.app.get('/api/project?created=%s' % year)
+        data = json.loads(res.data)
+        assert len(data) == 20, len(data)
+        res = self.app.get('/api/project?created=%s&limit=100' % year)
+        data = json.loads(res.data)
+        assert len(data) == 30, len(data)
+
 
     @with_context
     def test_get_query_with_api_key_and_all(self):
