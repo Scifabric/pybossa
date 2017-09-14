8c9fa0e891b5fbdc708827f8dbbbf2300bcb9bff
Daniel Lombraña González <teleyinex@gmail.com>
2017-09-14 13:27:03 +0200

    Fixes.

--- pybossa/core.py
+++ pybossa/core.py
@@ -325,7 +325,7 @@ def setup_twitter_login(app):
     try:  # pragma: no cover
         if (app.config['TWITTER_CONSUMER_KEY'] and
                 app.config['TWITTER_CONSUMER_SECRET'] and
-                app.config['LDAP_HOST'] is None):
+                app.config.get('LDAP_HOST') is None):
             twitter.init_app(app)
             from pybossa.view.twitter import blueprint as twitter_bp
             app.register_blueprint(twitter_bp, url_prefix='/twitter')
@@ -342,7 +342,7 @@ def setup_facebook_login(app):
     try:  # pragma: no cover
         if (app.config['FACEBOOK_APP_ID']
                 and app.config['FACEBOOK_APP_SECRET']
-                and app.config['LDAP_HOST'] is None):
+                and app.config.get('LDAP_HOST') is None):
             facebook.init_app(app)
             from pybossa.view.facebook import blueprint as facebook_bp
             app.register_blueprint(facebook_bp, url_prefix='/facebook')
@@ -359,7 +359,7 @@ def setup_google_login(app):
     try:  # pragma: no cover
         if (app.config['GOOGLE_CLIENT_ID']
                 and app.config['GOOGLE_CLIENT_SECRET']
-                and app.config['LDAP_HOST'] is None):
+                and app.config.get('LDAP_HOST') is None):
             google.init_app(app)
             from pybossa.view.google import blueprint as google_bp
             app.register_blueprint(google_bp, url_prefix='/google')
