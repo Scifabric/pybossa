2c10b183a9cbe2cffed7462fe0b843c6621da7d4
dtws <dtws@gigwork.net>
2017-10-19 15:14:34 -0400

    more tests

--- test/test_importers/test_localCSV_importer.py
+++ test/test_importers/test_localCSV_importer.py
@@ -19,6 +19,7 @@
 from mock import patch, Mock, mock_open
 from pybossa.importers.csv import BulkTaskLocalCSVImport, BulkTaskGDImport
 
+
 class TestBulkTaskLocalCSVImport(object):
 
     def setUp(self):
@@ -34,17 +35,20 @@ class TestBulkTaskLocalCSVImport(object):
         csv_file = self.importer._get_data()
         assert csv_file == 'fakefile.csv'
 
-    def test_count_tasks_returns_0_row(self):
+    @patch('pybossa.importers.csv.BulkTaskLocalCSVImport.get_local_csv_import_file_from_s3')
+    def test_count_tasks_returns_0_row(self, s3_get):
         with patch('pybossa.importers.csv.open', mock_open(read_data='Foo,Bar\n'), create=True):
             number_of_tasks = self.importer.count_tasks()
             assert number_of_tasks is 0, number_of_tasks
 
-    def test_count_tasks_returns_1_row(self):
+    @patch('pybossa.importers.csv.BulkTaskLocalCSVImport.get_local_csv_import_file_from_s3')
+    def test_count_tasks_returns_1_row(self, s3_mock):
         with patch('pybossa.importers.csv.open', mock_open(read_data='Foo,Bar\n1,2\n'), create=True):
             number_of_tasks = self.importer.count_tasks()
             assert number_of_tasks is 1, number_of_tasks
-            
-    def test_count_tasks_returns_2_rows(self):
+
+    @patch('pybossa.importers.csv.BulkTaskLocalCSVImport.get_local_csv_import_file_from_s3')
+    def test_count_tasks_returns_2_rows(self, s3_mock):
         with patch('pybossa.importers.csv.open', mock_open(read_data='Foo,Bar\n1,2\naaa,bbb\n'), create=True):
             number_of_tasks = self.importer.count_tasks()
             assert number_of_tasks is 2, number_of_tasks
