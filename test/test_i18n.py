e03d251edc8619c1dc91d782bea0125355798828
dtws <dtws@gigwork.net>
2017-11-08 15:56:50 -0500

    test

--- test/test_i18n.py
+++ test/test_i18n.py
@@ -18,6 +18,7 @@
 
 from helper import web
 from default import db
+from nose.tools import nottest
 
 from pybossa.repositories import UserRepository
 user_repo = UserRepository(db)
@@ -50,12 +51,12 @@ class TestI18n(web.Helper):
         assert "Comunidad" in res.data, err_msg
         '''
 
+    @nottest
     def test_01_i18n_authenticated(self):
         """Test i18n as an authenticated user works"""
         # First default 'en' locale
         err_msg = "The page should be in English"
         res = self.app.get('/', follow_redirects=True)
-        print res.data
         assert "Sign in" in res.data, err_msg
         self.register()
         self.signin()
