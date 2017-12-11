7a7a1ae69151459afce64a443522174fdb04e5ed
dtws <dtws@gigwork.net>
2017-12-11 11:42:02 -0500

    * Set the completed column to display by default on task browse

--- pybossa/cache/task_browse_helpers.py
+++ pybossa/cache/task_browse_helpers.py
@@ -198,7 +198,7 @@ def parse_tasks_browse_args(args):
         parsed_args["display_columns"] = json.loads(args['display_columns'])
     if not isinstance(parsed_args.get("display_columns"), list):
         parsed_args["display_columns"] = ['task_id', 'priority', 'pcomplete',
-                                          'created', 'actions']
+                                          'created', 'finish_time', 'actions']
 
     parsed_args["order_by_dict"] = dict()
     if args.get('order_by'):
