b2c181b6869537da5ffa8e016a055c66e7280258
dtws <dtws@gigwork.net>
2017-12-12 10:14:02 -0500

    handle empty user info

--- pybossa/api/user.py
+++ pybossa/api/user.py
@@ -59,7 +59,7 @@ class UserAPI(APIBase):
             tmp['subadmin'] = user_data['subadmin']
             tmp['pro'] = user_data['pro']
             tmp['enabled'] = user_data['enabled']
-            metadata = user_data['info'].get('metadata')
+            metadata = user_data.get('info', {}).get('metadata')
             if metadata:
                 tmp['info']['metadata'] = metadata
             return tmp
