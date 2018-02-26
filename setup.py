47a7a66885aba7a6c10e3ecd17d2cecdce4be63b
Daniel Lombraña González <teleyinex@gmail.com>
2018-02-26 13:30:00 +0100

    Increase version.

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ requirements = [
 
 setup(
     name = 'pybossa',
-    version = '2.9.1',
+    version = '2.9.2',
     packages = find_packages(),
     install_requires = requirements,
     # only needed when installing directly from setup.py (PyPi, eggs?) and pointing to e.g. a git repo.
