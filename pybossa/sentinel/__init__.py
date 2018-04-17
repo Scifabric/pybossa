aeb75b239dbb01ee7a4709c87030af9e4fbb15d8
Daniel Lombraña González <teleyinex@gmail.com>
2018-04-17 10:03:40 +0200

    PEP8.

--- pybossa/sentinel/__init__.py
+++ pybossa/sentinel/__init__.py
@@ -25,12 +25,12 @@ class Sentinel(object):
         self.app = app
         self.master = StrictRedis()
         self.slave = self.master
-        if app is not None: # pragma: no cover
+        if app is not None:  # pragma: no cover
             self.init_app(app)
 
     def init_app(self, app):
         self.connection = sentinel.Sentinel(app.config['REDIS_SENTINEL'],
-                                                  socket_timeout=0.1)
+                                            socket_timeout=0.1)
         redis_db = app.config.get('REDIS_DB') or 0
         redis_master = app.config.get('REDIS_MASTER') or 'mymaster'
         self.master = self.connection.master_for(redis_master, db=redis_db)
