acacdd80e3f7eebeeccf0ce90b91b8500b9029bc
Daniel Lombraña González <teleyinex@gmail.com>
2018-04-20 11:34:25 +0200

    Default to false. Fixes #1826

--- pybossa/model/user.py
+++ pybossa/model/user.py
@@ -61,7 +61,7 @@ class User(db.Model, DomainObject, UserMixin):
     newsletter_prompted = Column(Boolean, default=False)
     valid_email = Column(Boolean, default=False)
     confirmation_email_sent = Column(Boolean, default=False)
-    subscribed = Column(Boolean, default=True)
+    subscribed = Column(Boolean, default=False)
     consent = Column(Boolean, default=False)
     info = Column(MutableDict.as_mutable(JSONB), default=dict())
     user_pref = Column(JSONB)
