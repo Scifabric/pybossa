5604ef277f9fe881522fce661ab49d136a040893
Daniel Lombraña González <teleyinex@gmail.com>
2018-05-11 13:21:50 +0200

    Refactor.

--- pybossa/exporter/json_export.py
+++ pybossa/exporter/json_export.py
@@ -27,6 +27,7 @@ from pybossa.core import uploader, task_repo
 from werkzeug.datastructures import FileStorage
 from werkzeug.utils import secure_filename
 
+
 class JsonExporter(Exporter):
 
     def gen_json(self, table, project_id):
@@ -36,43 +37,51 @@ class JsonExporter(Exporter):
         # TODO: check ty here
         return self.gen_json(ty, id)
 
-    def _make_zip(self, project, ty, name=None, data=None, user_id=None):
+    def _make_zip(self, project, ty, name=None, data=None, user_id=None,
+                  zipname=None):
         if data:
-            self.handle_zip(name, data, ty, user_id, project)
+            self.handle_zip(name, data, ty, user_id, project, 'json', zipname)
         else:
             name = self._project_name_latin_encoded(project)
             json_task_generator = self._respond_json(ty, project.id)
             if json_task_generator is not None:
-                self.handle_zip(name, json_task_generator, ty, user_id, project)
-
-    def handle_zip(self, name, data, ty, user_id, project):
-        datafile = tempfile.NamedTemporaryFile()
-        try:
-            datafile.write(json.dumps(data))
-            datafile.flush()
-            zipped_datafile = tempfile.NamedTemporaryFile()
-            try:
-                _zip = self._zip_factory(zipped_datafile.name)
-                _zip.write(datafile.name, secure_filename('%s_%s.json' % (name, ty)))
-                _zip.close()
-                if user_id:
-                    container = "user_%d" % user_id
-                    _file = FileStorage(filename="user_%s.zip" % user_id, stream=zipped_datafile)
-                else:
-                    container = "user_%d" % project.owner_id
-                    _file = FileStorage(filename=self.download_name(project, ty), stream=zipped_datafile)
-                print _file
-                uploader.upload_file(_file, container=container)
-            finally:
-                zipped_datafile.close()
-        finally:
-            datafile.close()
+                self.handle_zip(name, json_task_generator,
+                                ty, user_id, project, 'json', zipname)
 
     def download_name(self, project, ty):
         return super(JsonExporter, self).download_name(project, ty, 'json')
 
     def pregenerate_zip_files(self, project):
-        print "%d (json)" % project.id
+        print("%d (json)" % project.id)
         self._make_zip(project, "task")
         self._make_zip(project, "task_run")
         self._make_zip(project, "result")
+
+    def handle_zip(self, name, data, ty, user_id, project, ext, zipname=None):
+        zipped_datafile = tempfile.NamedTemporaryFile()
+        _zip = self._zip_factory(zipped_datafile.name)
+        try:
+            datafile = tempfile.NamedTemporaryFile()
+            try:
+                datafile.write(json.dumps(data))
+                datafile.flush()
+                _zip.write(datafile.name,
+                           secure_filename('%s_%s.%s' % (name, ty, ext)))
+            finally:
+                datafile.close()
+        finally:
+            _zip.close()
+            if user_id:
+                container = "user_%d" % user_id
+                if zipname is None:
+                    zipname = "user_%s.zip" % user_id
+                _file = FileStorage(filename=zipname,
+                                    stream=zipped_datafile)
+            else:
+                container = "user_%d" % project.owner_id
+                fname = self.download_name(project, ty)
+                _file = FileStorage(filename=fname,
+                                    stream=zipped_datafile)
+            uploader.upload_file(_file, container=container)
+
+            zipped_datafile.close()
