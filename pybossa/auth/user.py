954011639269ed11ecc412561c9d430f9af66fdb
Daniel Lombraña González <teleyinex@gmail.com>
2018-05-14 11:56:36 +0200

    Only allow to read users without the restrict flag.

--- pybossa/auth/user.py
+++ pybossa/auth/user.py
@@ -32,6 +32,8 @@ class UserAuth(object):
         return user.is_authenticated() and user.admin is True
 
     def _read(self, user, resource_user=None):
+        if (user.restrict):
+            return False
         return True
 
     def _update(self, user, resource_user):
