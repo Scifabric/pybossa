212abdd23f45684b7e5eab6a0b39d3e554ef01f9
ffiordalisi <ffiordalisi@bloomberg.net>
2018-05-15 08:38:59 -0400

    config from env var, loop

--- run_scheduler.py
+++ run_scheduler.py
@@ -1,23 +1,38 @@
-import settings_local as settings
+import os
 from redis import StrictRedis
 from redis.sentinel import Sentinel
 from rq_scheduler.scheduler import Scheduler
 from rq.logutils import setup_loghandlers
+from flask import Flask
+from time import sleep
 
 
 def run_scheduler():
     setup_loghandlers('DEBUG')
 
-    db = getattr(settings, 'REDIS_DB', 0)
-    if all(hasattr(settings, attr) for attr in
+    app = Flask('sched_config')
+
+    app.config.from_envvar('PYBOSSA_SETTINGS', silent=True)
+    if not os.environ.get('PYBOSSA_SETTINGS'):
+        here = os.path.dirname(os.path.abspath(__file__))
+        config_path = os.path.join(here, 'settings_local.py')
+        if os.path.exists(config_path):
+            app.config.from_pyfile(config_path)
+
+    db = app.config.get('REDIS_DB', 0)
+    if all(app.config.get(attr) for attr in
         ['REDIS_MASTER_DNS', 'REDIS_PORT']):
-        master = StrictRedis(host=settings.REDIS_MASTER_DNS,
-            port=settings.REDIS_PORT, db=db)
+        master = StrictRedis(host=app.config['REDIS_MASTER_DNS'],
+            port=app.config['REDIS_PORT'], db=db)
     else:
-        sentinel = Sentinel(settings.REDIS_SENTINEL)
-        master = sentinel.master_for(settings.REDIS_MASTER, db=db)
+        sentinel = Sentinel(app.config['REDIS_SENTINEL'])
+        master = sentinel.master_for(app.config['REDIS_MASTER'], db=db)
     scheduler = Scheduler(connection=master)
-    scheduler.run()
+    while True:
+        try:
+            scheduler.run()
+        except ValueError:
+            sleep(600)
 
 
 if __name__ == '__main__':
