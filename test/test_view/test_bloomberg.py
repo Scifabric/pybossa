c8d1ac0a2d51f401276762599ff2503f5d7a9cf0
Jaret Flores <jflores63@bloomberg.net>
2018-10-30 16:16:55 -0400

    bsso

---
+++ test/test_view/test_bloomberg.py
@@ -0,0 +1,67 @@
+# -*- coding: utf8 -*-
+# This file is part of PYBOSSA.
+#
+# Copyright (C) 2015 Scifabric LTD.
+#
+# PYBOSSA is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# PYBOSSA is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with PYBOSSA.  If not, see <http://www.gnu.org/licenses/>.
+from default import Test, with_context
+from mock import patch
+from mock import MagicMock
+from factories import UserFactory
+from pybossa.view import bloomberg as bb
+
+
+class TestBloomberg(Test):
+    def setUp(self):
+        super(TestBloomberg, self).setUp()
+
+    @with_context
+    def test_login_get(self):
+        res = self.app.get('/bloomberg/login')
+        redirect_url = self.flask_app.config['BSSO_SETTINGS']['idp']['singleSignOnService']['url']
+        assert res.status_code == 302, res.status_code
+        assert res.headers['Location'].startswith(redirect_url), res.headers
+
+    @with_context
+    @patch('pybossa.view.bloomberg.OneLogin_Saml2_Auth', autospec=True)
+    def test_login_post_errors(self, mock):
+        mock_auth = MagicMock()
+        mock_auth.get_errors.return_value = True
+        mock.return_value = mock_auth
+        res = self.app.post('/bloomberg/login')
+        assert res.status_code == 302, res.status_code
+
+    @with_context
+    @patch('pybossa.view.bloomberg.OneLogin_Saml2_Auth', autospec=True)
+    def test_login_post_success(self, mock_one_login):
+        user = UserFactory.create()
+        redirect_url = 'http://localhost'
+        mock_auth = MagicMock()
+        mock_auth.get_errors.return_value = False
+        mock_auth.is_authenticated.return_value = True
+        mock_auth.get_attributes.return_value = {'emailAddress': [user.email_addr]}
+        mock_one_login.return_value = mock_auth
+        res = self.app.post('/bloomberg/login', content_type='multipart/form-data', data={'RelayState': redirect_url})
+        assert res.status_code == 302, res.status_code
+        assert res.headers['Location'] == redirect_url, res.headers
+
+    @with_context
+    @patch('pybossa.view.bloomberg.OneLogin_Saml2_Auth', autospec=True)
+    def test_login_post_not_authenticated(self, mock_one_login):
+        mock_auth = MagicMock()
+        mock_auth.get_errors.return_value = False
+        mock_auth.is_authenticated.return_value = False
+        mock_one_login.return_value = mock_auth
+        res = self.app.post('/bloomberg/login')
+        assert res.status_code == 302, res.status_code
