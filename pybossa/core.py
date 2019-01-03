b66e3788f8e664f9169d68ebedcda1deba5dc8eb
Daniel Lombraña González <teleyinex@gmail.com>
2019-01-03 15:16:26 +0100

    Migrate from 2to3.

--- pybossa/core.py
+++ pybossa/core.py
@@ -104,7 +104,7 @@ def configure_app(app):
             app.config['SQLALCHEMY_DATABASE_TEST_URI']
     # Enable Slave bind in case is missing using Master node
     if app.config.get('SQLALCHEMY_BINDS') is None:
-        print "Slave binds are misssing, adding Master as slave too."
+        print("Slave binds are misssing, adding Master as slave too.")
         app.config['SQLALCHEMY_BINDS'] = \
             dict(slave=app.config.get('SQLALCHEMY_DATABASE_URI'))
     app.url_map.strict_slashes = app.config.get('STRICT_SLASHES')
@@ -258,7 +258,7 @@ def setup_logging(app):
 def setup_login_manager(app):
     """Setup login manager."""
     login_manager.login_view = 'account.signin'
-    login_manager.login_message = u"Please sign in to access this page."
+    login_manager.login_message = "Please sign in to access this page."
 
     @login_manager.user_loader
     def _load_user(username):
@@ -343,10 +343,10 @@ def setup_twitter_login(app):
             from pybossa.view.twitter import blueprint as twitter_bp
             app.register_blueprint(twitter_bp, url_prefix='/twitter')
     except Exception as inst:  # pragma: no cover
-        print type(inst)
-        print inst.args
-        print inst
-        print "Twitter signin disabled"
+        print(type(inst))
+        print(inst.args)
+        print(inst)
+        print("Twitter signin disabled")
         log_message = 'Twitter signin disabled: %s' % str(inst)
         app.logger.info(log_message)
 
@@ -360,10 +360,10 @@ def setup_facebook_login(app):
             from pybossa.view.facebook import blueprint as facebook_bp
             app.register_blueprint(facebook_bp, url_prefix='/facebook')
     except Exception as inst:  # pragma: no cover
-        print type(inst)
-        print inst.args
-        print inst
-        print "Facebook signin disabled"
+        print(type(inst))
+        print(inst.args)
+        print(inst)
+        print("Facebook signin disabled")
         log_message = 'Facebook signin disabled: %s' % str(inst)
         app.logger.info(log_message)
 
@@ -377,10 +377,10 @@ def setup_google_login(app):
             from pybossa.view.google import blueprint as google_bp
             app.register_blueprint(google_bp, url_prefix='/google')
     except Exception as inst:  # pragma: no cover
-        print type(inst)
-        print inst.args
-        print inst
-        print "Google signin disabled"
+        print(type(inst))
+        print(inst.args)
+        print(inst)
+        print("Google signin disabled")
         log_message = 'Google signin disabled: %s' % str(inst)
         app.logger.info(log_message)
 
@@ -395,10 +395,10 @@ def setup_flickr_importer(app):
             importer_params = {'api_key': app.config['FLICKR_API_KEY']}
             importer.register_flickr_importer(importer_params)
     except Exception as inst:  # pragma: no cover
-        print type(inst)
-        print inst.args
-        print inst
-        print "Flickr importer not available"
+        print(type(inst))
+        print(inst.args)
+        print(inst)
+        print("Flickr importer not available")
         log_message = 'Flickr importer not available: %s' % str(inst)
         app.logger.info(log_message)
 
@@ -408,10 +408,10 @@ def setup_dropbox_importer(app):
         if app.config['DROPBOX_APP_KEY']:
             importer.register_dropbox_importer()
     except Exception as inst:  # pragma: no cover
-        print type(inst)
-        print inst.args
-        print inst
-        print "Dropbox importer not available"
+        print(type(inst))
+        print(inst.args)
+        print(inst)
+        print("Dropbox importer not available")
         log_message = 'Dropbox importer not available: %s' % str(inst)
         app.logger.info(log_message)
 
@@ -426,10 +426,10 @@ def setup_twitter_importer(app):
             }
             importer.register_twitter_importer(importer_params)
     except Exception as inst:  # pragma: no cover
-        print type(inst)
-        print inst.args
-        print inst
-        print "Twitter importer not available"
+        print(type(inst))
+        print(inst.args)
+        print(inst)
+        print("Twitter importer not available")
         log_message = 'Twitter importer not available: %s' % str(inst)
         app.logger.info(log_message)
 
@@ -441,10 +441,10 @@ def setup_youtube_importer(app):
             }
             importer.register_youtube_importer(importer_params)
     except Exception as inst:  # pragma: no cover
-        print type(inst)
-        print inst.args
-        print inst
-        print "Youtube importer not available"
+        print(type(inst))
+        print(inst.args)
+        print(inst)
+        print("Youtube importer not available")
         log_message = 'Youtube importer not available: %s' % str(inst)
         app.logger.info(log_message)
 
@@ -549,7 +549,7 @@ def setup_hooks(app):
         if app.config.get('ANNOUNCEMENT'):
             announcement = app.config['ANNOUNCEMENT']
             if current_user and current_user.is_authenticated():
-                for key in announcement.keys():
+                for key in list(announcement.keys()):
                     if key == 'admin' and current_user.admin:
                         flash(announcement[key], 'info')
                     if key == 'owner' and len(current_user.projects) != 0:
