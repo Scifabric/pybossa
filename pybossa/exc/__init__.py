581360170e520094c87a3ef89913fa96f29ce251
Daniel Lombraña González <teleyinex@gmail.com>
2019-01-03 15:32:08 +0100

    Migrate to 3.

--- pybossa/exc/__init__.py
+++ pybossa/exc/__init__.py
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with PYBOSSA.  If not, see <http://www.gnu.org/licenses/>.
 
-from repository import WrongObjectError, DBIntegrityError
+from .repository import WrongObjectError, DBIntegrityError
 
 assert WrongObjectError
 assert DBIntegrityError
