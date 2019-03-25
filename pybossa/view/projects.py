cb204e4c527e182338e0c5333435a7e8595cc799
ffiordalisi <ffiordalisi@bloomberg.net>
2019-03-25 14:35:54 -0400

    test

--- pybossa/view/projects.py
+++ pybossa/view/projects.py
@@ -3010,18 +3010,11 @@ def show_performance_stats(short_name):
     title = project_title(project, "Performance Statistics")
     pro = pro_features(owner)
 
-    if project.needs_password():
-        redirect_to_password = _check_if_redirect_to_password(project)
-        if redirect_to_password:
-            return redirect_to_password
-
     answer_fields = project.info.get('answer_fields', {})
-
     project_sanitized, owner_sanitized = sanitize_project_owner(project,
                                                                 owner,
                                                                 current_user,
                                                                 ps)
-
     _, _, user_ids = stats.stats_users(project.id)
     users = {uid: cached_users.get_user_info(uid)['name'] for uid, _ in user_ids}
 
@@ -3030,7 +3023,7 @@ def show_performance_stats(short_name):
                     project=project_sanitized,
                     answer_fields=answer_fields,
                     owner=owner_sanitized,
-                    users=users,
+                    contributors=users,
                     pro_features=pro)
 
     return handle_content_type(response)
