f59754570049a41ed6448d608abca5a9a0595216
ywrsusan <wyuan35@bloomberg.net>
2019-07-01 15:06:01 -0400

    fix comments

--- pybossa/util.py
+++ pybossa/util.py
@@ -1064,15 +1064,17 @@ def get_user_pref_db_clause(user_pref, user_email=None):
 
     if not user_prefs:
         user_pref_sql = '''(task.user_pref IS NULL OR task.user_pref = \'{}\' )'''
-        email_sql = ''' OR (task.user_pref->\'{}\' IS NULL AND task.user_pref->\'{}\' IS NULL
-                AND task.user_pref->\'{}\' IS NOT NULL AND task.user_pref @> :assign_user)
-                '''.format(location_key, language_key, assign_key)
+        if user_email:
+            email_sql = ''' OR (task.user_pref->\'{}\' IS NULL AND task.user_pref->\'{}\' IS NULL
+                    AND task.user_pref->\'{}\' IS NOT NULL AND task.user_pref @> :assign_user)
+                    '''.format(location_key, language_key, assign_key)
     else:
         sql = ('task.user_pref @> \'{}\''.format(json.dumps(up).lower())
                    for up in user_prefs)
         user_pref_sql = '''( (task.user_pref-> \'{}\' IS NULL AND task.user_pref-> \'{}\' IS NULL) OR ({}) )'''.format(location_key, language_key, ' OR '.join(sql))
-        email_sql = ''' AND (task.user_pref->\'{}\' IS NULL OR task.user_pref @> :assign_user)
-                '''.format(assign_key)
+        if user_email:
+            email_sql = ''' AND (task.user_pref->\'{}\' IS NULL OR task.user_pref @> :assign_user)
+                    '''.format(assign_key)
 
     return user_pref_sql + email_sql if user_email else user_pref_sql
 
