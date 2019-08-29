f7eb9a965b23693a1793f89ab77a80aabdf1b957
ffiordalisi <ffiordalisi@bloomberg.net>
2019-08-29 17:25:35 -0400

    remove logs

--- pybossa/sched.py
+++ pybossa/sched.py
@@ -303,10 +303,8 @@ def locked_task_sql(project_id, user_id=None, limit=1, rand_within_priority=Fals
         gold_first ->   gold tasks will be returned before non-gold tasks.
     '''
     filters = []
-    current_app.logger.info(filter_user_prefs)
     if filter_user_prefs:
         filters.append('AND ({})'.format(cached_users.get_user_preferences(user_id)))
-    current_app.logger.info(filters)
     filters.append(data_access.get_data_access_db_clause_for_task_assignment(user_id) or '')
     if task_type == 'gold':
         filters.append('AND task.calibration = 1')
