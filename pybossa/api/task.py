46085b5230c7ac683a2e31302c26863cc840a4d3
Francesco Fiordalisi <fiorda@users.noreply.github.com>
2019-10-24 16:13:32 -0400

    Update task.py

--- pybossa/api/task.py
+++ pybossa/api/task.py
@@ -67,7 +67,7 @@ class TaskAPI(APIBase):
             if gold_task or (old.n_answers < new.n_answers and
                 n_taskruns < new.n_answers):
                 new.state = 'ongoing'
-            if not gold_tasks and (n_taskruns >= new.n_answers):
+            if not gold_task and (n_taskruns >= new.n_answers):
                 new.state = 'completed'
         new.calibration = int(gold_task)
         new.exported = gold_task
