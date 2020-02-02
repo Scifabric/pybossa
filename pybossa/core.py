fb0cf0363023e8797851d3c27426d3c88111b2bb
Daniel Lombraña González <teleyinex@gmail.com>
2020-02-02 14:10:06 +0100

    fix: flask-babel force 0.9
    Request and current_user get out of context in newer versions.

--- pybossa/core.py
+++ pybossa/core.py
@@ -276,9 +276,12 @@ def setup_babel(app):
 
     @babel.localeselector
     def _get_locale():
+        from flask import request
         locales = [l[0] for l in app.config.get('LOCALES')]
-        if current_user.is_authenticated:
-            lang = current_user.locale
+        lang = 'en'
+        if current_user != None: 
+            if current_user.is_authenticated:
+                lang = current_user.locale
         else:
             lang = request.cookies.get('language')
         if (lang is None or lang == '' or
