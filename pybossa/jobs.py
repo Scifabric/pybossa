8c3d7e01753176e487dc43351042cb6dc5eb824c
ywrsusan <wyuan35@bloomberg.net>
2020-02-11 15:40:54 +0700

    simplify

--- pybossa/jobs.py
+++ pybossa/jobs.py
@@ -728,8 +728,8 @@ def send_email_notifications():
             body = u'Hello,\n\nThere have been new tasks uploaded to the previously finished project, {0}. ' \
                    u'\nLog on to {1} to complete any available tasks.' \
                 .format(project.name, current_app.config.get('BRAND'))
-            recipients_chunk = [recipients[x * MAX_RECIPIENTS : (x+1) * MAX_RECIPIENTS]
-                                for x in range(0, int(math.ceil(len(recipients)*1.0 / MAX_RECIPIENTS)))]
+            recipients_chunk = [recipients[x : x + MAX_RECIPIENTS]
+                                for x in range(0, len(recipients), MAX_RECIPIENTS)]
             for group in recipients_chunk:
                 mail_dict = dict(recipients=group, subject=subject, body=body)
                 send_mail(mail_dict)
