7f7bde371bed805572fb1f53dc9e59651d4551ee
Satyendra Singh <satyendra.singh@mykaamr.com>
2020-06-16 14:56:07 -0700

    renaming theme folder

--- pybossa/themes/mykaarma/templates/account/email/otp.md
+++
@@ -1,7 +0,0 @@
-##One Time Password (OTP) Authentication
-
-Hello, {{user.fullname}}!
-
-Please apply the following OTP Code to complete the signin
-
-OTP code: {{otpcode}}
--- pybossa/themes/mykaarma/templates/account/js/metadata.js
+++
@@ -1,8 +0,0 @@
-<script>
-  $("#edit-button").on('click', function(){
-    $("#input-form").removeClass("hidden");
-    $("#edit-button").hide();
-    $("#no-info-text").hide();
-    $("#user-info").hide();
-  })
-</script>
