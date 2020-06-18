871dfb5eacc0f4500c8d0718bb5cfe9b16666aef
Anna Cianciara <acianciara@bloomberg.net>
2020-06-18 23:31:14 +0000

    fix tests

--- test/test_view/test_bloomberg.py
+++ test/test_view/test_bloomberg.py
@@ -77,7 +77,7 @@ class TestBloomberg(Test):
         mock_auth.process_response.return_value = None
         mock_auth.is_authenticated = False
         mock_one_login.return_value = mock_auth
-        mock_auth.get_attributes.return_value = {'UUID': [u'1234567'], 'firstName': [u'test'], 'emailAddress': ['test@test.com'], 'lastName': [u'test'], 'PVFLevels': [u'PVF_GUTS_3'], 'username': [u'test']}
+        mock_auth.get_attributes.return_value = {'firstName': [u'test'], 'emailAddress': ['test@test.com'], 'lastName': [u'test'], 'username': [u'test']}
         res = self.app.post('/bloomberg/login', method='POST', content_type='multipart/form-data', data={'RelayState': redirect_url})
         assert mock_create_account.called == False
         assert res.status_code == 302, res.status_code
@@ -92,7 +92,7 @@ class TestBloomberg(Test):
         mock_auth.process_response.return_value = None
         mock_auth.is_authenticated = True 
         mock_one_login.return_value = mock_auth
-        mock_auth.get_attributes.return_value = {'UUID': [u'1234567'], 'firstName': [u'test'], 'lastName': [u'test'], 'PVFLevels': [u'PVF_GUTS_3'], 'emailAddress': [u'test@bloomberg.net'], 'loginID': [u'test']}
+        mock_auth.get_attributes.return_value = {'firstName': [u'test'], 'lastName': [u'test'], 'emailAddress': [u'test@bloomberg.net'], 'username': [u'test']}
         mock_create_account.return_value = None
         res = self.app.post('/bloomberg/login', method='POST', content_type='multipart/form-data', data={'RelayState': redirect_url})
         assert mock_create_account.called
@@ -110,7 +110,7 @@ class TestBloomberg(Test):
         mock_auth.is_authenticated = True
         mock_one_login.return_value = mock_auth
         mock_sign_in.side_effect = Exception()
-        mock_auth.get_attributes.return_value = {'UUID': [u'1234567'], 'FirstName': [u'test'], 'emailAddress': ['test@test.com'], 'LastName': [u'test'], 'PVFLevels': [u'PVF_GUTS_3'], 'LoginID': [u'test']}
+        mock_auth.get_attributes.return_value = {'firstName': [u'test'], 'emailAddress': ['test@test.com'], 'lastName': [u'test'], 'PVFLevels': [u'PVF_GUTS_3'], 'username': [u'test']}
         res = self.app.post('/bloomberg/login', method='POST', content_type='multipart/form-data', data={'RelayState': redirect_url})
-        assert mock_create_account.called == True
+        assert mock_create_account.called 
         assert res.status_code == 302, res.status_code
