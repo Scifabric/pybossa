290d05beca08198fff6a3f994eb9e54534d424ef
Anna Cianciara <acianciara@gmail.com>
2020-06-23 02:16:59 +0000

    fix test

--- test/test_view/test_bloomberg.py
+++ test/test_view/test_bloomberg.py
@@ -98,7 +98,7 @@ class TestBloomberg(Test):
         assert res.status_code == 302, res.status_code
         
     @with_context
-    @patch('pybossa.util.generate_bsso_account_notification', autospec=True)
+    @patch('pybossa.view.account.generate_bsso_account_notification', autospec=True)
     @patch('pybossa.view.bloomberg.OneLogin_Saml2_Auth', autospec=True)
     def test_bsso_auto_account_alert(self, mock_one_login, mock_bsso_alert):
         redirect_url = 'http://localhost'
@@ -107,6 +107,7 @@ class TestBloomberg(Test):
         mock_auth.process_response.return_value = None
         mock_auth.is_authenticated = True 
         mock_one_login.return_value = mock_auth
+        mock_bsso_alert.return_value = None
         mock_auth.get_attributes.return_value = {'firstName': [u'test'], 'lastName': [u'test'], 'emailAddress': [u'test@bloomberg.net'], 'username': [u'test']}
         res = self.app.post('/bloomberg/login', method='POST', content_type='multipart/form-data', data={'RelayState': redirect_url})
         assert mock_bsso_alert.called
