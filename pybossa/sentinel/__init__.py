1c4998d6602f7ccdaa831544402e8111ff7c6637
Niti Shah <nits2097@gmail.com>
2020-07-25 18:46:08 -0700

    sentinel changes

--- pybossa/sentinel/__init__.py
+++ pybossa/sentinel/__init__.py
@@ -29,17 +29,18 @@ class Sentinel(object):
             self.init_app(app)
 
     def init_app(self, app):
-        socket_timeout = app.config.get('REDIS_SOCKET_TIMEOUT', None)
-        retry_on_timeout = app.config.get('REDIS_RETRY_ON_TIMEOUT', True)
-        self.connection = sentinel.Sentinel(app.config['REDIS_SENTINEL'],
-                                            socket_timeout=socket_timeout,
-                                            retry_on_timeout=retry_on_timeout)
-        redis_db = app.config.get('REDIS_DB') or 0
-        redis_master = app.config.get('REDIS_MASTER') or 'mymaster'
+        
         
         if(app.config.get('REDIS_MODE')=='master'):
             self.master = StrictRedis(host=app.config.get('REDIS_MK_MASTER'),port=app.config.get('REDIS_PORT'),password=app.config.get('REDIS_PASSWORD'),ssl=app.config.get('REDIS_SSL'))
             self.slave = StrictRedis(host=app.config.get('REDIS_MK_SLAVE'),port=app.config.get('REDIS_PORT'),password=app.config.get('REDIS_PASSWORD'),ssl=app.config.get('REDIS_SSL'))
         else:
+            socket_timeout = app.config.get('REDIS_SOCKET_TIMEOUT', None)
+            retry_on_timeout = app.config.get('REDIS_RETRY_ON_TIMEOUT', True)
+            self.connection = sentinel.Sentinel(app.config['REDIS_SENTINEL'],
+                                                socket_timeout=socket_timeout,
+                                                retry_on_timeout=retry_on_timeout)
+            redis_db = app.config.get('REDIS_DB') or 0
+            redis_master = app.config.get('REDIS_MASTER') or 'mymaster'
             self.master = self.connection.master_for(redis_master, db=redis_db)
             self.slave = self.connection.slave_for(redis_master, db=redis_db)
