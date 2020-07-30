1a35d4a2a6fda5b92f33323d99462d52707cb53c
Niti Shah <nits2097@gmail.com>
2020-07-30 11:49:41 -0700

    added flush

--- app_context_rqworker.py
+++ app_context_rqworker.py
@@ -22,30 +22,30 @@ from rq import Queue, Connection, Worker
 
 from pybossa.core import create_app, sentinel
 
-print("calling create app function")
+print("calling create app function", flush=True)
 app = create_app(run_as_server=False)
-print("returned from app")
-print("just before try")
+print("returned from app", flush=True)
+print("just before try", flush=True)
 try:
-    print("inside try")
+    print("inside try", flush=True)
     # Provide queue names to listen to as arguments to this script,
     # similar to rqworker
     with app.app_context():
-        print("Inside app context class")
+        print("Inside app context class", flush=True)
         with Connection(sentinel.master):
-            print("before creating queue map")
+            print("before creating queue map", flush=True)
             qs = map(Queue, sys.argv[1:]) or [Queue()]
-            print("before creating worker object")
+            print("before creating worker object", flush=True)
             w = Worker(qs)
-            print("1",w.successful_job_count)  # Number of jobs finished successfully
-            print("2",w.failed_job_count) # Number of failed jobs processed by this worker
-            print("3",w.total_working_time)  # Amount of time spent executing jobs (in seconds)
+            print("1",w.successful_job_count, flush=True)  # Number of jobs finished successfully
+            print("2",w.failed_job_count, flush=True) # Number of failed jobs processed by this worker
+            print("3",w.total_working_time, flush=True)  # Amount of time spent executing jobs (in seconds)
             w.work()
-            print("4",w.successful_job_count)  # Number of jobs finished successfully
-            print("5",w.failed_job_count) # Number of failed jobs processed by this worker
-            print("6",w.total_working_time)  # Amount of time spent executing jobs (in seconds)
-        print("outside inner with")
-    print("outside outer with")
+            print("4",w.successful_job_count, flush=True)  # Number of jobs finished successfully
+            print("5",w.failed_job_count, flush=True) # Number of failed jobs processed by this worker
+            print("6",w.total_working_time, flush=True)  # Amount of time spent executing jobs (in seconds)
+        print("outside inner with", flush=True)
+    print("outside outer with", flush=True)
 except Exception as e: 
     logger.error('Failed to add worker: '+ str(e))
-    print("error")
+    print("error", flush=True)
