b3a476edcbc6b24bfbb3bb86f1040ba942813fe5
Niti Shah <nits2097@gmail.com>
2020-07-31 14:18:31 -0700

    requeue job

--- pybossa/jobs.py
+++ pybossa/jobs.py
@@ -803,7 +803,7 @@ def check_failed(queue_name):
             ttl = current_app.config.get('FAILED_JOBS_MAILS')*24*60*60
             sentinel.master.setex(KEY, ttl, 1)
         if int(sentinel.slave.get(KEY)) < FAILED_JOBS_RETRIES:
-            requeue_job(job_id)
+            requeue_job(job_id,connection=redis_conn)
         else:
             KEY = 'pybossa:job:failed:mailed:%s' % job_id
             if (not sentinel.slave.exists(KEY) and
