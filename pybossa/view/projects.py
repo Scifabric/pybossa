917930f0c6dabf8b755e3df196de843c5ac9d3c2
Anna Cianciara <acianciara@bloomberg.net>
2020-07-31 15:49:06 +0000

    modify warning wording

--- pybossa/view/projects.py
+++ pybossa/view/projects.py
@@ -3246,18 +3246,17 @@ def ext_config(short_name):
                     ext_conf.pop('hdfs', None)
 
                 if not ext_conf and current_app.config.get('PRIVATE_INSTANCE'):
-                    flash(gettext('At least one target location must be provided. {} was not updated').format(display), 'error')
+                    flash(gettext('At least one response file & consensus location must be provided. {} was not updated').format(display), 'error')
                 else:
                     project.info['ext_config'] = ext_conf
                     project_repo.save(project)
-                    sanitize_project, _ = sanitize_project_owner(project, owner, current_user, ps)
-
                     current_app.logger.info('Project id {} external configurations set. {} {}'.format(
-                                            project.id, form_name, form.data))
+                            project.id, form_name, form.data))
                     flash(gettext('Configuration for {} was updated').format(display), 'success')
-                   
+
+    sanitize_project, _ = sanitize_project_owner(project, owner, current_user, ps)
     template_forms = [(name, disp, cl(MultiDict(ext_conf.get(name, {}))))
-                      for name, disp, cl in form_classes]
+                        for name, disp, cl in form_classes]
 
     response = dict(
         template='/projects/external_config.html',
