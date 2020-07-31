64ba4faa8808c3d662c5f05fdac29980c61b3b95
Niti Shah <nits2097@gmail.com>
2020-07-31 14:00:45 -0700

    added new failedqueue code

--- pybossa/jobs.py
+++ pybossa/jobs.py
@@ -159,7 +159,7 @@ def get_maintenance_jobs():
     """Return mantainance jobs."""
     timeout = current_app.config.get('TIMEOUT')
     print("ret from get maintenance jobs", flush=True)
-    yield dict(name=check_failed, args=[], kwargs={},
+    yield dict(name=check_failed, args=['maintenance'], kwargs={},
                timeout=timeout, queue='maintenance')
 
 
@@ -776,24 +776,27 @@ def news():
         notify_news_admins()
 
 
-def check_failed():
+def check_failed(queue_name):
     """Check the jobs that have failed and requeue them."""
     from rq import Queue, get_failed_queue, requeue_job
     # from rq import Queue, requeue_job
     from pybossa.core import sentinel
     from rq.registry import FailedJobRegistry
 
+    
+    redis_conn = sentinel.master 
+    queue = Queue(queue_name, connection=redis_conn)
 
-    fq = get_failed_queue()
-    # registry = FailedJobRegistry(queue=queue)
-    # job_ids = registry.get_job_ids()
-    job_ids = fq.job_ids
+    #fq = get_failed_queue()
+    registry = FailedJobRegistry(queue=queue)
+    job_ids = registry.get_job_ids()
+    #job_ids = fq.job_ids
     count = len(job_ids)
     FAILED_JOBS_RETRIES = current_app.config.get('FAILED_JOBS_RETRIES')
     for job_id in job_ids:
         KEY = 'pybossa:job:failed:%s' % job_id
-        # job = queue.fetch_job(job_id)
-        job = fq.fetch_job(job_id)
+        job = queue.fetch_job(job_id)
+        # job = fq.fetch_job(job_id)
         if sentinel.slave.exists(KEY):
             sentinel.master.incr(KEY)
         else:
