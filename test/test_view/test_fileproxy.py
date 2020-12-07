87cf17e511657463dd49cf93b9095b34b8d13d1e
dchhabda <dchhabda@bloomberg.net>
2020-12-07 14:49:00 -0500

    encrypted task payload

--- test/test_view/test_fileproxy.py
+++ test/test_view/test_fileproxy.py
@@ -438,3 +438,160 @@ def test_is_file_url():
     assert not is_valid_url('/a/b?offset=1&length=3&task-signature=asdfasdfad')
     assert is_valid_url('/a/b?offset=1&length=2')
     assert is_valid_url('/a/b?offset=1&length=2&task-signature=asdfasdfas')
+
+
+class TestEncryptedPayload(web.Helper):
+
+    app_config = {
+        'VAULT_CONFIG': {
+            'url': 'https://valut.com/{key_id}',
+            'request': {
+                'headers': {'Authorization': 'apikey'}
+            },
+            'response': ['key'],
+            'error': ['error']
+        },
+        'ENCRYPTION_CONFIG_PATH': ['ext_config', 'encryption']
+    }
+
+    @with_context
+    def test_proxy_no_signature(self):
+        project = ProjectFactory.create()
+        owner = project.owner
+
+        task_id = 2020127
+        url = '/fileproxy/encrypted/taskpayload/%s/%s?api_key=%s' \
+             % (project.id, task_id, owner.api_key)
+        res = self.app.get(url, follow_redirects=True)
+        assert res.status_code == 403, res.status_code
+
+    @with_context
+    def test_proxy_no_task(self):
+        project = ProjectFactory.create()
+        owner = project.owner
+
+        task_id = 2020127
+        signature = signer.dumps({'task_id': task_id})
+
+        url = '/fileproxy/encrypted/taskpayload/%s/%s?api_key=%s&task-signature=%s' \
+            % (project.id, task_id, owner.api_key, signature)
+        with patch.dict(self.flask_app.config, self.app_config):
+            res = self.app.get(url, follow_redirects=True)
+            assert res.status_code == 400, res.status_code
+
+    @with_context
+    @patch('pybossa.view.fileproxy.requests.get')
+    def test_proxy_owner(self, http_get):
+        res = MagicMock()
+        res.json.return_value = {'key': 'testkey'}
+        http_get.return_value = res
+
+        project = ProjectFactory.create(info={
+            'ext_config': {
+                'encryption': {'key_id': 123}
+            }
+        })
+
+        encryption_key = 'testkey'
+        aes = AESWithGCM(encryption_key)
+        content = json.dumps(dict(a=1,b="2"))
+        encrypted_content = aes.encrypt(content)
+        task = TaskFactory.create(project=project, info={
+            'private_json__encrypted_payload': encrypted_content
+        })
+        owner = project.owner
+
+        signature = signer.dumps({'task_id': task.id})
+        url = '/fileproxy/encrypted/taskpayload/%s/%s?api_key=%s&task-signature=%s' \
+            % (project.id, task.id, owner.api_key, signature)
+
+        with patch.dict(self.flask_app.config, self.app_config):
+            res = self.app.get(url, follow_redirects=True)
+            assert res.status_code == 200, res.status_code
+            assert res.data == content, res.data
+
+    @with_context
+    @patch('pybossa.view.fileproxy.requests.get')
+    def test_proxy_admin(self, http_get, hdfs_get):
+        res = MagicMock()
+        res.json.return_value = {'key': 'testkey'}
+        http_get.return_value = res
+
+        admin, owner = UserFactory.create_batch(2)
+        project = ProjectFactory.create(owner=owner, info={
+            'ext_config': {
+                'encryption': {'key_id': 123}
+            }
+        })
+
+        encryption_key = 'testkey'
+        aes = AESWithGCM(encryption_key)
+        content = json.dumps(dict(a=1,b="2"))
+        encrypted_content = aes.encrypt(content)
+        task = TaskFactory.create(project=project, info={
+            'private_json__encrypted_payload': encrypted_content
+        })
+
+        signature = signer.dumps({'task_id': task.id})
+        url = '/fileproxy/encrypted/taskpayload/%s/%s?api_key=%s&task-signature=%s' \
+            % (project.id, task.id, admin.api_key, signature)
+
+        with patch.dict(self.flask_app.config, self.app_config):
+            res = self.app.get(url, follow_redirects=True)
+            assert res.status_code == 200, res.status_code
+            assert res.data == content, res.data
+
+    @with_context
+    @patch('pybossa.view.fileproxy.requests.get')
+    def test_empty_response(self, http_get):
+        """Returns empty response with task payload not containing encrypted data."""
+        res = MagicMock()
+        res.json.return_value = {'key': 'testkey'}
+        http_get.return_value = res
+
+        project = ProjectFactory.create(info={
+            'ext_config': {
+                'encryption': {'key_id': 123}
+            }
+        })
+        encryption_key = 'testkey'
+        task = TaskFactory.create(project=project, info={}) # missing private_json__encrypted_payload
+        owner = project.owner
+
+        signature = signer.dumps({'task_id': task.id})
+        url = '/fileproxy/encrypted/taskpayload/%s/%s?api_key=%s&task-signature=%s' \
+            % (project.id, task.id, owner.api_key, signature)
+
+        with patch.dict(self.flask_app.config, self.app_config):
+            res = self.app.get(url, follow_redirects=True)
+            assert res.status_code == 200, res.status_code
+            assert res.data == '', res.data
+
+    @with_context
+    @patch('pybossa.view.fileproxy.requests.get')
+    def test_proxy_key_err(self, http_get):
+        res = MagicMock()
+        res.json.return_value = {'error': 'an error occurred'}
+        http_get.return_value = res
+
+        admin, owner = UserFactory.create_batch(2)
+        project = ProjectFactory.create(owner=owner, info={
+            'ext_config': {
+                'encryption': {'key_id': 123}
+            }
+        })
+        encryption_key = 'testkey'
+        aes = AESWithGCM(encryption_key)
+        content = json.dumps(dict(a=1,b="2"))
+        encrypted_content = aes.encrypt(content)
+        task = TaskFactory.create(project=project, info={
+            'private_json__encrypted_payload': encrypted_content
+        })
+
+        signature = signer.dumps({'task_id': task.id})
+        url = '/fileproxy/encrypted/taskpayload/%s/%s?api_key=%s&task-signature=%s' \
+            % (project.id, task.id, admin.api_key, signature)
+
+        with patch.dict(self.flask_app.config, self.app_config):
+            res = self.app.get(url, follow_redirects=True)
+            assert res.status_code == 500, res.status_code
