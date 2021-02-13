5c58a24168ccf43bb141939b30da466635d8111e
Daniel Lombraña González <teleyinex@gmail.com>
2021-02-13 20:36:57 +0100

    fix: pin cryptography package.

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ requirements = [
     "cov-core==1.15.0",
     "coverage==4.5.2",
     "croniter==0.3.28",
-    "cryptography>=2.4.2",
+    "cryptography==3.2.1",
     "cssselect==1.0.3",
     "debtcollector==1.20.0",
     "decorator==4.3.0",
