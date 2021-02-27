dee99b05b680ff773b365fd5a22efe76d4b7f6c5
Daniel Lombraña González <teleyinex@gmail.com>
2021-02-27 10:25:51 +0100

    fix: remove duplicated method.
    Closes #1888

--- cli.py
+++ cli.py
@@ -745,27 +745,6 @@ def update_counters():
         db.session.commit()
 
 
-def update_project_stats():
-    """Update project stats for draft projects."""
-    from pybossa.core import db
-    from pybossa.core import project_repo, task_repo, result_repo
-    from pybossa.model.task import Task
-    from pybossa.model.task_run import TaskRun
-    from pybossa.model.counter import Counter
-    import pybossa.cache.project_stats as stats
-
-    projects = project_repo.get_all()
-
-    for project in projects:
-        print("Working on project: %s" % project.short_name)
-        sql_query = """INSERT INTO project_stats
-                       (project_id, n_tasks, n_task_runs, n_results, n_volunteers,
-                       n_completed_tasks, overall_progress, average_time,
-                       n_blogposts, last_activity, info)
-                       VALUES (%s, 0, 0, 0, 0, 0, 0, 0, 0, 0, '{}');""" % (project.id)
-        db.engine.execute(sql_query)
-
-
 def anonymize_ips():
     """Anonymize all the IPs of the server."""
     from pybossa.core import anonymizer, task_repo
