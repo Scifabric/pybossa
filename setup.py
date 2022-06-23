c524862e92a4b3c7425428a56f684be5fd087fbe
Daniel Lombraña González <teleyinex@gmail.com>
2022-06-23 12:59:54 +0200

    chore: pin werkzeug version.

--- setup.py
+++ setup.py
@@ -132,7 +132,7 @@ requirements = [
     "urllib3==1.24.2",
     "validators==0.12.6",
     "webassets==0.12.1",
-    "Werkzeug",
+    "Werkzeug==1.0.1",
     "wrapt==1.10.11",
     "WTForms>=1.0.5",
     "WTForms-Components>=0.10.3",
